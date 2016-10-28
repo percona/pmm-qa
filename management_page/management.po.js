4b399325142837e664beb8645f88a36ac878208c
nailya <nkutlubaeva@gmail.com>
2016-10-28 07:56:34 +0000

    added elements on managemenet page

--- management_page/management.po.js
+++ management_page/management.po.js
@@ -7,7 +7,10 @@ module.exports = {
     selectedConnection: element(by.className('btn btn-warning navbar-btn dropdown-toggle ng-binding')),
     statusTab: element(by.xpath('//a[text()="STATUS"]')),  
     logTab: element(by.xpath('//a[text()="LOG"]')),  
-    settingsTab: element(by.xpath('//a[text()="SETTINGS"]'))  
+    settingsTab: element(by.xpath('//a[text()="SETTINGS"]')),
+    apply: element(by.css('[ng-click="setQanConfig(selected_agent)"]')),
+    sendQueryExamples: element(by.model('qanConf.ExampleQueries')), 
+    collectFrom: element(by.name('source'))     
 },
 
   get: function() {
@@ -27,5 +30,10 @@ module.exports = {
 
   clickSettingsTab: function(connection)  {
     this.managementPage.settingsTab.click();
+  },
+
+  inputCollectInterval: function(interval)  {
+    this.managementPage.collectInterval.clear();
+    this.managementPage.collectInterval.sendKeys(interval);
   }
 };
