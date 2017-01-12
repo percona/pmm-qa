4a01c36df1ab973b3d5fca99e30ecb623b6c099e
nailya <nkutlubaeva@gmail.com>
2017-01-12 11:42:55 +0200

    added auth test for grafana

--- specs/grafana_login_spec.js
+++ specs/grafana_login_spec.js
@@ -1,34 +1,19 @@
-var GrafanaLogin = require('../page_objects/grafana_login.po.js')
 var global = require('../test_data/global_data.json')
-var utils = require('../common/utils.js')
 
-describe('Grafana Login Test Case', function() {
-  beforeEach(function () {
-    browser.ignoreSynchronization = false;
-    GrafanaLogin.get(global['url']);
-    element.all(by.css('.alert.msg')).then(function(items)  {
-      expect(items.length).toBe(0);
-    });
-  });
+describe('Grafana authentication tests', function() { 
+var fail = function() { expect(true).toBe(false); }
 
-  afterEach(function() {
-    browser.get(global['url'] + '/graph/logout');
-  });
+  it('should redirect to the login page if trying to load protected page while not authenticated', function() {
+    browser.get(global['url_graf']);
+    browser.sleep(15000);
+    browser.ignoreSynchronization = true;
+    loginURL = browser.getCurrentUrl();
 
- it('should sign up new user', function() {
-  GrafanaLogin.get(global['url']);
-    element(by.xpath("//div[@class='login-tab-header']//button[.='Sign up']")).click();
-var email = utils.getRandomEmail();
-    console.log(email)
-    GrafanaLogin.setEmail(email);       
-   element(by.xpath("//div[@class='gf-form-button-row']//button[.='Sign up']")).click();
-    value = element(by.name("name")).getAttribute('value');
-   // expect(value).toContain("" + );
-    text = element(by.tagName('html')).getText();
-    expect(text).toContain("" + "Continue");
-element(by.id("inputPassword")).sendKeys("Mc'Adamson\n");
-   browser.sleep(5000);    
-    element(by.linkText("Home")).click();
-    element(by.xpath("//div[@class='search-results-container']//span[.='Disk Performance']")).click();
+     
+    expect(browser.getCurrentUrl()).toContain('login');
   });
+  /*it('should warn on missing/malformed credentials', fail);
+  it('should accept a valid email address and password', fail);
+  it('should return to the login page after logout', fail);
+*/
 });
