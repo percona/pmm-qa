eb376495e666e3ee64c424d432dc4c09852c4f92
puneet0191 <puneet.0191@gmail.com>
2018-11-08 14:07:55 +0530

    PMM-3230 Adding tests for Listing Page Remote Instances

---
+++ tests/pages/remoteInstancesPage.js
@@ -0,0 +1,11 @@
+const I = actor();
+
+module.exports = {
+
+    // insert your locators and methods here
+    // setting locators
+    url: "graph/d/pmm-list/_pmm-rds-and-remote-instances?orgId=1",
+    fields: {
+        pageHeaderText: "_PMM RDS and Remote Instances"
+    }
+}
---
+++ tests/verifyRemoteInstances_test.js
@@ -0,0 +1,13 @@
+
+Feature('to verify monitoried Remote Db instances');
+
+Before((I, loginPage) => {
+    I.amOnPage('/');
+    I.amOnPage(loginPage.url);
+    loginPage.login("admin", "admin");
+});
+
+Scenario('open the Remote and RDS Instances Listing Page', async (I, remoteInstancesPage) => {
+    I.amOnPage(remoteInstancesPage.url);
+    I.see(remoteInstancesPage.fields.pageHeaderText)
+});
