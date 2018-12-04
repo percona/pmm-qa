905753e43de54c2360b603ceb1080c1db7c367c1
puneet0191 <puneet.0191@gmail.com>
2018-12-04 19:16:31 +0530

    PMM-3346 Adding Tests for System Overview Dashboards

---
+++ tests/pages/systemOverviewPage.js
@@ -0,0 +1,25 @@
+const I = actor();
+
+module.exports = {
+
+    // insert your locators and methods here
+    // setting locators
+    url: "graph/d/qyzrQGHmk/system-overview",
+    fields: {
+        pageHeaderText: "System Overview",
+        notAvailableMetrics: "//span[contains(text(), 'NA')]"
+    },
+    metrics: ["System Uptime", "Virtual CPUs", "Load Average", "RAM", "Memory Available", "CPU Usage", "CPU Saturation and Max Core Usage"],
+
+    graphsLocator (metricName){
+        locator = "//span[contains(text(), '"+ metricName +"')]";
+        return locator;
+    },
+
+    verifyMetricsExistence () {
+        for (var i in this.metrics) {
+            I.seeElement(this.graphsLocator(this.metrics[i]));
+        }
+        I.dontSeeElement(this.fields.notAvailableMetrics);
+    }
+}
