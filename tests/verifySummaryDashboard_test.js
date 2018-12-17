f52a82997e0b8619f0b7518d987b983cd55589f7
Nailya <nkutlubaeva@gmail.com>
2018-12-17 15:54:25 +0000

    Added Summary dashbaord test

---
+++ tests/verifySummaryDashboard_test.js
@@ -0,0 +1,14 @@
+
+Feature('to verify Summary Dashboards');
+
+Before((I, loginPage) => {
+    I.amOnPage('/');
+    I.amOnPage(loginPage.url);
+    loginPage.login("admin", "admin");
+});
+
+Scenario('Open the Summary Dashboard', async (I, adminPage, summaryDashboardPage) => {
+    adminPage.navigateToDashboard("Insight", "Summary Dashboard");
+    adminPage.applyTimer("1m");
+    summaryDashboardPage.verifyMetricsExistence();
+});
