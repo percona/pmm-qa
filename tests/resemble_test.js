ecdf276fa8756d6c2e10049ebbf362fe1c5871d4
puneet0191 <puneet.0191@gmail.com>
2018-12-28 20:06:45 +0530

    PMM-3382 fixing the test

--- tests/resemble_test.js
+++ tests/resemble_test.js
@@ -1,6 +1,6 @@
-Feature('to verify monitoried Remote Db instances');
+Feature('To test screen comparison with resemble Js Example test');
 
-Scenario('Open the System Overview Dashboard', async (I, adminPage, loginPage) => {
+Scenario('Open the System Overview Dashboard and take CPU Usage Screenshot @visual-test', async (I, adminPage, loginPage) => {
     I.amOnPage('/');
     I.amOnPage(loginPage.url);
     loginPage.login("admin", "admin");
@@ -13,6 +13,6 @@ Scenario('Open the System Overview Dashboard', async (I, adminPage, loginPage) =
     I.seeFile('System_Overview_CPU_Usage.png');
 });
 
-Scenario('Compare CPU Usage Images', async (I) => {
+Scenario('Compare CPU Usage Images @visual-test', async (I) => {
     I.verifyMisMatchPercentage("System_Overview_CPU_Usage.png", "System_Overview_CPU_Usage.png", "DiffImage_SystemOverview_CPU_USAGE_Dashboard", 5, true);
 });
