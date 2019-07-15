a01f446988e6cd570f5cfbdfbe7bf0c2ddc9b50b
puneet0191 <puneet.0191@gmail.com>
2019-07-15 13:07:07 +0530

    PMM-4329 UPdating Tests

--- tests/verifyMysqlOverviewDashboard_test.js
+++ tests/verifyMysqlOverviewDashboard_test.js
@@ -11,5 +11,6 @@ Scenario('Open the MySQL Overview Dashboard', async (I, adminPage, mysqlOverview
     await adminPage.navigateToDashboard("MySQL", "MySQL Overview");
     adminPage.applyTimer("1m");
     await adminPage.handleLazyLoading(10);
+    I.click(mysqlOverviewPage.fields.systemChartsToggle);
     mysqlOverviewPage.verifyMetricsExistence();
 });
