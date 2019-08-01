6655c04cd443de1350c7b0c5fc4b147724f589a4
puneet0191 <puneet.0191@gmail.com>
2019-08-01 17:47:25 +0530

    PMM-4309 Adding Tests for QAN

--- tests/pages/qanPage.js
+++ tests/pages/qanPage.js
@@ -1,12 +1,23 @@
 const I = actor();
-
+var assert = require('assert');
 module.exports = {
-
+    url: "graph/d/7w6Q3PJmz/pmm-query-analytics",
+    filterGroups: [
+        "Environment", "Cluster", "Replication Set", "Database", "Schema", "Server", "Client Host", "Availability Zone",
+        "Container Name", "Node Model", "Region Name", "Service Type", "User Name", "Machine ID"
+    ],
+    tableHeader: [
+        "Query", "Load", "Query Count", "Query Time"
+    ],
+    serverList: ["PMM Server PostgreSQL", "PGSQL_", "PXC_NODE", "mysql"],
     fields : {
-        table: "//app-qan-table//div[@class='ps-content']",
+        table: "//table/tr[2]",
+        detailsTable: "//app-details-table//app-details-row[1]",
         filter: "//app-qan-filter//div[@class='ps-content']",
         search: "//app-qan-search//input",
-        pagination: "//div[@class='pagination-tools']//ul[@class='ngx-pagination']",
+        pagination: "//ul[@role='navigation']",
+        nextPageNavigation: "//ul[@role='navigation']//li[last()]",
+        previousPageNavigation: "//ul[@role='navigation']//li[1]",
         pmmImage: "//footer/img",
         pmmVersion: "//footer/small",
         paginationArrow: "(//ul[@class='ngx-pagination']/li)[last()]",
@@ -16,34 +27,98 @@ module.exports = {
 
         results: "//ng-select[@ng-reflect-items='10,50,100']",
         fifty: "//div[@id='ad0800a556c8']/span",
-        hundred: "//div[@id='a305c6a9fc9e']"
+        hundred: "//div[@id='a305c6a9fc9e']",
+        iframe: "//div[@class='panel-content']//iframe"
     },
 
-    viewElementExistence() {
-        I.seeElement(this.fields.table);
-        I.seeElement(this.fields.filter);
-        I.seeElement(this.fields.search);
-        I.seeElement(this.fields.pagination);
-        I.seeElement(this.fields.pmmImage);
-        I.seeElement(this.fields.pmmVersion);
-        I.seeElement(this.fields.addColumn);
-        I.seeElement(this.fields.total);
-
-        I.seeElement(this.fields.columns + "[1]"); // load
-        I.seeElement(this.fields.columns + "[2]"); // count
-        I.seeElement(this.fields.columns + "[3]"); // time
+    filterGroupLocator (filterName) {
+        return "//div[@class='filter-group__title']//span[contains(text(), '" + filterName + "')]";
     },
 
-    checkForPagination() {
-        I.click(this.fields.paginationArrow);
+    tableHeaderLocator (tableHeader) {
+        return "//ng-select//span[contains(@class, 'ng-value-label') and contains(text(), '" + tableHeader + "')]";
+    },
+
+    checkPagination() {
+        I.waitForElement(this.fields.nextPageNavigation, 30);
+        I.click(this.fields.nextPageNavigation);
         I.wait(10);
-        I.seeElement(this.fields.columns + "[1]"); // load
+        I.waitForVisible(this.fields.nextPageNavigation, 30);
+        I.seeElement(this.fields.nextPageNavigation);
+        I.click(this.fields.previousPageNavigation);
+        I.waitForVisible(this.fields.previousPageNavigation, 30);
     },
 
-    // checkChangeNumResults() {
-    //     I.click(this.fields.results);
-    //     I.click(this.fields.fifty);
-    //     I.wait(20);
-    //     I.seeElement(this.fields.columns + "[1]"); // load
-    // }
+    checkServerList() {
+        I.click("//table//tr//th[2]//ng-select/div");
+        I.waitForElement("//table//tr//th[2]//ng-dropdown-panel//div//span[contains(text(), 'Server')]");
+        I.click("//table//tr//th[2]//ng-dropdown-panel//div//span[contains(text(), 'Server')]");
+        I.wait(5);
+        for (let i = 0; i< this.serverList.length; i++){
+            I.seeElement("//table/tr/td[2]//span[contains(text(), '" + this.serverList[i] + "')]");
+        }
+    },
+
+    checkTableHeaders() {
+        for (let i = 0; i< this.tableHeader.length; i++){
+            I.seeElement(this.tableHeaderLocator(this.tableHeader[i]));
+        }
+    },
+
+    checkSparkLines() {
+        I.seeNumberOfVisibleElements("//table//tr//app-qan-table-cell[1]//div[1]//div[1]", 11);
+    },
+
+    checkFilterGroups() {
+        for (let i = 0; i < this.filterGroups.length; i++) {
+            I.seeElement(this.filterGroupLocator(this.filterGroups[i]));
+        }
+    },
+
+    changeResultsPerPage(count) {
+        I.click("//ng-select[@ng-reflect-items='10,50,100']");
+        I.waitForVisible("//ng-select//span[contains(text(), '" + count + "')]", 30);
+        I.click("//ng-select//span[contains(text(), '" + count + "')]");
+        I.wait(10);
+        I.waitForVisible(this.fields.table, 30);
+        I.waitForVisible(this.filterGroupLocator(this.filterGroups[6]), 30);
+    },
+
+    applyFilter(filterValue){
+        I.click("//section[@class='aside__filter-group']//span[contains(text(), '" + filterValue + "')]/../span[@class='checkbox-container__checkmark']");
+        I.waitForVisible(this.fields.table, 30);
+    },
+
+    async _getData(row, column)
+    {
+        let percentage = await I.grabTextFrom("//table//tr[@ng-reflect-router-link='details/," + (row - 1) + "']//app-qan-table-cell[" + column +"]//div[1]//div[3]");
+        let value = await I.grabTextFrom("//table//tr[@ng-reflect-router-link='details/," + (row - 1) + "']//app-qan-table-cell[" + column +"]//div[1]//div[2]");
+
+        return {percentage: percentage, val: value};
+    },
+
+    async getDetailsData(row)
+    {
+        let percentage = await I.grabTextFrom("//app-details-table//app-details-row[" + row +"]//div[3]//span[2]");
+        let value = await I.grabTextFrom("//app-details-table//app-details-row[" + row +"]//div[3]//span[1]");
+        return {percentage: percentage, val: value};
+    },
+
+    _selectDetails(row) {
+        I.click("//table/tr["+ (row + 1) + "]//td[2]");
+        I.waitForVisible(this.fields.detailsTable, 30);
+        I.seeElement(this.fields.detailsTable);
+    },
+
+    async verifyDataSet(row){
+        var queryCountData = await this._getData(row, 2);
+        var queryTimeData = await this._getData(row, 3);
+        this._selectDetails(row);
+        var detailsQueryCountData = await this.getDetailsData(1);
+        var detailsQueryTimeData = await this.getDetailsData(2);
+        assert.equal(detailsQueryCountData.percentage.indexOf(queryCountData.percentage) > -1, true, "Details Query Count Percentage Doesn't Match");
+        assert.equal(detailsQueryCountData.val.indexOf(queryCountData.val) > -1, true, "Details Query Count Value Doesn't Match");
+        assert.equal(detailsQueryTimeData.percentage.indexOf(queryTimeData.percentage) > -1, true, "Details Query Time Percentage Doesn't Match");
+        assert.equal(detailsQueryTimeData.val.indexOf(queryTimeData.val) > -1, true, "Details Query Time value Doesn't Match");
+    }
 };
--- tests/verifyQAN_test.js
+++ tests/verifyQAN_test.js
@@ -1,4 +1,3 @@
-
 Feature("To verify and test the QAN Dashboard");
 
 Before((I, loginPage) => {
@@ -7,12 +6,20 @@ Before((I, loginPage) => {
 });
 
 Scenario('Open the QAN Dashboard and Test for Elements', async (I, adminPage, qanPage) => {
-    await adminPage.navigateToDashboard("QAN", "_PMM Query Analytics");
-
-    within({frame: "//div[@class='panel-content']//iframe"}, () => {
-        I.wait(10);
-        qanPage.viewElementExistence();
-        qanPage.checkForPagination();
-        // qanPage.checkChangeNumResults();
-    });
+    I.amOnPage(qanPage.url);
+    await I.waitForElement(qanPage.fields.iframe, 60);
+    await I.switchTo(qanPage.fields.iframe); // switch to first iframe
+    I.wait(10);
+    qanPage.changeResultsPerPage(50);
+    qanPage.checkFilterGroups();
+    qanPage.applyFilter("mysql");
+    await qanPage.verifyDataSet(1);
+    await qanPage.verifyDataSet(2);
+    qanPage.applyFilter("mysql");
+    qanPage.changeResultsPerPage(10);
+    qanPage.checkSparkLines();
+    qanPage.checkPagination();
+    qanPage.checkTableHeaders();
+    qanPage.checkServerList();
+    I.switchTo();
 });
