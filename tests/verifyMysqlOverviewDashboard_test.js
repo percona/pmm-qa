ee046cd23427816110befba8054a3f0739114262
Kartikeya Gokhale <kgkartikeyagokhale@gmail.com>
2019-08-12 13:04:26 +0530

    Verifying various dashboards in MySQL Overview

--- tests/verifyMysqlOverviewDashboard_test.js
+++ tests/verifyMysqlOverviewDashboard_test.js
@@ -10,7 +10,19 @@ Scenario('Open the MySQL Overview Dashboard', async (I, adminPage, mysqlOverview
     I.amOnPage(mysqlOverviewPage.url);
     I.waitForElement(adminPage.fields.metricTitle, 30);
     adminPage.applyTimer("1m");
-    await adminPage.handleLazyLoading(10);
-    I.click(mysqlOverviewPage.fields.systemChartsToggle);
-    mysqlOverviewPage.verifyMetricsExistence();
+
+    // Looping through all dashboards
+    let i=1;
+    for(; i<=5; i++) {
+        // Selecting a dashboards
+        I.click("(//a[@class='variable-value-link'])[6]");
+        I.click("//value-select-dropdown/div/div/div/div/a[" + i + "]");
+        I.wait(3);
+
+        // Checking each dashboard
+        await adminPage.handleLazyLoading(10);
+        I.click(mysqlOverviewPage.fields.systemChartsToggle);
+        I.pressKey('Home');
+        mysqlOverviewPage.verifyMetricsExistence();
+    }
 });
