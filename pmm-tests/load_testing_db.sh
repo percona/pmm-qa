1b00b3845ce3704c73d63212e8d8ee498ece2f88
puneet0191 <puneet.0191@gmail.com>
2019-09-13 14:46:05 +0530

    PMM-4255 Minor change in Load testing Script

--- pmm-tests/load_testing_db.sh
+++ pmm-tests/load_testing_db.sh
@@ -12,10 +12,10 @@ for k in `seq 1 ${Nodes}`;do
         if [[ "${DB}" == "ms" && "${ACTION}" != "delete" ]]; then
             if [ $(( ${k} % 2 )) -eq 0 ]; then	
                 echo "Adding Slow Log";
-                pmm-admin add mysql --use-slowlog --username=msandbox --password=msandbox --environment=slow-log-test ${MYSQL_IP_PORT} ms_sl_lt_${k}
+                pmm-admin add mysql --query-source=slowlog --username=msandbox --password=msandbox --environment=slow-log-test ${MYSQL_IP_PORT} ms_sl_lt_${k}
             else
                 echo "Adding Performance Schema";
-                pmm-admin add mysql --use-perfschema --username=msandbox --password=msandbox --environment=performance-schema-test ${MYSQL_IP_PORT} ms_ps_lt_${k}
+                pmm-admin add mysql --query-source=slowlog --username=msandbox --password=msandbox --environment=performance-schema-test ${MYSQL_IP_PORT} ms_ps_lt_${k}
             fi
         fi
         if [[ "${DB}" == "ms" && "${ACTION}" == "delete" ]]; then
