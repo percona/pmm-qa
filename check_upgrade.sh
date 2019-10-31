baf22cd1916a108ce1a06530376972250e9aea49
puneet0191 <puneet.0191@gmail.com>
2019-10-31 12:44:27 +0530

    PMM-4309 Upgrade Script Changes

--- check_upgrade.sh
+++ check_upgrade.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 #check for packages after upgrade
-docker exec $1 rpm -qa | grep percona-qan-app-$2
-docker exec $1 rpm -qa | grep percona-qan-api2-$2
-docker exec $1 rpm -qa | grep percona-dashboards-$2
-docker exec $1 rpm -qa | grep pmm-update-$2
-docker exec $1 rpm -qa | grep pmm-server-$2
-docker exec $1 rpm -qa | grep pmm-managed-$2
-docker exec $1 rpm -qa | grep pmm2-client-$2
+export PMM_SERVER_DOCKER_CONTAINER=$(docker ps | grep 'pmm-server' | awk '{print $12}')
+echo $PMM_SERVER_DOCKER_CONTAINER
+docker exec $PMM_SERVER_DOCKER_CONTAINER rpm -qa | grep percona-qan-app-$1
+docker exec $PMM_SERVER_DOCKER_CONTAINER rpm -qa | grep percona-qan-api2-$1
+docker exec $PMM_SERVER_DOCKER_CONTAINER rpm -qa | grep percona-dashboards-$1
+docker exec $PMM_SERVER_DOCKER_CONTAINER rpm -qa | grep pmm-update-$1
+docker exec $PMM_SERVER_DOCKER_CONTAINER rpm -qa | grep pmm-server-$1
+docker exec $PMM_SERVER_DOCKER_CONTAINER rpm -qa | grep pmm-managed-$1
+docker exec $PMM_SERVER_DOCKER_CONTAINER rpm -qa | grep pmm2-client-$1
