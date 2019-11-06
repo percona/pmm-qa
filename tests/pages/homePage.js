8f5c24c50198771fa8e69aa3f5b729123655356b
puneet0191 <puneet.0191@gmail.com>
2019-11-06 14:21:49 +0530

    PMM-4309 Updating timeout

--- tests/pages/homePage.js
+++ tests/pages/homePage.js
@@ -25,12 +25,12 @@ module.exports = {
     async upgradePMM(){
         I.click(this.fields.checkUpdateButton);
         I.wait(5);
-        I.waitForElement(this.fields.triggerUpdate, 90);
+        I.waitForElement(this.fields.triggerUpdate, 180);
         I.seeElement(this.fields.triggerUpdate);
         I.click(this.fields.triggerUpdate);
         I.waitForElement(this.fields.updateProgressModal, 30);
         I.waitForText("Update in progress", 30, this.fields.updateProgressModal);
-        I.waitForText("Successfully updated", 600, this.fields.updateProgressModal);
+        I.waitForText("Successfully updated", 1200, this.fields.updateProgressModal);
         I.click(this.fields.reloadButtonAfterUpgrade);
         I.wait(10);
         I.refreshPage();
