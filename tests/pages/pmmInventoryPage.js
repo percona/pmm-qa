4c7c79cf28d69858e1128c131c2a799f42e4076c
puneet0191 <puneet.0191@gmail.com>
2019-12-20 11:45:07 +0530

    PMM-5067 Updating Remote Instance Locator

--- tests/pages/pmmInventoryPage.js
+++ tests/pages/pmmInventoryPage.js
@@ -12,6 +12,8 @@ module.exports = {
         inventoryTableColumn: "//table//td",
         agentsLink: "//div[@role='tab'][contains(text(),'Agents')]",
         nodesLink: "//div[@role='tab'][contains(text(),'Nodes')]",
+        agentsLinkOld: "//a[contains(text(), 'Agents')]",
+        nodesLinkOld: "//a[contains(text(), 'Nodes')]",
         pmmAgentLocator: "//table//td[contains(text(), 'PMM Agent')]"
     },
 
@@ -31,6 +33,21 @@ module.exports = {
         return currentServiceId;
     },
 
+    async getServiceIdOld (serviceName) {
+        let totalServices = await I.grabNumberOfVisibleElements(this.fields.inventoryTableRows);
+        var matchedServices = 0;
+        for (var i = 2; i <= totalServices; i++)
+        {
+            var currentServiceName = await I.grabTextFrom(this.fields.inventoryTableRows + "[" + i + "]/" + "td[3]");
+            if (serviceName == currentServiceName) {
+                var currentServiceId = await I.grabTextFrom(this.fields.inventoryTableRows + "[" + i + "]/" + "td[1]");
+                matchedServices += 1;
+            }
+        }
+        assert.equal(matchedServices, 1, "Their must only be one entry for the newly added service with name " + serviceName );
+        return currentServiceId;
+    },
+
     async checkAgentStatus (serviceId) {
         I.click(this.fields.agentsLink);
         I.waitForElement(this.fields.pmmAgentLocator, 60);
@@ -39,6 +56,25 @@ module.exports = {
         let totalAgents = await I.grabNumberOfVisibleElements(this.fields.inventoryTableRows);
         var agentMatched = 0;
         for (var i = 1; i < totalAgents; i++)
+        {
+            var agentForServiceId = await I.grabNumberOfVisibleElements(this.fields.inventoryTableRows + "[" + i + "]/" + "td[3]//span[contains(text(), '" + serviceId + "')]");
+            if (agentForServiceId == 1){
+                agentMatched += 1;
+                var statusIsRunning = await I.grabNumberOfVisibleElements(this.fields.inventoryTableRows + "[" + i + "]/" + "td[3]//span[contains(text(), 'status: RUNNING')]" );
+                assert.equal(statusIsRunning, 1, "status for serviceId " + serviceId + " is not equal to Running, hence failing");
+            }
+        }
+        assert.equal(agentMatched, 2, " Service ID must have only 2 Agents running for different services" + serviceId );
+    },
+
+    async checkAgentStatusOld (serviceId) {
+        I.click(this.fields.agentsLinkOld);
+        I.waitForElement(this.fields.pmmAgentLocator, 60);
+        adminPage.peformPageDown(5);
+        I.waitForElement(this.fields.inventoryTable, 60);
+        let totalAgents = await I.grabNumberOfVisibleElements(this.fields.inventoryTableRows);
+        var agentMatched = 0;
+        for (var i = 2; i <= totalAgents; i++)
         {
             var agentForServiceId = await I.grabNumberOfVisibleElements(this.fields.inventoryTableRows + "[" + i + "]/" + "td[3]//span[contains(text(), '" + serviceId + "')]");
             if (agentForServiceId == 1){
