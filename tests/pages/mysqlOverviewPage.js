fb3cf41ab2ed763636db18c4951780cd8ed2482c
yurkovychv <vasyl.yurkovych@percona.com>
2020-02-19 16:53:39 +0200

    PMM- 5067 added methods for taking screenshot for each graph

--- tests/pages/mysqlOverviewPage.js
+++ tests/pages/mysqlOverviewPage.js
@@ -1,4 +1,4 @@
-const I = actor();
+const {I, mysqlOverviewPage} = inject();
 
 module.exports = {
 
@@ -9,7 +9,13 @@ module.exports = {
     fields: {
         pageHeaderText: "MySQL Overview",
         notAvailableMetrics: "//span[contains(text(), 'N/A')]",
-        systemChartsToggle: "//a[contains(text(), 'Node Summary')]"
+        systemChartsToggle: "//a[contains(text(), 'Node Summary')]",
+        graphId:"//div[contains(@id, 'panel')][div[@class='panel-height-helper']]",
+        graphLegend:"//div[@class='graph-legend']",
+        panelTitle:"//span[@class='panel-title-text']",
+        panelContent: "//div[contains(@class, 'panel-content')]/ng-transclude[@class='panel-height-helper']",
+        graphTooltipTime: "//div[@class='graph-tooltip-time']",
+        dashboardName: "mysql_instance_summary_"
     },
     metrics: ["MySQL Uptime", "Current QPS", "InnoDB Buffer Pool Size", "Buffer Pool Size of Total RAM",
         "MySQL Connections", "MySQL Client Thread Activity", "MySQL Questions", "MySQL Thread Cache",
@@ -29,5 +35,50 @@ module.exports = {
             I.seeElement(this.graphsLocator(this.metrics[i]));
         }
         I.dontSeeElement(this.fields.notAvailableMetrics);
+    },
+
+    async grabPanelIdAttribute() {
+        let panelId = await I.grabAttributeFrom(this.fields.graphId, 'id');
+        return panelId;
+    },
+
+    async getIds() {
+        let panelId = await this.grabPanelIdAttribute();
+        console.log(panelId);
+        let ids = [];
+        for (let i = 0; i < panelId.length; i++) {
+            ids.push(panelId[i].slice(6));
+        };
+        console.log(ids);
+        return ids;
+    },
+
+    async waitForGraphLoaded() {
+        await I.waitForVisible(this.fields.panelTitle, 30);
+        await I.waitForVisible(this.fields.panelContent, 30);
+    },
+
+    async checkForGraph() {
+        if (await I.grabNumberOfVisibleElements(`${this.fields.panelContent}//canvas`) == 1) {
+            I.wait(1);
+        } else {
+            console.log("no Graph");
+        }
+    },
+
+    async createEachPanelScreenshot() {
+        let ids = await this.getIds();
+        ids.shift();
+        console.log(ids);
+        let currentURL = await I.grabCurrentUrl();
+        for (let i = 0; i < ids.length; i++) {
+            I.amOnPage(`${currentURL}&fullscreen&panelId=${ids[i]}`);
+            await this.waitForGraphLoaded();
+            await this.checkForGraph();
+            let title = await I.grabTextFrom(this.fields.panelTitle);
+            let screenshotName = this.fields.dashboardName + title.toString().toLowerCase().replace(' ','_') + ".png";
+            I.saveScreenshot(screenshotName);
+        }
+
     }
 }
