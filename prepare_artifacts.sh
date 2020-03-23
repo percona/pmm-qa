a2c33249df145f6893eb2c75344cdfebe4d58b79
puneet0191 <puneet.0191@gmail.com>
2020-03-23 11:15:42 +0530

    PMM-5584 Move artifcats to right location

--- prepare_artifacts.sh
+++ prepare_artifacts.sh
@@ -4,14 +4,14 @@
 chmod 777 -R video/
 
 ### Create Video output directory in tests folder
-mkdir -p ./tests/output/video
+mkdir -p ./pmm-app/tests/output/video
 
 ### Go through failed test files in output_folder, move videos for failing tests.
 
-for file in ./tests/output/parallel_chunk*/selenoid/*.json
+for file in ./pmm-app/tests/output/parallel_chunk*/selenoid/*.json
 do
   if [ -f "$file" ]; then
-    mv video/$(cat ${file} | jq -r '.sessionid').mp4 tests/output/video/$(cat ${file} | jq -r '.testName' | sed -e 's/ /_/g').mp4
+    mv video/$(cat ${file} | jq -r '.sessionid').mp4 pmm-app/tests/output/video/$(cat ${file} | jq -r '.testName' | sed -e 's/ /_/g').mp4
   fi
 done
 
