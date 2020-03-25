7f214de2f243af393c0988e7085c3c0919e474f5
yurkovychv <vasyl.yurkovych@percona.com>
2020-03-25 15:36:03 +0200

    PMM-5362 fixing failing tests changed test names

--- tests/pages/dashboardPage.js
+++ tests/pages/dashboardPage.js
@@ -13,7 +13,7 @@ module.exports = {
             "Network Traffic", "Network Utilization Hourly", "Load Average", "I/O Activity"]
     },
     advancedDataExplorationDashboard: {
-        url: "/graph/d/1oz9QMHmk/advanced-data-exploration?orgId=1&refresh=1m&var-metric=go_gc_duration_seconds",
+        url: "graph/d/1oz9QMHmk/advanced-data-exploration?orgId=1&refresh=1m&var-metric=go_gc_duration_seconds",
         metrics: ["View Actual Metric Values (Gauge)", "View Metric Rate of Change (Counter)", "Metric Rates",
             "Metric Data Table"],
     },
