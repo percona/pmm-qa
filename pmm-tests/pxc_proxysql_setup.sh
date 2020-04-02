9449d255d9a373ace13ca1e1f47171590f3b4cfc
puneet0191 <puneet.0191@gmail.com>
2020-04-02 18:28:25 +0530

    PMM-5629 Minor fix

--- pmm-tests/pxc_proxysql_setup.sh
+++ pmm-tests/pxc_proxysql_setup.sh
@@ -6,11 +6,11 @@ sudo yum install -y socat
 wget https://raw.githubusercontent.com/Percona-QA/percona-qa/master/pxc-tests/pxc-startup.sh
 
 ## Download right PXC version
-if [[ "$pxc_version" != "5.7" ]]; then
+if [ "$pxc_version" == "5.7" ]; then
 	wget https://www.percona.com/downloads/Percona-XtraDB-Cluster-57/Percona-XtraDB-Cluster-5.7.28-31.41/binary/tarball/Percona-XtraDB-Cluster-5.7.28-rel31-31.41.1.Linux.x86_64.ssl101.tar.gz
 	sudo yum install -y percona-xtrabackup-24
 fi
-if [[ "$pxc_version" != "8.0" ]]; then
+if [ "$pxc_version" == "8.0" ]; then
 	sed -i 's+wsrep_node_incoming_address=$ADDR+wsrep_node_incoming_address=$ADDR:$RBASE1+g' pxc-startup.sh
 	wget https://www.percona.com/downloads/Percona-XtraDB-Cluster-80/Percona-XtraDB-Cluster-8.0.18-9.1.rc/binary/tarball/Percona-XtraDB-Cluster_8.0.18.9_Linux.x86_64.el7.tar.gz	
 fi
