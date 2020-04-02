2c7a6bb79ba6455400b5e7ea34c91ace7ce9d347
Puneet Kala <puneet.0191@gmail.com>
2020-04-02 18:35:25 +0530

    PMM-5629 Minor version check fix (#266)
    * PMM-5629 PXC 8.x support
    
    * PMM-5629 Adding minor changes, fixing failures
    
    * PMM-5629 minor fix
    
    * PMM-5629 Minor Fix
    
    * PMM-5629 Adding username support
    
    * PMM-5629 Minor
    
    * PMM-5629 Support for 5.7 pxc with proxysql
    
    * PMM-5629 Trying workload scripts with pxc nodes
    
    * PMM-5629 Slow log changes
    
    * PMM-5629 Minor fix
    
    * PMM-5629 enable sysbench also

--- pmm-tests/pxc_proxysql_setup.sh
+++ pmm-tests/pxc_proxysql_setup.sh
@@ -6,11 +6,11 @@ sudo yum install -y socat
 wget https://raw.githubusercontent.com/Percona-QA/percona-qa/master/pxc-tests/pxc-startup.sh
 
 ## Download right PXC version
-if [[ "$pxc_version" != "5.7" ]]; then
+if [ "$pxc_version" == "5.7" ]; then
 	wget https://www.percona.com/downloads/Percona-XtraDB-Cluster-57/Percona-XtraDB-Cluster-5.7.28-31.41/binary/tarball/Percona-XtraDB-Cluster-5.7.28-rel31-31.41.1.Linux.x86_64.ssl101.tar.gz
 	sudo yum install -y percona-xtrabackup-24
 fi
-if [[ "$pxc_version" != "8.0" ]]; then
+if [ "$pxc_version" == "8.0" ]; then
 	sed -i 's+wsrep_node_incoming_address=$ADDR+wsrep_node_incoming_address=$ADDR:$RBASE1+g' pxc-startup.sh
 	wget https://www.percona.com/downloads/Percona-XtraDB-Cluster-80/Percona-XtraDB-Cluster-8.0.18-9.1.rc/binary/tarball/Percona-XtraDB-Cluster_8.0.18.9_Linux.x86_64.el7.tar.gz	
 fi
@@ -34,6 +34,8 @@ do
 	bin/mysql -A -uroot -Snode$j/socket.sock -e "SET GLOBAL slow_query_log='ON';"
 	bin/mysql -A -uroot -Snode$j/socket.sock -e "SET GLOBAL long_query_time=0;"
 	bin/mysql -A -uroot -Snode$j/socket.sock -e "SET GLOBAL log_slow_rate_limit=1;"
+	bin/mysql -A -uroot -Snode$j/socket.sock -e "SET GLOBAL log_slow_verbosity='full';"
+	bin/mysql -A -uroot -Snode$j/socket.sock -e "SET GLOBAL log_slow_rate_type='query';"
 done
 
 bin/mysql -A -uroot -Snode1/socket.sock -e "create user admin@localhost identified with mysql_native_password by 'admin';"
@@ -51,7 +53,7 @@ sleep 20
 sudo proxysql-admin -e
 
 ## Start Running Load
-##sysbench /usr/share/sysbench/oltp_insert.lua --mysql-db=sbtest --mysql-user=sysbench --mysql-socket=node1/socket.sock --mysql-password=test --db-driver=mysql --threads=5 --tables=10 --table-size=1000 prepare > sysbench_run_node1_prepare.txt 2>&1 &
-##sleep 20
-##sysbench /usr/share/sysbench/oltp_read_only.lua --mysql-db=sbtest --mysql-user=sysbench --mysql-socket=node1/socket.sock --mysql-password=test --db-driver=mysql --threads=5 --tables=10 --table-size=1000 --time=6000 run > sysbench_run_node1_read_only.txt 2>&1 &
-##sysbench /usr/share/sysbench/oltp_read_write.lua --mysql-db=sbtest --mysql-user=sysbench --mysql-socket=node1/socket.sock --mysql-password=test --db-driver=mysql --threads=5 --tables=10 --table-size=1000 --time=6000 run > sysbench_run_node1_read_write.txt 2>&1 &
+sysbench /usr/share/sysbench/oltp_insert.lua --mysql-db=sbtest --mysql-user=sysbench --mysql-socket=node1/socket.sock --mysql-password=test --db-driver=mysql --threads=5 --tables=10 --table-size=1000 prepare > sysbench_run_node1_prepare.txt 2>&1 &
+sleep 20
+sysbench /usr/share/sysbench/oltp_read_only.lua --mysql-db=sbtest --mysql-user=sysbench --mysql-socket=node1/socket.sock --mysql-password=test --db-driver=mysql --threads=5 --tables=10 --table-size=1000 --time=12000 run > sysbench_run_node1_read_only.txt 2>&1 &
+sysbench /usr/share/sysbench/oltp_read_write.lua --mysql-db=sbtest --mysql-user=sysbench --mysql-socket=node1/socket.sock --mysql-password=test --db-driver=mysql --threads=5 --tables=10 --table-size=1000 --time=12000 run > sysbench_run_node1_read_write.txt 2>&1 &
