005ef9a991df8e87bf5a55c26415510f47d565e4
Puneet Kala <puneet.0191@gmail.com>
2020-05-02 15:33:09 +0530

    PMM-5362 Minor fix for service (#283)
    * Update version
    
    * Fix Failing test
    
    * Fixing Failing Tests
    
    * PXC_8.0 Official Release Tarball link
    
    * PMM-5362 Fixing Test, Removing Old confusion
    
    * PMM-5362 Minor style
    
    * PMM-5362 Fixing Test Failure
    
    * PMM-5362 Fixing Failing Tests
    
    * PMM-5362 fix test
    
    * PMM-5362

--- tests/pages/pmmInventoryPage.js
+++ tests/pages/pmmInventoryPage.js
@@ -40,7 +40,7 @@ module.exports = {
         I.waitForElement(this.fields.pmmAgentLocator, 60);
         I.waitForElement(this.fields.inventoryTable, 60);
         let numberOfServices = await I.grabNumberOfVisibleElements("//span[contains(text(), '" + serviceId + "')]/following-sibling::span[contains(text(),'status: RUNNING')]");
-        if( service_name === "mongodb_remote_new" || service_name === "postgresql_remote_new" || service_name === "mysql_remote_new" ){
+        if( service_name === "rds-mysql56" || service_name === "mongodb_remote_new" || service_name === "postgresql_remote_new" || service_name === "mysql_remote_new" ){
             assert.equal(numberOfServices, 2, " Service ID must have only 2 Agents running for different services" + serviceId);
         } else {
             assert.equal(numberOfServices, 1, " Service ID must have only 1 Agent running" + serviceId);
