9e09baa02100a3c816e0c05e78c4a8cf127413b5
Puneet Kala <puneet.0191@gmail.com>
2021-01-19 21:50:52 +0530

    PMM-7304 Addressing comments

--- pmm-tests/minikube_operators_setup.sh
+++ pmm-tests/minikube_operators_setup.sh
@@ -3,6 +3,7 @@
 # Prepare a set of base64 encoded values and non encoded for user and pass with administrator privileges to pmm-server (DBaaS)
 PMM_USER='admin';
 PMM_PASS='admin';
+OPERATOR_VERSION=$1;
 
 PMM_USER_B64="$(echo -n "${PMM_USER}" | base64)";
 PMM_PASS_B64="$(echo -n "${PMM_PASS}" | base64)";
@@ -12,5 +13,5 @@ curl -sSf -m 30 https://raw.githubusercontent.com/percona/percona-xtradb-cluster
 curl -sSf -m 30 https://raw.githubusercontent.com/percona/percona-xtradb-cluster-operator/pmm-branch/deploy/secrets.yaml | sed "s/pmmserver:.*=/pmmserver: ${PMM_PASS_B64}/g" | minikube kubectl -- apply -f -
 
 # Install the PSMDB operator
-curl -sSf -m 30 https://raw.githubusercontent.com/percona/percona-server-mongodb-operator/v1.6.0/deploy/bundle.yaml | minikube kubectl -- apply -f -
-curl -sSf -m 30 https://raw.githubusercontent.com/percona/percona-server-mongodb-operator/v1.6.0/deploy/secrets.yaml | sed "s/PMM_SERVER_USER:.*$/PMM_SERVER_USER: ${PMM_USER}/g;s/PMM_SERVER_PASSWORD:.*$/PMM_SERVER_PASSWORD: ${PMM_PASS}/g;" | minikube kubectl -- apply -f -
+curl -sSf -m 30 https://raw.githubusercontent.com/percona/percona-server-mongodb-operator/v${OPERATOR_VERSION}/deploy/bundle.yaml | minikube kubectl -- apply -f -
+curl -sSf -m 30 https://raw.githubusercontent.com/percona/percona-server-mongodb-operator/v${OPERATOR_VERSION}/deploy/secrets.yaml | sed "s/PMM_SERVER_USER:.*$/PMM_SERVER_USER: ${PMM_USER}/g;s/PMM_SERVER_PASSWORD:.*$/PMM_SERVER_PASSWORD: ${PMM_PASS}/g;" | minikube kubectl -- apply -f -
