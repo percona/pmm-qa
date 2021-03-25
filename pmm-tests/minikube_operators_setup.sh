2c51f7d1b81b88d5aa8823a3fc852edc1e9b6137
Puneet Kala <puneet.0191@gmail.com>
2021-03-25 13:19:03 +0530

    Prepare for Operator Version 1.7.0 PXC (#381)

--- pmm-tests/minikube_operators_setup.sh
+++ pmm-tests/minikube_operators_setup.sh
@@ -15,15 +15,15 @@ PMM_USER_B64="$(echo -n "${PMM_USER}" | base64)";
 PMM_PASS_B64="$(echo -n "${PMM_PASS}" | base64)";
 
 # Install the PXC operator
-curl -sSf -m 30 https://raw.githubusercontent.com/percona/percona-xtradb-cluster-operator/pmm-branch/deploy/bundle.yaml \
+curl -sSf -m 30 https://raw.githubusercontent.com/percona/percona-xtradb-cluster-operator/v${OPERATOR_VERSION}/deploy/bundle.yaml \
 | minikube kubectl -- apply -f -
-curl -sSf -m 30 https://raw.githubusercontent.com/percona/percona-xtradb-cluster-operator/pmm-branch/deploy/secrets.yaml \
-| sed "s/pmmserver:.*=/pmmserver: ${PMM_PASS_B64}/g" \
+curl -sSf -m 30 https://raw.githubusercontent.com/percona/percona-xtradb-cluster-operator/v${OPERATOR_VERSION}/deploy/secrets.yaml \
+| sed "s/pmmserver:.*=/pmmserver: ${PMM_PASS}/g" \
 | minikube kubectl -- apply -f -
 
 # Install the PSMDB operator
-curl -sSf -m 30 https://raw.githubusercontent.com/percona/percona-server-mongodb-operator/v${OPERATOR_VERSION}/deploy/bundle.yaml \
+curl -sSf -m 30 https://raw.githubusercontent.com/percona/percona-server-mongodb-operator/v1.6.0/deploy/bundle.yaml \
 | minikube kubectl -- apply -f -
-curl -sSf -m 30 https://raw.githubusercontent.com/percona/percona-server-mongodb-operator/v${OPERATOR_VERSION}/deploy/secrets.yaml \
+curl -sSf -m 30 https://raw.githubusercontent.com/percona/percona-server-mongodb-operator/v1.6.0/deploy/secrets.yaml \
 | sed "s/PMM_SERVER_USER:.*$/PMM_SERVER_USER: ${PMM_USER}/g;s/PMM_SERVER_PASSWORD:.*$/PMM_SERVER_PASSWORD: ${PMM_PASS}/g;" \
 | minikube kubectl -- apply -f -
