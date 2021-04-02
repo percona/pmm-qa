bb8f766a093f506e4c7052f0329c2c590082e83a
Puneet Kala <puneet.0191@gmail.com>
2021-04-02 16:02:09 +0530

    Client exporter checks (#387)
    * Check Agent Status and Exporters
    
    * Update check_client_upgrade.sh

---
+++ pmm-tests/check_client_upgrade.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#check for exporters and agents after client_upgrade on upgrade job.
+
+pmm-admin status | grep -q Running
+pmm-admin status | grep node_exporter | grep -qv Waiting
+pmm-admin status | grep vmagent | grep -qv Waiting
+pmm-admin status | grep mysqld_exporter | grep -qv Waiting
+pmm-admin status | grep mysql_perfschema_agent | grep -qv Waiting
+pmm-admin status | grep Version | grep -q $1
+ls -la /usr/local/percona/pmm2/exporters | grep -q azure_exporter
+ls -la /usr/local/percona/pmm2/exporters | grep -q mongodb_exporter
+ls -la /usr/local/percona/pmm2/exporters | grep -q mysqld_exporter
+ls -la /usr/local/percona/pmm2/exporters | grep -q node_exporter
+ls -la /usr/local/percona/pmm2/exporters | grep -q postgres_exporter
+ls -la /usr/local/percona/pmm2/exporters | grep -q proxysql_exporter
+ls -la /usr/local/percona/pmm2/exporters | grep -q rds_exporter
+ls -la /usr/local/percona/pmm2/exporters | grep -q vmagent
