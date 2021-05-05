58951d6b25758e4672ec406dc817b6a322c3ad06
VojtaK <vojtech.koval@sde.cz>
2021-05-05 18:02:25 +0200

    remove 5.6 option

--- install_backup_toolkit.sh
+++ install_backup_toolkit.sh
@@ -6,8 +6,6 @@ if [[ \$msql_version == '5.7' ]]; then
     sudo yum install percona-xtrabackup-24  qpress -y
 elif [[ \$msql_version == '8.0' ]]; then
     sudo yum install percona-xtrabackup80  qpress -y
-elif [[ \$msql_version == '5.6' ]]; then
-    sudo yum install percona-xtrabackup-56  qpress -y
 else
     echo "ERROR! You have selected non supported combination of version MySQL & XtraBackup"
     exit 1
