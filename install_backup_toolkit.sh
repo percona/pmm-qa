a3702347c32f61f16daa28617cdf9ca97ce9bb27
VojtaK <vojtech.koval@sde.cz>
2021-05-05 13:08:49 +0200

    add-pre_requirements needed for yum package installation

--- install_backup_toolkit.sh
+++ install_backup_toolkit.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 msql_version=$1
+sudo percona-release enable-only tools release;
+sudo yum install https://repo.percona.com/yum/percona-release-latest.noarch.rpm
 if [[ \$msql_version == '5.7' ]]; then
     sudo yum install percona-xtrabackup-24  qpress -y
 elif [[ \$msql_version == '8.0' ]]; then
-    sudo yum install percona-xtrabackup-80  qpress -y
+    sudo yum install percona-xtrabackup80  qpress -y
+elif [[ \$msql_version == '5.6' ]]; then
+    sudo yum install percona-xtrabackup-56  qpress -y
 else
     echo "ERROR! You have selected non supported combination of version MySQL & XtraBackup"
     exit 1
