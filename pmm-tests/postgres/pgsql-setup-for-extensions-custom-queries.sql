154e20d039f02426354c76d84abc0ddf6c59cc94
Puneet Kala <puneet.0191@gmail.com>
2021-09-03 17:28:31 +0530

    PMM-8625 adding dummy queries for pgsql (#421)
    * PMM-8625 Adding option for Postgresql ssl
    
    * PMM-8625 fix the default branch name
    
    * PMM-8625 change path for cert files
    
    * PMM-8625 Handle crash for mysql sandboxs
    
    * PMM-8625 Fix task
    
    * PMM-8625 Adding host option
    
    * PMM-8625 get docker logs
    
    * PMM-8625 Adding support for postgresql custom metrics
    
    * PMM-8625 Trying sample queries for postgres
    
    * PMM-8625 fix error in sql file

---
+++ pmm-tests/postgres/pgsql-setup-for-extensions-custom-queries.sql
@@ -0,0 +1,12 @@
+create extension pg_stat_statements;
+ALTER SYSTEM SET track_io_timing=ON;
+SELECT pg_reload_conf();
+create table dummy_table(name varchar(20),address text,age int);
+insert into dummy_table values('XYZ','location-A',25);
+insert into dummy_table values('ABC','location-B',35);
+insert into dummy_table values('DEF','location-C',40);
+insert into dummy_table values('PQR','location-D',54);
+select * from dummy_table;
+update dummy_table set age=50 where name='PQR';
+select * from dummy_table;
+delete from dummy_table where age=50;
