d49bebeddc5e8674c92eb331a45be7b397158670
puneet0191 <puneet.0191@gmail.com>
2021-09-10 19:15:33 +0530

    PMM-8625 Change branch name to use default

--- pmm-tests/pmm-framework.sh
+++ pmm-tests/pmm-framework.sh
@@ -2610,7 +2610,7 @@ setup_postgres_ssl () {
   mkdir -p /tmp/ssl || :
   pushd /tmp/ssl
   if [ ! -d "pmm-ui-tests" ]; then
-    git clone --branch PMM-8625 https://github.com/percona/pmm-ui-tests
+    git clone https://github.com/percona/pmm-ui-tests
   fi
   sudo chown -R $USER:$USER pmm-ui-tests
   pushd pmm-ui-tests
@@ -2628,7 +2628,7 @@ setup_remote_db_docker_compose () {
   mkdir -p /tmp/remote_db || :
   pushd /tmp/remote_db
   if [ ! -d "pmm-ui-tests" ]; then
-    git clone --branch PMM-8625 https://github.com/percona/pmm-ui-tests
+    git clone https://github.com/percona/pmm-ui-tests
   fi
   pushd pmm-ui-tests
   PWD=$(pwd) docker-compose -f docker-compose-ami-db-setup.yml up -d
