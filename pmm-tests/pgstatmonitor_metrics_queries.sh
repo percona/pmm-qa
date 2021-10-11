aab2c018683cce90ee60e2033e84984c05d39434
Puneet Kala <puneet.0191@gmail.com>
2021-10-11 19:59:58 +0530

    PMM-8300 create directory

--- pmm-tests/pgstatmonitor_metrics_queries.sh
+++ pmm-tests/pgstatmonitor_metrics_queries.sh
@@ -3,6 +3,7 @@
 export PDPGSQL_CONTAINER=$1
 git clone https://github.com/percona/pg_stat_monitor
 db=1
+docker exec ${PDPGSQL_CONTAINER} mkdir /tmp/sql
 pushd pg_stat_monitor/regression/sql/
 for filename in *.sql; do
     docker exec ${PDPGSQL_CONTAINER} bash -c "psql -h localhost -U postgres -c 'create database test${db}'"
