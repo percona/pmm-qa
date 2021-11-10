c689b528d98eda6ba7128a2a8780a77bc3ab2baf
Puneet Kala <puneet.0191@gmail.com>
2021-11-10 13:26:35 +0530

    PMM-T440 fixing plugin version check

--- pmm-tests/check_upgrade.sh
+++ pmm-tests/check_upgrade.sh
@@ -9,6 +9,7 @@ if [ $3 == "ami" ]; then
 	rpm -qa | grep pmm-managed-$1
 	rpm -qa | grep pmm2-client-$1
 	grafana-cli plugins ls | grep alexanderzobnin-zabbix-app
+	grafana-cli plugins ls | grep "vertamedia-clickhouse-datasource @ 2.3.1"
 
 	if [ $2 == "post" ]; then
 		rpm -qa | grep dbaas-controller-$1
@@ -24,8 +25,7 @@ else
 	docker exec $PMM_SERVER_DOCKER_CONTAINER rpm -qa | grep pmm2-client-$1
 	if [ $2 == "post" ]; then
 		docker exec $PMM_SERVER_DOCKER_CONTAINER rpm -qa | grep dbaas-controller-$1
-		if [[ $PERFORM_DOCKER_WAY_UPGRADE != "yes" ]]; then
-			docker exec -e GF_PLUGIN_DIR=/srv/grafana/plugins/ $PMM_SERVER_DOCKER_CONTAINER grafana-cli plugins ls | grep alexanderzobnin-zabbix-app
-		fi
+		docker exec -e GF_PLUGIN_DIR=/srv/grafana/plugins/ $PMM_SERVER_DOCKER_CONTAINER grafana-cli plugins ls | grep alexanderzobnin-zabbix-app
+		docker exec -e GF_PLUGIN_DIR=/srv/grafana/plugins/ $PMM_SERVER_DOCKER_CONTAINER grafana-cli plugins ls | grep "vertamedia-clickhouse-datasource @ 2.3.1"
 	fi
 fi
