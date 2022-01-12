7115eaf9a6d26ded41f772620cd373dce9bd7592
Puneet Kala <puneet.0191@gmail.com>
2022-01-12 13:57:04 +0530

    PMM-9393 Improved metric check function

--- pmm-tests/pmm-2-0-bats-tests/check_metric.sh
+++ pmm-tests/pmm-2-0-bats-tests/check_metric.sh
@@ -4,10 +4,15 @@ SERVICE_NAME=$1
 METRIC_NAME=$2
 PMM_SERVER_IP=$3
 AGENT_TYPE=$4
-AGENT_USER=$5
 AGENT_PASSWORD=$6
+METRIC_VALUE=$5
+
 
 export SERVICE_ID=$(pmm-admin list | grep ${SERVICE_NAME} | awk -F" " '{print $4}')
 export AGENT_ID=$(pmm-admin list | grep ${SERVICE_ID} | grep ${AGENT_TYPE} | awk -F" " '{print $4}')
+if [ -z "$AGENT_PASSWORD" ]; then
+	export AGENT_PASSWORD=${AGENT_ID}
+fi
+export TOKEN=$(printf '%s' pmm:${AGENT_PASSWORD} | base64)
 export LISTEN_PORT=$(curl -s "http://${PMM_SERVER_IP}/v1/inventory/Agents/List" -H 'Authorization: Basic YWRtaW46YWRtaW4=' --data-raw '{"promise":{}}' --compressed --insecure | jq ".${AGENT_TYPE}[] | select(.agent_id == "\"${AGENT_ID}"\") | .listen_port")
-curl -s "http://${AGENT_USER}:${AGENT_PASSWORD}@${PMM_SERVER_IP}:${LISTEN_PORT}/metrics" | grep "${METRIC_NAME} 1"
+curl -s -H "Authorization: Basic ${TOKEN}" "http://${PMM_SERVER_IP}:${LISTEN_PORT}/metrics" | grep "${METRIC_NAME} ${METRIC_VALUE}"
