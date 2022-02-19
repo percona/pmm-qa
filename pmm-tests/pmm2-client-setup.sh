4be35633c0457127d178ed71afdccf7eb1e724f7
Puneet Kala <puneet.0191@gmail.com>
2022-02-19 12:21:28 +0530

    PMM-9210 minor updates

--- pmm-tests/pmm2-client-setup.sh
+++ pmm-tests/pmm2-client-setup.sh
@@ -1,10 +1,39 @@
 #!/bin/sh
 
-export PMM_SERVER_IP=$1
-export CLIENT_VERSION=$2
-export METRICS_METHOD=$3
-export ADMIN_PASSWORD=$4
-export INSTALL_CLIENT=$5
+
+while [ $# -gt 0 ]; do
+
+   if [[ $1 == *"--"* ]]; then
+        param="${1/--/}"
+        declare $param="$2"
+   fi
+
+  shift
+done
+
+if [ -z "$admin_password" ]; then
+    export admin_password=admin
+fi
+
+if [ -z "$pmm_server_ip" ]; then
+    export pmm_server_ip=127.0.0.1
+fi
+
+if [ -z "$client_version" ]; then
+    export client_version=dev-latest
+fi
+
+if [ -z "$install_client" ]; then
+    export install_client=no
+fi
+
+if [ -z "$metrics_mode" ]; then
+    export metrics_mode=auto
+fi
+
+if [ -z "$use_metrics_mode" ]; then
+    export use_metrics_mode=yes
+fi
 
 apt-get update
 apt-get install -y wget gnupg2 libtinfo-dev libnuma-dev mysql-client postgresql-client
@@ -14,56 +43,65 @@ apt-get update
 export PMM_AGENT_SETUP_NODE_NAME=client_container_$(echo $((1 + $RANDOM % 9999)))
 mv -v /artifacts/* .
 
-if [[ "$CLIENT_VERSION" == "dev-latest" ]]; then
+if [[ "$client_version" == "dev-latest" ]]; then
     percona-release enable-only original experimental
     apt-get update
     apt-get -y install pmm2-client
 fi
 
-if [[ "$CLIENT_VERSION" == "pmm2-rc" ]]; then
+if [[ "$client_version" == "pmm2-rc" ]]; then
     percona-release enable-only original testing
     apt-get update
     apt-get -y install pmm2-client
 fi
 
-if [[ "$CLIENT_VERSION" == "pmm2-latest" ]]; then
+if [[ "$client_version" == "pmm2-latest" ]]; then
     apt-get -y install pmm2-client
     apt-get -y update
     percona-release enable-only original experimental
 fi
 
 ## only supported for debian based systems for now
-if [[ "$CLIENT_VERSION" == 2* && dpkg -l ]]; then
+if [[ "$client_version" == 2* && $(dpkg --list vim) ]]; then
     export distro=$(cat /etc/*-release | grep DISTRIB_CODENAME | awk -F'=' '{print $2}')
-    wget -O pmm2-client.deb https://repo.percona.com/pmm2-client/apt/pool/main/p/pmm2-client/pmm2-client_${CLIENT_VERSION}-6.${distro}_amd64.deb
+    wget -O pmm2-client.deb https://repo.percona.com/pmm2-client/apt/pool/main/p/pmm2-client/pmm2-client_${client_version}-6.${distro}_amd64.deb
     dpkg -i pmm2-client.deb
+    percona-release enable-only original experimental
 fi
 
-if [[ "$CLIENT_VERSION" == http* ]]; then
-		if [[ "$INSTALL_CLIENT" == "yes" ]]; then
-			wget -O pmm2-client.tar.gz --progress=dot:giga "${CLIENT_VERSION}"
-		fi
-        tar -zxpf pmm2-client.tar.gz
-        rm -r pmm2-client.tar.gz
-        export PMM2_CLIENT=`ls -1td pmm2-client* 2>/dev/null | grep -v ".tar" | grep -v ".sh" | head -n1`
-        echo ${PMM2_CLIENT}
-        mv ${PMM2_CLIENT} pmm2-client
-        cd pmm2-client
-        bash -x ./install_tarball
-        pwd
-        cd ../
-        export PMM_CLIENT_BASEDIR=`ls -1td pmm2-client 2>/dev/null | grep -v ".tar" | head -n1`
-        export PATH="`pwd`/pmm2-client/bin:$PATH"
-        echo "export PATH=`pwd`/pmm2-client/bin:$PATH" >> ~/.bash_profile
-        source ~/.bash_profile
-        pmm-admin --version
-        pmm-agent setup --config-file=`pwd`/pmm2-client/config/pmm-agent.yaml --server-address=${PMM_SERVER_IP}:443 --server-insecure-tls --metrics-mode=${METRICS_METHOD} --server-username=admin --server-password=${ADMIN_PASSWORD}
-		sleep 10
-		pmm-agent --config-file=`pwd`/pmm2-client/config/pmm-agent.yaml > pmm-agent.log 2>&1 &
+if [[ "$client_version" == http* ]]; then
+	if [[ "$install_client" == "yes" ]]; then
+		wget -O pmm2-client.tar.gz --progress=dot:giga "${client_version}"
+	fi
+    tar -zxpf pmm2-client.tar.gz
+    rm -r pmm2-client.tar.gz
+    export PMM2_CLIENT=`ls -1td pmm2-client* 2>/dev/null | grep -v ".tar" | grep -v ".sh" | head -n1`
+    echo ${PMM2_CLIENT}
+    mv ${PMM2_CLIENT} pmm2-client
+    cd pmm2-client
+    bash -x ./install_tarball
+    pwd
+    cd ../
+    export PMM_CLIENT_BASEDIR=`ls -1td pmm2-client 2>/dev/null | grep -v ".tar" | head -n1`
+    export PATH="`pwd`/pmm2-client/bin:$PATH"
+    echo "export PATH=`pwd`/pmm2-client/bin:$PATH" >> ~/.bash_profile
+    source ~/.bash_profile
+    pmm-admin --version
+    if [[ "$use_metrics_mode" == "yes" ]]; then
+        pmm-agent setup --config-file=`pwd`/pmm2-client/config/pmm-agent.yaml --server-address=${pmm_server_ip}:443 --server-insecure-tls --metrics-mode=${metrics_mode} --server-username=admin --server-password=${admin_password}
+	else
+        pmm-agent setup --config-file=`pwd`/pmm2-client/config/pmm-agent.yaml --server-address=${pmm_server_ip}:443 --server-insecure-tls --server-username=admin --server-password=${admin_password}
+    fi
+    sleep 10
+	pmm-agent --config-file=`pwd`/pmm2-client/config/pmm-agent.yaml > pmm-agent.log 2>&1 &
 else
-		pmm-agent setup --config-file=/usr/local/percona/pmm2/config/pmm-agent.yaml --server-address=${PMM_SERVER_IP}:443 --server-insecure-tls --metrics-mode=${METRICS_METHOD} --server-username=admin --server-password=${ADMIN_PASSWORD}
-		sleep 10
-		pmm-agent --config-file=/usr/local/percona/pmm2/config/pmm-agent.yaml > pmm-agent.log 2>&1 &
+    if [[ "$use_metrics_mode" == "yes" ]]; then
+        pmm-agent setup --config-file=/usr/local/percona/pmm2/config/pmm-agent.yaml --server-address=${pmm_server_ip}:443 --server-insecure-tls --metrics-mode=${metrics_mode} --server-username=admin --server-password=${admin_password}
+	else
+        pmm-agent setup --config-file=/usr/local/percona/pmm2/config/pmm-agent.yaml --server-address=${pmm_server_ip}:443 --server-insecure-tls --server-username=admin --server-password=${admin_password}
+    fi    
+    sleep 10
+	pmm-agent --config-file=/usr/local/percona/pmm2/config/pmm-agent.yaml > pmm-agent.log 2>&1 &
 fi
 sleep 10
 pmm-admin status
