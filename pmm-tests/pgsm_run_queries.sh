c5e1bc7b137635d7c6e56bb21f541ade31876c57
Puneet Kala <puneet.0191@gmail.com>
2022-03-24 17:14:38 +0530

    PMM-7 fix branch name

--- pmm-tests/pgsm_run_queries.sh
+++ pmm-tests/pgsm_run_queries.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ## Running Queries
-wget https://raw.githubusercontent.com/percona/pmm-agent/pmm-2.26.0/testqueries/postgres/pg_stat_monitor_load.sql
+wget https://raw.githubusercontent.com/percona/pmm-agent/main/testqueries/postgres/pg_stat_monitor_load.sql
 while true
 do
   su postgres bash -c 'psql -d contrib_regression -f pg_stat_monitor_load.sql'
