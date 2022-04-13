b86f56d2f8a282076809260c155a0545fa63b987
Puneet Kala <puneet.0191@gmail.com>
2022-04-13 22:03:35 +0530

    PMM-7-fix (#473)

--- pmm-tests/check_upgrade.sh
+++ pmm-tests/check_upgrade.sh
@@ -8,9 +8,20 @@ if [ $3 == "ami" ]; then
 	rpm -qa | grep pmm-server-$1
 	rpm -qa | grep pmm-managed-$1
 	rpm -qa | grep pmm2-client-$1
+	sudo supervisorctl status | grep qan-api2 | grep RUNNING
+	sudo supervisorctl status | grep alertmanager | grep RUNNING
+	sudo supervisorctl status | grep clickhouse | grep RUNNING
+	sudo supervisorctl status | grep cron | grep RUNNING
+	sudo supervisorctl status | grep grafana | grep RUNNING
+	sudo supervisorctl status | grep nginx | grep RUNNING
+	sudo supervisorctl status | grep pmm-agent | grep RUNNING
+	sudo supervisorctl status | grep pmm-managed | grep RUNNING
+	sudo supervisorctl status | grep postgresql | grep RUNNING
 
 	if [ $2 == "post" ]; then
 		rpm -qa | grep dbaas-controller-$1
+		sudo supervisorctl status | grep victoriametrics | grep RUNNING
+		sudo supervisorctl status | grep vmalert | grep RUNNING
 		grafana-cli plugins ls | grep "vertamedia-clickhouse-datasource @ 2.3.1"
 		grafana-cli plugins ls | grep alexanderzobnin-zabbix-app
 	fi
@@ -19,7 +30,9 @@ else
 	echo $PMM_SERVER_DOCKER_CONTAINER
 	docker exec $PMM_SERVER_DOCKER_CONTAINER rpm -qa | grep percona-qan-api2-$1
 	docker exec $PMM_SERVER_DOCKER_CONTAINER rpm -qa | grep percona-dashboards-$1
-	docker exec $PMM_SERVER_DOCKER_CONTAINER rpm -qa | grep pmm-update-$1
+	if [ $1 != "2.25.0" ]; then
+		docker exec $PMM_SERVER_DOCKER_CONTAINER rpm -qa | grep pmm-update-$1
+	fi
 	docker exec $PMM_SERVER_DOCKER_CONTAINER rpm -qa | grep pmm-server-$1
 	docker exec $PMM_SERVER_DOCKER_CONTAINER rpm -qa | grep pmm-managed-$1
 	docker exec $PMM_SERVER_DOCKER_CONTAINER rpm -qa | grep pmm2-client-$1
