6c21b65e6959992fdd931ea99d40158952d562ae
Puneet Kala <puneet.0191@gmail.com>
2022-04-14 13:13:25 +0530

    PMM-7 change load script

--- pmm-tests/pgstatmonitor_metrics_queries.sh
+++ pmm-tests/pgstatmonitor_metrics_queries.sh
@@ -3,11 +3,10 @@
 export PDPGSQL_CONTAINER=$1
 db=1
 docker exec ${PDPGSQL_CONTAINER} mkdir /tmp/sql
-pushd pg_stat_monitor/regression/sql/
+wget https://raw.githubusercontent.com/percona/pmm-agent/main/testqueries/postgres/pg_stat_monitor_load.sql
 for filename in *.sql; do
     docker exec ${PDPGSQL_CONTAINER} bash -c "psql -h localhost -U postgres -c 'create database test${db}'"
     docker cp ${filename} ${PDPGSQL_CONTAINER}:/tmp/sql
     docker exec -u postgres ${PDPGSQL_CONTAINER} psql test${db} postgres -f /tmp/sql/${filename}
     ((db++))
 done
-popd
