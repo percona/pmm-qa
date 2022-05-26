0787b755c7b83371935a42c9e8c6d3db21b47444
Beata Handzelova <beata.handzelova@office365.3pillarglobal.com>
2022-05-26 15:44:56 +0200

    install operators separately

--- pmm-tests/minikube_operators_setup.sh
+++ pmm-tests/minikube_operators_setup.sh
@@ -3,21 +3,20 @@
 # Prepare a set of base64 encoded values and non encoded for user and pass with administrator privileges to pmm-server (DBaaS)
 PMM_USER='admin';
 PMM_PASS='admin';
-OPERATOR_VERSION=$1;
+PXC_OPERATOR_VERSION=$1;
+PSMDB_OPERATOR_VERSION=$2;
 
-if [ -z "$OPERATOR_VERSION" ] ; then
-	echo "Error: no parameter passed"
-	echo "Use: bash ./minikube_operators_setup.sh <operator_version>"
-	exit 1
+if [ "$PXC_OPERATOR_VERSION" == none ] ; then
+	echo "No PXC operator installed"
+else
+	curl -sSf -m 30 https://raw.githubusercontent.com/percona/percona-xtradb-cluster-operator/"${PXC_OPERATOR_VERSION}"/deploy/bundle.yaml | kubectl apply -f -		
+fi
+
+if [ "$PSMDB_OPERATOR_VERSION" == none ] ; then
+	echo "No PSMDB operator installed"
+else
+	curl -sSf -m 30 https://raw.githubusercontent.com/percona/percona-server-mongodb-operator/"${PSMDB_OPERATOR_VERSION}"/deploy/bundle.yaml | kubectl apply -f -	
 fi
 
 PMM_USER_B64="$(echo -n "${PMM_USER}" | base64)";
 PMM_PASS_B64="$(echo -n "${PMM_PASS}" | base64)";
-
-# Install the PXC operator
-curl -sSf -m 30 https://raw.githubusercontent.com/percona/percona-xtradb-cluster-operator/v1.8.0/deploy/bundle.yaml \
-| minikube kubectl -- apply -f -
-
-# Install the PSMDB operator
-curl -sSf -m 30 https://raw.githubusercontent.com/percona/percona-server-mongodb-operator/v1.8.0/deploy/bundle.yaml \
-| minikube kubectl -- apply -f -
