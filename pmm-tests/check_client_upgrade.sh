286f6b4833c7fe8965c1d82f945582ef78bd245d
Puneet Kala <puneet.0191@gmail.com>
2022-06-08 16:57:23 +0530

    PMM-10083 fix for FB execution

--- pmm-tests/check_client_upgrade.sh
+++ pmm-tests/check_client_upgrade.sh
@@ -12,23 +12,25 @@ pmm-admin status | grep postgres_exporter | grep -qv Waiting
 pmm-admin status | grep mongodb_profiler_agent | grep -qv Waiting
 pmm-admin status | grep postgresql_pgstatements_agent | grep -qv Waiting
 server_version=$(pmm-admin status | grep Version | awk -F' ' '{print $2}')
-if [ "$server_version" != "$1" ]; then
-    echo "PMM Server Version is not equal to expected $1";
-    exit 1;
-fi
-admin_version=$(pmm-admin status | grep pmm-admin | awk -F' ' '{print $3}')
-if [ "$admin_version" != "$1" ]; then
-    echo "PMM Admin Version is not equal to expected $1";
-    exit 1;
-fi
-agent_version=$(pmm-admin status | grep pmm-agent | awk -F' ' '{print $3}')
-if [ "$agent_version" != "$1" ]; then
-    echo "PMM Agent Version is not equal to expected $1";
-    exit 1;
-fi
-if [ "$agent_version" != "$admin_version" ]; then
-    echo "PMM-Agent Version Does not Match PMM-Admin Version";
-    exit 1;
+if [ ${FB_EXECUTION} != "yes" ]; then
+        if [ "$server_version" != "$1" ]; then
+            echo "PMM Server Version is not equal to expected $1";
+            exit 1;
+        fi
+        admin_version=$(pmm-admin status | grep pmm-admin | awk -F' ' '{print $3}')
+        if [ "$admin_version" != "$1" ]; then
+            echo "PMM Admin Version is not equal to expected $1";
+            exit 1;
+        fi
+        agent_version=$(pmm-admin status | grep pmm-agent | awk -F' ' '{print $3}')
+        if [ "$agent_version" != "$1" ]; then
+            echo "PMM Agent Version is not equal to expected $1";
+            exit 1;
+        fi
+        if [ "$agent_version" != "$admin_version" ]; then
+            echo "PMM-Agent Version Does not Match PMM-Admin Version";
+            exit 1;
+        fi
 fi
 ls -la /usr/local/percona/pmm2/exporters | grep -q azure_exporter
 ls -la /usr/local/percona/pmm2/exporters | grep -q mongodb_exporter
