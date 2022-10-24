dc22c7bd9456dc10cf79b4f6aac681e1b0dbbf0b
Peter <sirotnak.p@gmail.com>
2022-10-24 16:25:34 +0200

    Fixes variable value

--- pmm-tests/pgsql-vacuum.sh
+++ pmm-tests/pgsql-vacuum.sh
@@ -5,7 +5,7 @@ export PGSQL_VACUUM_VERSION="14.5"
 echo "Setting up Postgres for vacuum monitoring"
 if [ ! -z $@ ]; then
     echo "Using pgsql version ${$@}"
-    PGSQL_VACUUM_VERSION=$@
+    PGSQL_VACUUM_VERSION="$@"
 fi  
 echo "$@"
 echo "$1"
