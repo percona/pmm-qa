cbbff84f1b991bea2d8998f20e70592c8d8788b0
puneet0191 <puneet.0191@gmail.com>
2022-11-07 20:54:04 +0530

    PMM-7 Fix the issue with setup

--- pmm-tests/client_container_ps_setup.sh
+++ pmm-tests/client_container_ps_setup.sh
@@ -65,5 +65,5 @@ fi
 
 ## Start Running Load
 ~/sandboxes/${db_sandbox}/sysbench_ready prepare > sysbench_prepare.txt 2>&1 &
-sleep 60
+sleep 120
 ~/sandboxes/${db_sandbox}/sysbench_ready run > sysbench_run.txt 2>&1 &
