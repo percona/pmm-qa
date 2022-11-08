5e73c2f20627b1e844555f9d3b40eb48a1a3099b
Peter <sirotnak.p@gmail.com>
2022-11-08 21:12:29 +0100

    adds missing steps for vacuum

--- pmm-integration/postgres/pgsql-vacuum-setup.ts
+++ pmm-integration/postgres/pgsql-vacuum-setup.ts
@@ -14,7 +14,7 @@ const pgsqlVacuumSetup = async ({pgsqlVersion = 'latest'}) => {
   await executeCommand(`docker exec ${dockerContainerName} wget https://www.postgresqltutorial.com/wp-content/uploads/2019/05/dvdrental.zip`);
   await executeCommand(`docker exec ${dockerContainerName} unzip dvdrental.zip`);
   await executeCommand(`docker exec ${dockerContainerName} psql -U postgres -c "CREATE EXTENSION pg_stat_statements;"`);
-  await executeCommand(`docker exec ${dockerContainerName} psql -U postgres -c 'create database dvdrental;"`);
+  await executeCommand(`docker exec ${dockerContainerName} psql -U postgres -c 'create database dvdrental;'`);
   await executeCommand(`docker exec ${dockerContainerName} pg_restore -U postgres -d dvdrental dvdrental.tar"`);
 
   await executeCommand(`rm dvdrental.tar.xz || true`);
@@ -25,6 +25,24 @@ const pgsqlVacuumSetup = async ({pgsqlVersion = 'latest'}) => {
   await executeCommand(`docker exec ${dockerContainerName} psql -d dvdrental -f dvdrental.sql -U postgres`);
 
   await executeCommand(`pmm-admin add postgresql --username=postgres --password=YIn7620U1SUc pgsql_vacuum_db localhost:7432`);
+  await executeCommand(`j=0 \ 
+    while [ $j -lt 3 ]  \
+    do \
+      export LENGTH=$(shuf -i 100-120 -n 1) \
+      export LENGTH_NEW=$(shuf -i 100-120 -n 1) \
+      export TABLE=$(shuf -i 1-1000 -n 1) \
+      export COUNT=$(docker exec pgsql_vacuum_db psql -U postgres -d dvdrental -c "select count(*) from film_testing_\${TABLE} where length=${LENGTH};" | tail -3 | head -1 | xargs) \
+      docker exec pgsql_vacuum_db psql -U postgres -d dvdrental -c "delete from film_testing_\${TABLE} where length=\${LENGTH};" \
+      i=0 \
+      while [ "$i" -le \${COUNT} ]; do \
+          docker exec pgsql_vacuum_db psql -U postgres -d dvdrental -c "insert into film_testing_\${TABLE} values (\${i}, 'title for \${i}', 'Description for \${i}', \${LENGTH});" \
+          i=$(( i + 1 )) \
+      done  \
+      docker exec pgsql_vacuum_db psql -U postgres -d dvdrental -c "update film_testing_\${TABLE} set length=\${LENGTH_NEW} where length=\${LENGTH};" \
+      sleep 5 \ 
+      j=$(( j + 1 )) \
+  done \ 
+  `)
 
 }
 
