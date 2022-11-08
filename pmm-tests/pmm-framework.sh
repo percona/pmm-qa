5983d8eb19a8b26fbfc75987122c1f0b48edd51f
yurkovychv <vasyl.yurkovych@percona.com>
2022-11-08 18:33:51 +0200

    PMM-11026 update permissions

--- pmm-tests/pmm-framework.sh
+++ pmm-tests/pmm-framework.sh
@@ -2896,6 +2896,7 @@ setup_mongo_replica_for_backup() {
   echo "Setting up MongoDB replica set with PBM"
   sudo percona-release enable pbm release && sudo yum -y install percona-backup-mongodb
   setup_docker_compose
+  sudo openssl rand -base64 700 > $SCRIPT_PWD/backup/mongodb/file.key
   sudo chmod 400 $SCRIPT_PWD/backup/mongodb/file.key
   docker-compose -f $SCRIPT_PWD/backup/mongodb/docker-compose-mongo-replica.yml up -d
   sleep 20
