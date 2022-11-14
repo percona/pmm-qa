2949ec1cce065122dcdd3a24ce91be48902bb576
Peter <sirotnak.p@gmail.com>
2022-11-14 08:25:02 +0100

    PMM-10994: Adds test flags to the integration setup

--- pmm-integration/availableArgs.ts
+++ pmm-integration/availableArgs.ts
@@ -1,4 +1,4 @@
-import { executeCommand } from "./helpers/commandLine";
+import { executeCommand, setEnvVariable } from "./helpers/commandLine";
 import SetupParameters from "./helpers/setupParameters.interface";
 import pgsqlVacuumSetup from "./postgres/pgsql-vacuum-setup";
 
@@ -12,7 +12,10 @@ export const availableSetups: SetupsInterface[] = [
   {
     arg: '--setup-pgsql-vacuum',
     description: 'Use this do setup postgres for vacuum monitoring tests',
-    function: async (parameters: SetupParameters) => await pgsqlVacuumSetup(parameters),
+    function: async (parameters: SetupParameters) => {
+      await pgsqlVacuumSetup(parameters);
+      await setEnvVariable("INTEGRATION_FLAG", "@pgsql_vacuum");
+    },
   },
   {
     arg: '--setup-pmm-pgsm-integration',
@@ -20,6 +23,7 @@ export const availableSetups: SetupsInterface[] = [
     function: async (parameters: SetupParameters) => {
       await executeCommand('chmod +x ./postgres/pgsql_pgsm_setup/setup_pmm_pgsm_integration.sh');
       console.log(await executeCommand('./postgres/pgsql_pgsm_setup/setup_pmm_pgsm_integration.sh'));
+      await setEnvVariable("INTEGRATION_FLAG", "@pmm-pgsm-integration");
     },
   },
   {
@@ -28,6 +32,7 @@ export const availableSetups: SetupsInterface[] = [
     function: async (parameters: SetupParameters) => {
       await executeCommand('chmod +x ./postgres/pgsql_pgss_setup/setup_pmm_pgss_integration.sh');
       console.log(await executeCommand('./postgres/pgsql_pgss_setup/setup_pmm_pgss_integration.sh'));
+      await setEnvVariable("INTEGRATION_FLAG", "@pmm-pgss-integration");
     },
   },
   {
@@ -36,13 +41,14 @@ export const availableSetups: SetupsInterface[] = [
     function: async (parameters: SetupParameters) => {
       await executeCommand('chmod +x ./mongoDb/mongo_psmdb_setup/setup_pmm_psmdb_integration.sh');
       console.log(await executeCommand('./mongoDb/mongo_psmdb_setup/setup_pmm_psmdb_integration.sh'));
+      await setEnvVariable("INTEGRATION_FLAG", "@pmm-psmdb-integration");
     },
   }
 ];
 
 export const availableConstMap = new Map<string, string>([
-    ["--pgsql-version", "Pass Postgre SQL server version Info"],
-    ["--mo-version", "Pass MongoDB Server version info"]
+  ["--pgsql-version", "Pass Postgre SQL server version Info"],
+  ["--mo-version", "Pass MongoDB Server version info"]
 ]);
 
 export const availableSetupMap = new Map(
@@ -53,6 +59,6 @@ export const availableSetupMap = new Map(
 
 export const availableArgsMap = new Map<string, string>([...availableConstMap, ...availableSetupMap]);
 
-const availableCommandsLineArgs: string[] =  Array.from( availableArgsMap.keys() );
+const availableCommandsLineArgs: string[] = Array.from(availableArgsMap.keys());
 
 export default availableCommandsLineArgs;
