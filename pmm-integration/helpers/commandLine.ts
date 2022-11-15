5ac99b83be60b7242e7a9a776403f57387897a94
Peter <sirotnak.p@gmail.com>
2022-11-15 09:07:24 +0100

    command line refactoring

--- pmm-integration/helpers/commandLine.ts
+++ pmm-integration/helpers/commandLine.ts
@@ -4,13 +4,20 @@ import { promisify } from 'util';
 const awaitExec = promisify(exec);
 
 export const executeCommand = async (command: string) => {
-  return awaitExec(command, { env: process.env });
+  const response = await awaitExec(command, { env: process.env });
+
+  console.log(response.stdout)
+
+  if (response.stderr) {
+    throw new Error(`Command: "${command}" failed due to error: "${response.stderr}"`)
+  }
+  return response;
 }
 
 export const executeCommandIgnoreErrors = async (command: string) => {
   return awaitExec(command)
-  .then((response: any) => response)
-  .catch((e: Error) => {});
+    .then((response: any) => response)
+    .catch((e: Error) => { });
 }
 
 export const setEnvVariable = async (variable: string, value: string) => {
