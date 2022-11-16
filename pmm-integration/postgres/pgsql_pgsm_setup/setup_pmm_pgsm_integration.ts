59849d71e7347a1a3a863e4c90fe7fa95dbfdd58
Peter <sirotnak.p@gmail.com>
2022-11-16 20:31:34 +0100

    Log steps in node js

--- pmm-integration/postgres/pgsql_pgsm_setup/setup_pmm_pgsm_integration.ts
+++ pmm-integration/postgres/pgsql_pgsm_setup/setup_pmm_pgsm_integration.ts
@@ -4,8 +4,10 @@ import * as core from '@actions/core';
 
 const setup_pmm_pgsm_integration = async (parameters: SetupParameters) => {
 
+    console.log('Install Ansible')
     await executeCommand('sudo yum install -y ansible');
 
+    console.log('Set Env Variable')
     await setEnvVariable("PGSQL_PGSM_CONTAINER", `pgsql_pgsm_${process.env['PGSQL_VERSION']}`);
     core.exportVariable('PGSQL_PGSM_CONTAINER', `pgsql_pgsm_${process.env['PGSQL_VERSION']}`);
 
@@ -15,10 +17,12 @@ const setup_pmm_pgsm_integration = async (parameters: SetupParameters) => {
     await setEnvVariable("CLIENT_VERSION", `dev-latest`);
     core.exportVariable('CLIENT_VERSION', `dev-latest`);
 
-
+    console.log('Get Container Name')
     const pmmServerContainer = await executeCommand(`echo $(docker ps --format "table {{.ID}}\t{{.Image}}\t{{.Names}}" | grep 'pmm-server' | awk '{print $3}')`)
+    console.log('Create Network')
     await executeCommandIgnoreErrors('docker network create pmm-qa')
     await executeCommandIgnoreErrors(`docker network connect ${pmmServerContainer}`)
+    console.log('Run Ansible Playbook.')
     console.log(await executeCommand('ansible-playbook --connection=local --inventory 127.0.0.1, --limit 127.0.0.1 ./postgres/pgsql_pgsm_setup/pgsql_pgsm_setup.yml'))
 
 }
