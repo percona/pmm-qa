d4505bab2191736336a10aea71092e33e09ed060
Peter <sirotnak.p@gmail.com>
2022-11-16 13:38:19 +0100

    refactoring

--- pmm-integration/postgres/pgsql_pgsm_setup/setup_pmm_pgsm_integration.ts
+++ pmm-integration/postgres/pgsql_pgsm_setup/setup_pmm_pgsm_integration.ts
@@ -16,9 +16,7 @@ const setup_pmm_pgsm_integration = async (parameters: SetupParameters) => {
 
     const pmmServerContainer = await executeCommand(`echo $(docker ps --format "table {{.ID}}\t{{.Image}}\t{{.Names}}" | grep 'pmm-server' | awk '{print $3}')`)
     await executeCommandIgnoreErrors('docker network create pmm-qa')
-    pmmServerContainer?.stdout.trim().split(' ').forEach(async (container) => {
-        await executeCommandIgnoreErrors(`docker network connect ${container}`)
-    })
+    await executeCommandIgnoreErrors(`docker network connect ${pmmServerContainer}`)
     await executeAnsiblePlaybook('ansible-playbook --connection=local --inventory 127.0.0.1, --limit 127.0.0.1 ./postgres/pgsql_pgsm_setup/pgsql_pgsm_setup.yml')
 }
 
