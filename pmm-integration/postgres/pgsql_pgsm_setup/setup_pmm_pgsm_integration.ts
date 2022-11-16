c24fd23fedd8d1be331bf9f5ba96527a53756158
Peter <sirotnak.p@gmail.com>
2022-11-16 13:26:43 +0100

    adds required env variables

--- pmm-integration/postgres/pgsql_pgsm_setup/setup_pmm_pgsm_integration.ts
+++ pmm-integration/postgres/pgsql_pgsm_setup/setup_pmm_pgsm_integration.ts
@@ -1,7 +1,19 @@
-import { executeAnsiblePlaybook, executeCommand, executeCommandIgnoreErrors } from '../../helpers/commandLine';
+import { executeAnsiblePlaybook, executeCommand, executeCommandIgnoreErrors, setEnvVariable } from '../../helpers/commandLine';
 import SetupParameters from '../../helpers/setupParameters.interface';
+import * as core from '@actions/core';
 
 const setup_pmm_pgsm_integration = async (parameters: SetupParameters) => {
+
+    await setEnvVariable("PGSQL_PGSM_CONTAINER", `pgsql_pgsm_${process.env['PGSQL_VERSION']}`);
+    core.exportVariable('PGSQL_PGSM_CONTAINER', `pgsql_pgsm_${process.env['PGSQL_VERSION']}`);
+
+    await setEnvVariable("PMM_SERVER_IP", `127.0.0.1`);
+    core.exportVariable('PMM_SERVER_IP', `127.0.0.1`);
+
+    await setEnvVariable("CLIENT_VERSION", `dev-latest`);
+    core.exportVariable('CLIENT_VERSION', `dev-latest`);
+
+
     const pmmServerContainer = await executeCommand(`echo $(docker ps --format "table {{.ID}}\t{{.Image}}\t{{.Names}}" | grep 'pmm-server' | awk '{print $3}')`)
     await executeCommandIgnoreErrors('docker network create pmm-qa')
     pmmServerContainer?.stdout.trim().split(' ').forEach(async (container) => {
