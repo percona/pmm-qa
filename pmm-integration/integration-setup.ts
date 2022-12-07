67b2bb6c787bcdf360075433cb23bee00fbb3077
Peter <sirotnak.p@gmail.com>
2022-12-07 12:55:59 +0100

    log all variables

--- pmm-integration/integration-setup.ts
+++ pmm-integration/integration-setup.ts
@@ -12,7 +12,7 @@ export const pmmIntegrationServerName = 'pmm-integration-server'
 const run = async () => {
   let parameters: SetupParameters = {};
   const commandLineArgs: string[] = process.argv.slice(2);
-  console.log('NODE_ENV is:' + process.env.NODE_ENV)
+  console.log('ENV VARS are:' + process.env)
   if(process.env.CI) {
     await installAnsible();
   } else {
