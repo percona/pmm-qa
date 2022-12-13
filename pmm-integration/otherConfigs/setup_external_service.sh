1b9cf3291f8222925a3a12817081b687e4382f40
Peter <sirotnak.p@gmail.com>
2022-12-13 09:52:17 +0100

    connect to the docker pmm client for external service

--- pmm-integration/otherConfigs/setup_external_service.sh
+++ pmm-integration/otherConfigs/setup_external_service.sh
@@ -9,10 +9,10 @@ sleep 10
 touch redis.log
 JENKINS_NODE_COOKIE=dontKillMe nohup bash -c './redis_exporter -redis.password=oFukiBRg7GujAJXq3tmd -redis.addr=localhost:6379 -web.listen-address=:42200 > redis.log 2>&1 &'
 sleep 10
-pmm-admin add external --listen-port=42200 --group="redis" --service-name="redis_external"
+docker exec pmm-client pmm-admin add external --listen-port=42200 --group="redis" --service-name="redis_external"
 echo "Setting up node_process"
 wget https://github.com/ncabatoff/process-exporter/releases/download/v${NODE_PROCESS_EXPORTER_VERSION}/process-exporter_${NODE_PROCESS_EXPORTER_VERSION}_linux_amd64.deb
 sudo dpkg -i process-exporter_${NODE_PROCESS_EXPORTER_VERSION}_linux_amd64.deb
 sudo service process-exporter start
 sleep 10
-pmm-admin add external --group=processes --listen-port=9256 --service-name=external_nodeprocess
+docker exec pmm-client pmm-admin add external --group=processes --listen-port=9256 --service-name=external_nodeprocess
