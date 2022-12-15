778f6a8f1a1590a6537191874736727daeff188f
Peter <sirotnak.p@gmail.com>
2022-12-15 12:07:02 +0100

    Comment to explain validation of args

--- pmm-integration/helpers/validateArgs.ts
+++ pmm-integration/helpers/validateArgs.ts
@@ -4,6 +4,7 @@ import availableCommandsLineArgs, { availableArgsMap } from "../availableArgs";
 const validateArgs = async (args: string[]) => {
     args.forEach((arg) => {
         if(availableCommandsLineArgs.findIndex(option => arg.includes(option)) < 0) {
+            console.log(`Arg "${arg}" in not a valid parameter.\nPlease select one from list below:`);
             availableArgsMap.forEach((value, key) => {
                 console.log(`   ${key}:    ${value}`);
             })
