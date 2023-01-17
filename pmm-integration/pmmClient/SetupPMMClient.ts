87d3604d101afe3ec279721aac62bc07889e8ea5
Peter <sirotnak.p@gmail.com>
2023-01-17 17:39:07 +0100

    Refactors pmm client setup

--- pmm-integration/pmmClient/SetupPMMClient.ts
+++ pmm-integration/pmmClient/SetupPMMClient.ts
@@ -2,24 +2,24 @@ import { executeCommand } from "../helpers/commandLine";
 import SetupParameters from "../helpers/setupParameters.interface";
 
 const SetupPMMClient = async (params: SetupParameters) => {
-    await executeCommand('sudo wget https://repo.percona.com/apt/percona-release_latest.focal_all.deb');
-    await executeCommand('sudo dpkg -i percona-release_latest.focal_all.deb');
-    await executeCommand('sudo apt-get update');
+    await executeCommand('sudo wget https://repo.percona.com/apt/percona-release_latest.generic_all.deb');
+    await executeCommand('sudo dpkg -i percona-release_latest.generic_all.deb');
+    await executeCommand('sudo apt update');
     if(params.pmmClientVersion?.includes('dev-latest')) {
         await executeCommand('sudo percona-release enable-only original experimental');
-        await executeCommand('sudo apt-get -y install pmm2-client');
-        await executeCommand('sudo apt-get update')
+        await executeCommand('sudo apt -y install pmm2-client');
+        await executeCommand('sudo apt update')
     } else if(params.pmmClientVersion?.includes('pmm2-rc')) {
         await executeCommand('sudo percona-release enable-only original testing');
-        await executeCommand('sudo apt-get -y install pmm2-client');
-        await executeCommand('sudo apt-get update')
+        await executeCommand('sudo apt -y install pmm2-client');
+        await executeCommand('sudo apt update')
     } else if(params.pmmClientVersion?.includes('pmm2-latest')) {
-        await executeCommand('sudo apt-get -y install pmm2-client');
-        await executeCommand('sudo apt-get update')
+        await executeCommand('sudo apt -y install pmm2-client');
+        await executeCommand('sudo apt update')
         await executeCommand('sudo percona-release enable-only original experimental');
     } else if(params.pmmClientVersion?.includes('2.')) {
-        await executeCommand(`sudo apt-get -y install pmm2-client-${params.pmmClientVersion}-6.el7.x86_64`);
-        await executeCommand('sudo apt-get update')
+        await executeCommand(`sudo apt -y install pmm2-client-${params.pmmClientVersion}-6.el7.x86_64`);
+        await executeCommand('sudo apt update')
         await executeCommand('sudo percona-release enable-only original experimental');
     }
 };
