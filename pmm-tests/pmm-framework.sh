3b9551a69363ba2e7750351632b0e722922051ea
beatahandzelova <beata.handzelova.sde@gmail.com>
2023-01-20 16:00:57 +0100

    move after sleep

--- pmm-tests/pmm-framework.sh
+++ pmm-tests/pmm-framework.sh
@@ -1432,8 +1432,8 @@ add_clients(){
       for j in `seq 1  ${ADDCLIENTS_COUNT}`;do
         check_port $PGSQL_PORT postgres
         docker run --name PGSQL_${pgsql_version}_${IP_ADDRESS}_$j -v $SCRIPT_PWD/postgres:/docker-entrypoint-initdb.d/:rw -e POSTGRES_PASSWORD=${PGSQL_PASSWORD} -p $PGSQL_PORT:5432 -d postgres:${pgsql_version} -c shared_preload_libraries='pg_stat_statements' -c pg_stat_statements.max=10000 -c pg_stat_statements.track=all
-        docker exec PGSQL_${pgsql_version}_${IP_ADDRESS}_$j psql -h localhost -U postgres -c 'CREATE DATABASE mydat;'
         sleep 20
+        docker exec PGSQL_${pgsql_version}_${IP_ADDRESS}_$j psql -h localhost -U postgres -c 'CREATE DATABASE mydat;'
         if [ $(( ${j} % 2 )) -eq 0 ]; then
           if [[ ! -z $metrics_mode ]]; then
             pmm-admin add postgresql --username=postgres --password=${PGSQL_PASSWORD} --environment=pgsql-prod --cluster=pgsql-prod-cluster --metrics-mode=$metrics_mode --replication-set=pgsql-repl2 PGSQL_${pgsql_version}_${IP_ADDRESS}_$j localhost:$PGSQL_PORT
