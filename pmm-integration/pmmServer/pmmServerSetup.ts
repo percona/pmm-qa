e3b3dd574586c09b3310dd095c7d56b3d225dcb5
Peter <sirotnak.p@gmail.com>
2023-02-21 16:11:39 +0100

    temporary adds feature build testing

--- pmm-integration/pmmServer/pmmServerSetup.ts
+++ pmm-integration/pmmServer/pmmServerSetup.ts
@@ -9,7 +9,7 @@ const pmmServerSetup = async (parameters: SetupParameters) => {
     } else {
       portalVariables = '-e PERCONA_TEST_SAAS_HOST=check-dev.percona.com -e PERCONA_TEST_PLATFORM_ADDRESS=https://check-dev.percona.com:443'
     }
-    await executeCommand(`docker run -d --restart always ${portalVariables} --network="${dockerNetworkName}" --publish 80:80 --publish 443:443 --name ${pmmIntegrationServerName} perconalab/pmm-server:${parameters.pmmServerVersion}`);
+    await executeCommand(`docker run -d --restart always ${portalVariables} --network="${dockerNetworkName}" --publish 80:80 --publish 443:443 --name ${pmmIntegrationServerName} perconalab/pmm-server-fb:${parameters.pmmServerVersion}`);
 }
 
 export default pmmServerSetup
