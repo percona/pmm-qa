db6862c4949f2d8ace3c2b1f91436f1b56722515
Peter <sirotnak.p@gmail.com>
2023-03-02 10:28:07 +0100

    Fixes issue with pmm client in ci not using docker

--- pmm-integration/mongoDb/addClientMoDB.ts
+++ pmm-integration/mongoDb/addClientMoDB.ts
@@ -45,26 +45,26 @@ const addClientMoDB = async (parameters: SetupParameters, numberOfClients: numbe
       await executeCommand(`sudo docker exec -u 0  ${containerName} mongo -u mongoadmin -p ${password} mongodb_user_setup.js`);
     }
 
+    const prefix = parameters.ci ? 'sudo ' : `sudo docker exec  -u 0 ${pmmIntegrationClientName} `;
+
     if (parameters.useSocket) {
-      await executeCommand(
-        `sudo docker exec -u 0 ${pmmIntegrationClientName} chmod -R 0777 /tmp/mo-integration-${clientPort}/mongodb-27017.sock`,
-      );
+      await executeCommand(`${prefix} chmod -R 0777 /tmp/mo-integration-${clientPort}/mongodb-27017.sock`);
       if (parameters.metricsMode) {
-        await executeCommand(`sudo docker exec -u 0 ${pmmIntegrationClientName} pmm-admin add mongodb --cluster=${containerName} \
+        await executeCommand(`${prefix} pmm-admin add mongodb --cluster=${containerName} \
         --username=mongoadmin --password=${password} --metrics-mode=${parameters.metricsMode} \
         --environment=modb-prod ${containerName} --socket=/tmp/mo-integration-${clientPort}/mongodb-27017.sock --debug`);
       } else {
-        await executeCommand(`sudo docker exec -u 0 ${pmmIntegrationClientName} pmm-admin add mongodb --cluster=${containerName} \
+        await executeCommand(`${prefix} pmm-admin add mongodb --cluster=${containerName} \
         --username=mongoadmin --password=${password} --environment=modb-prod ${containerName} \
         --socket=/tmp/mo-integration-${clientPort}/mongodb-27017.sock --debug`);
       }
     } else if (parameters.metricsMode) {
-      await executeCommand(`sudo docker exec ${pmmIntegrationClientName} pmm-admin add mongodb --cluster=${containerName} \
+      await executeCommand(`${prefix} pmm-admin add mongodb --cluster=${containerName} \
         --username=mongoadmin --password=${password} --metrics-mode=${parameters.metricsMode} \
         --environment=modb-prod ${containerName} --debug ${containerName}:27017
         `);
     } else {
-      await executeCommand(`sudo docker exec ${pmmIntegrationClientName} pmm-admin add mongodb --cluster=${containerName} \
+      await executeCommand(`${prefix} pmm-admin add mongodb --cluster=${containerName} \
         --username=mongoadmin --password=${password} --environment=modb-prod ${containerName} --debug ${containerName}:27017
         `);
     }
