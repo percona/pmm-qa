a83137b896cce5b8d9549755ced7b73d543fbf49
Peter Sirotnak <sirotnak.p@gmail.com>
2023-03-10 14:44:17 +0100

    PMM-11697 (#575)
    * Changes for the different version of the client to the server.
    
    * Temporary comments out pmm admin list commands
    
    * Fixes typo
    
    * Fixes typo
    
    * pmm-admin list command.
    
    * Uncomment pmm-admin list commands
    
    * Log status and list commands
    
    * moves admin version command

--- pmm-tests/check_client_upgrade.sh
+++ pmm-tests/check_client_upgrade.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #check for packages after upgrade
-
+pmm-admin status
 pmm-admin status | grep -q Running
 pmm-admin status | grep node_exporter | grep -qv Waiting
 pmm-admin status | grep vmagent | grep -qv Waiting
@@ -11,25 +11,56 @@ pmm-admin status | grep mongodb_exporter | grep -qv Waiting
 pmm-admin status | grep postgres_exporter | grep -qv Waiting
 pmm-admin status | grep mongodb_profiler_agent | grep -qv Waiting
 pmm-admin status | grep postgresql_pgstatements_agent | grep -qv Waiting
+
+pmm-admin list 
+pmm-admin list | grep -q Running
+pmm-admin list | grep node_exporter | grep -qv Waiting
+pmm-admin list | grep vmagent | grep -qv Waiting
+pmm-admin list | grep mysqld_exporter | grep -qv Waiting
+pmm-admin list | grep mysql_perfschema_agent | grep -qv Waiting
+pmm-admin list | grep mongodb_exporter | grep -qv Waiting
+pmm-admin list | grep postgres_exporter | grep -qv Waiting
+pmm-admin list | grep mongodb_profiler_agent | grep -qv Waiting
+pmm-admin list | grep postgresql_pgstatements_agent | grep -qv Waiting
+
 server_version=$(pmm-admin status | grep Version | awk -F' ' '{print $2}')
 if [ "$server_version" != "$1" ]; then
     echo "PMM Server Version is not equal to expected $1";
     exit 1;
 fi
-admin_version=$(pmm-admin status | grep pmm-admin | awk -F' ' '{print $3}')
-if [ "$admin_version" != "$1" ]; then
-    echo "PMM Admin Version is not equal to expected $1";
-    exit 1;
+
+if [ -z "$2" ]; then
+    admin_version=$(pmm-admin status | grep pmm-admin | awk -F' ' '{print $3}')
+    if [ "$admin_version" != "$1" ]; then
+        echo "PMM Admin Version is not equal to expected $1";
+        exit 1;
+    fi
+    agent_version=$(pmm-admin status | grep pmm-agent | awk -F' ' '{print $3}')
+    if [ "$agent_version" != "$1" ]; then
+        echo "PMM Agent Version is not equal to expected $1";
+        exit 1;
+    fi
+    if [ "$agent_version" != "$admin_version" ]; then
+        echo "PMM-Agent Version Does not Match PMM-Admin Version";
+        exit 1;
+    fi
+else
+    admin_version=$(pmm-admin status | grep pmm-admin | awk -F' ' '{print $3}')
+    if [ "$admin_version" != "$2" ]; then
+        echo "PMM Admin Version is not equal to expected $2";
+        exit 1;
+    fi
+    agent_version=$(pmm-admin status | grep pmm-agent | awk -F' ' '{print $3}')
+    if [ "$agent_version" != "$2" ]; then
+        echo "PMM Agent Version is not equal to expected $2";
+        exit 1;
+    fi
+    if [ "$agent_version" != "$admin_version" ]; then
+        echo "PMM-Agent Version Does not Match PMM-Admin Version";
+        exit 1;
+    fi
 fi
-agent_version=$(pmm-admin status | grep pmm-agent | awk -F' ' '{print $3}')
-if [ "$agent_version" != "$1" ]; then
-    echo "PMM Agent Version is not equal to expected $1";
-    exit 1;
-fi
-if [ "$agent_version" != "$admin_version" ]; then
-    echo "PMM-Agent Version Does not Match PMM-Admin Version";
-    exit 1;
-fi
+
 ls -la /usr/local/percona/pmm2/exporters | grep -q azure_exporter
 ls -la /usr/local/percona/pmm2/exporters | grep -q mongodb_exporter
 ls -la /usr/local/percona/pmm2/exporters | grep -q mysqld_exporter
