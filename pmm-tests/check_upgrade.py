465ffae071e3c5c44a0de780f8562d067b96a9c1
Vadim Yarosh <ddmargarin@gmail.com>
2023-10-18 15:13:15 +0200

    PMM-7 migrated PMM container components tests to unittest (#645)
    * PMM-7 migrated PMM container components tests to unittest
    
    * PMM-12223 reduced lines and removed hardcoded messages
    
    * PMM-7 added pydoc and improved error messaging
    
    * PMM-7 pydoc
    
    * PMM-7 removed extra strip()
    
    * PMM-7 fixed 2.25.0 excluded test

--- pmm-tests/check_upgrade.py
+++ pmm-tests/check_upgrade.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 import argparse
-import subprocess
 import os
+import subprocess
+import sys
+import unittest
+
+NOT_RUNNING_MSG = 'Status is Not "running"!'
+WRONG_VERSION_MSG = 'Unexpected version!'
+POST_UPGRADE = 'Post-upgrade test'
+RUNNING = 'RUNNING'
 
 
 def parse_args():
@@ -10,117 +17,166 @@ def parse_args():
     parser.add_argument('-p', '--pre_post', help="Pass 'pre' from pre-upgrade stage and pass 'post' for post-upgrade "
                                                  "stage", default='post')
     parser.add_argument('-e', '--env', help="Pass 'ami' if it is AMI env, otherwise leave empty", default='not_ami')
-
-    args = parser.parse_args()
-
-    return args
+    return parser.parse_args()
 
 
 def verify_command(command):
-    with open("/tmp/output.log", "a") as output:
-        error_code = subprocess.call(command, shell=True, stdout=output, stderr=output)
-        if error_code != 0:
-            output = subprocess.getoutput(command.split("|")[0])
-            assert error_code == 0, f"expected to run {command} without errors. \nOutput: {output}"
+    """
+    Executes shell specified command and return the console output.
+    If the exit code was non-zero it raises a CalledProcessError.
+
+    :param  command:    shell command string to execute
+    :type   command:    str
+    :return:            command console output
+    :rtype:             str
+    :raises:            a CalledProcessError if the exit code was non-zero.
+    """
+    from tempfile import TemporaryFile
+    with TemporaryFile() as output:
+        return subprocess.check_output(command, shell=True, text=True, stderr=output).rstrip()
 
 
-def main():
+class PmmServerComponents(unittest.TestCase):
+    """
+    Collection of tests for the PMM Server container. Supports both "Docker/Podman" and "AMI" instances.
+
+    Also includes 2 suits of tests to verify PMM Server instance:
+        * general test (use '-p pre' commandline argument)
+        * upgraded from previous version (default one or use '-p post' commandline argument to switch explicitly)
+
+    How to run:
+        * python3 ./check_upgrade.py --env=ami --pre_post=pre --version=2.41.0
+        * python3 ./check_upgrade.py -v 2.41.0 -p post
+    """
+
+    def test_percona_qan_api2_version(self):
+        self.assertIn(expected_pmm_version, grep_rpm('percona-qan-api2-'), WRONG_VERSION_MSG)
+
+    def test_percona_dashboards_version(self):
+        self.assertIn(expected_pmm_version, grep_rpm('percona-dashboards-'), WRONG_VERSION_MSG)
+
+    def test_clickhouse_version(self):
+        """PMM-12223 - Verify Clickhouse is v23.8 or later since 2.41.0"""
+        if expected_pmm_minor_version < 41: self.skipTest('Since version 2.41.0')
+        if is_ami:
+            out = verify_command('sudo clickhouse local --version')
+        else:
+            out = verify_command(f"docker exec {pmm_server_docker_container} clickhouse local --version")
+        self.assertIn('23.8.2.7', out, WRONG_VERSION_MSG)
+
+    def test_pmm_update_version(self):
+        if expected_pmm_version == "2.25.0": self.skipTest('not fo 2.25.0!')
+        self.assertIn(expected_pmm_version, grep_rpm('pmm-update-'), WRONG_VERSION_MSG)
+
+    def test_pmm_managed_version(self):
+        self.assertIn(expected_pmm_version, grep_rpm('pmm-managed-'), WRONG_VERSION_MSG)
+
+    def test_pmm2_client_version(self):
+        self.assertIn(expected_pmm_version, grep_rpm('pmm2-client-'), WRONG_VERSION_MSG)
+
+    def test_dbaas_controller_version(self):
+        if test_mode != "post": self.skipTest(POST_UPGRADE)
+        self.assertIn(expected_pmm_version, grep_rpm('dbaas-controller-'), WRONG_VERSION_MSG)
+
+    def test_pmm_dump_version(self):
+        if test_mode != "post": self.skipTest(POST_UPGRADE)
+        self.assertIn(expected_pmm_version, grep_rpm('pmm-dump-'), WRONG_VERSION_MSG)
+
+    def test_qan_api2_status(self):
+        self.assertIn(RUNNING, grep_supervisor_status('qan-api2'), NOT_RUNNING_MSG)
+
+    def test_alert_manager_status(self):
+        self.assertIn(RUNNING, grep_supervisor_status('alertmanager'), NOT_RUNNING_MSG)
+
+    def test_clickhouse_status(self):
+        self.assertIn(RUNNING, grep_supervisor_status('clickhouse'), NOT_RUNNING_MSG)
+
+    def test_grafana_status(self):
+        self.assertIn(RUNNING, grep_supervisor_status('grafana'), NOT_RUNNING_MSG)
+
+    def test_nginx_status(self):
+        self.assertIn(RUNNING, grep_supervisor_status('nginx'), NOT_RUNNING_MSG)
+
+    def test_pmm_agent_status(self):
+        self.assertIn(RUNNING, grep_supervisor_status('pmm-agent'), NOT_RUNNING_MSG)
+
+    def test_pmm_managed_status(self):
+        self.assertIn(RUNNING, grep_supervisor_status('pmm-managed'), NOT_RUNNING_MSG)
+
+    def test_postgresql_status(self):
+        self.assertIn(RUNNING, grep_supervisor_status('postgresql'), NOT_RUNNING_MSG)
+
+    def test_victoriametrics_status(self):
+        if test_mode != "post": self.skipTest(POST_UPGRADE)
+        self.assertIn(RUNNING, grep_supervisor_status('victoriametrics'), NOT_RUNNING_MSG)
+
+    def test_victoria_metrics_alert_status(self):
+        if test_mode != "post": self.skipTest(POST_UPGRADE)
+        self.assertIn(RUNNING, grep_supervisor_status('vmalert'), NOT_RUNNING_MSG)
+
+    def test_victoria_metrics_version(self):
+        if test_mode != "post": self.skipTest(POST_UPGRADE)
+        if is_ami:
+            out = verify_command('sudo victoriametrics --version')
+        else:
+            out = verify_command(f"docker exec {pmm_server_docker_container} victoriametrics --version")
+        self.assertIn('v1.93.4', out, WRONG_VERSION_MSG)
+
+    def test_vertamedia_clickhouse_plugin_absent(self):
+        # """PMM-T1758 - Verify vertamedia-clickhouse-datasource plugin is not installed after upgrade to 2.38.0"""
+        if expected_pmm_minor_version < 38: self.skipTest('Since version 2.41.0')
+        if is_ami:
+            out = verify_command(f"{grafana_cli} plugins ls")
+        else:
+            out = verify_command(
+                f"docker exec -e GF_PLUGIN_DIR=/srv/grafana/plugins/ "
+                f"{pmm_server_docker_container} {grafana_cli} plugins ls")
+        self.assertNotIn('vertamedia', out, 'Must be absent!')
+
+        # Disabled tests AMI+Docker:
+        # verify_command('grafana cli plugins ls | grep alexanderzobnin-zabbix-app')
+        # if (do_docker_way == "yes" and pmm_minor_v > 22) or (do_docker_way != "yes"):
+        #     verify_command(
+        #         f"docker exec -e GF_PLUGIN_DIR=/srv/grafana/plugins/ {pmm_server_docker_container} "
+        #         f"{grafana_cli} plugins ls | grep alexanderzobnin-zabbix-app")
+
+        # verify_command('grafana cli plugins ls | grep "vertamedia-clickhouse-datasource @ 2.4.4"')
+        # verify_command(
+        #     f"docker exec -e GF_PLUGIN_DIR=/srv/grafana/plugins/ {pmm_server_docker_container} "
+        #     f"{grafana_cli} plugins ls | grep \"vertamedia-clickhouse-datasource @ 2.4.4\"")
+
+
+if __name__ == '__main__':
     args = parse_args()
-    pmm_minor_v = int(args.version.split('.')[1])
+    expected_pmm_version = args.version
+    expected_pmm_minor_version = int(args.version.split('.')[1])
+    test_mode = args.pre_post
+    is_ami = args.env in "ami"
 
-    if args.env in "ami":
-        verify_command('rpm -qa | grep percona-qan-api2-' + args.version)
-        verify_command('rpm -qa | grep percona-dashboards-' + args.version)
-        if args.version == "2.25.0":
-            verify_command('rpm -qa | grep pmm-update-' + args.version)
-
-        verify_command('rpm -qa | grep pmm-managed-' + args.version)
-        verify_command('rpm -qa | grep pmm2-client-' + args.version)
-        verify_command('sudo supervisorctl status | grep qan-api2 | grep RUNNING')
-        verify_command('sudo supervisorctl status | grep alertmanager | grep RUNNING')
-        verify_command('sudo supervisorctl status | grep clickhouse | grep RUNNING')
-        verify_command('sudo supervisorctl status | grep grafana | grep RUNNING')
-        verify_command('sudo supervisorctl status | grep nginx | grep RUNNING')
-        verify_command('sudo supervisorctl status | grep pmm-agent | grep RUNNING')
-        verify_command('sudo supervisorctl status | grep pmm-managed | grep RUNNING')
-        verify_command('sudo supervisorctl status | grep postgresql | grep RUNNING')
-
-        if args.pre_post == "post":
-            verify_command('rpm -qa | grep dbaas-controller-' + args.version)
-            verify_command('rpm -qa | grep pmm-dump-' + args.version)
-            verify_command('sudo supervisorctl status | grep victoriametrics | grep RUNNING')
-            verify_command('sudo supervisorctl status | grep vmalert | grep RUNNING')
-            # verify_command('grafana cli plugins ls | grep "vertamedia-clickhouse-datasource @ 2.4.4"')
-            # verify_command('grafana cli plugins ls | grep alexanderzobnin-zabbix-app')
-            verify_command('sudo victoriametrics --version | grep v1.93.4')
-    else:
-        pmm_server_docker_container = subprocess.getoutput("docker ps --format \"table {{.ID}}\t{{.Image}}\t{{"
-                                                           ".Names}}\" | grep 'pmm-server' | awk '{print $3}'")
-        print(f"PMM Server container name is {pmm_server_docker_container}, verification for {args.pre_post}")
-
-        verify_command(f"docker exec {pmm_server_docker_container} rpm -qa | grep percona-qan-api2-{args.version}")
-        verify_command(
-            f"docker exec {pmm_server_docker_container} rpm -qa | grep percona-dashboards-{args.version}")
-
-        ### PMM-12223 - Verify Clickhouse  is v22.8 or later since 2.41.0
-        if pmm_minor_v >= 41:
-            verify_command(f"docker exec {pmm_server_docker_container} clickhouse local --version | grep 23.8.2.7")
-
-        if args.version != "2.25.0":
-            verify_command(f"docker exec {pmm_server_docker_container} rpm -qa | grep pmm-update-{args.version}")
-        verify_command(f"docker exec {pmm_server_docker_container} rpm -qa | grep pmm-managed-{args.version}")
-        verify_command(f"docker exec {pmm_server_docker_container} rpm -qa | grep pmm2-client-{args.version}")
-        verify_command(f"docker exec {pmm_server_docker_container} supervisorctl status | grep qan-api2 | grep "
-                       f"RUNNING")
-        verify_command(f"docker exec {pmm_server_docker_container} supervisorctl status | grep alertmanager | grep "
-                       f"RUNNING")
-        verify_command(f"docker exec {pmm_server_docker_container} supervisorctl status | grep clickhouse | grep "
-                       f"RUNNING")
-        verify_command(f"docker exec {pmm_server_docker_container} supervisorctl status | grep grafana | grep "
-                       f"RUNNING")
-        verify_command(f"docker exec {pmm_server_docker_container} supervisorctl status | grep nginx | grep "
-                       f"RUNNING")
-        verify_command(f"docker exec {pmm_server_docker_container} supervisorctl status | grep pmm-agent | grep "
-                       f"RUNNING")
-        verify_command(f"docker exec {pmm_server_docker_container} supervisorctl status | grep pmm-managed | grep "
-                       f"RUNNING")
-        verify_command(f"docker exec {pmm_server_docker_container} supervisorctl status | grep postgresql | grep "
-                       f"RUNNING")
-        if args.pre_post == "post":
-            verify_command(
-                f"docker exec {pmm_server_docker_container} rpm -qa | grep dbaas-controller-{args.version}")
-            verify_command(
-                f"docker exec {pmm_server_docker_container} rpm -qa | grep pmm-dump-{args.version}")
-            verify_command(
-                f"docker exec {pmm_server_docker_container} supervisorctl status | grep victoriametrics | grep "
-                f"RUNNING")
-            verify_command(
-                f"docker exec {pmm_server_docker_container} supervisorctl status | grep vmalert | grep "
-                f"RUNNING")
-            verify_command(
-                f"docker exec {pmm_server_docker_container} victoriametrics --version | grep v1.93.4")
-
-            docker_version = os.getenv("DOCKER_VERSION")
-            do_docker_way = os.getenv("PERFORM_DOCKER_WAY_UPGRADE")
-
-            grafana_cli = "grafana cli" if pmm_minor_v >= 39 else "grafana-cli"
-
-            ### PMM-T1758 - Verify vertamedia-clickhouse-datasource plugin is not installed after upgrade to 2.38.0
-            if pmm_minor_v >= 38:
-                verify_command(
-                    f"docker exec -e GF_PLUGIN_DIR=/srv/grafana/plugins/ "
-                    f"{pmm_server_docker_container} {grafana_cli} plugins ls | exit $(grep -c vertamedia)")
-
-            # if (do_docker_way == "yes" and pmm_minor_v > 22) or (do_docker_way != "yes"):
-            #     verify_command(
-            #         f"docker exec -e GF_PLUGIN_DIR=/srv/grafana/plugins/ {pmm_server_docker_container} "
-            #         f"{grafana_cli} plugins ls | grep alexanderzobnin-zabbix-app")
-
-            # verify_command(
-            #     f"docker exec -e GF_PLUGIN_DIR=/srv/grafana/plugins/ {pmm_server_docker_container} "
-            #     f"{grafana_cli} plugins ls | grep \"vertamedia-clickhouse-datasource @ 2.4.4\"")
-            print(f"Post upgrade verification complete! for {args.version}")
+    do_docker_way = os.getenv("PERFORM_DOCKER_WAY_UPGRADE")
+    grafana_cli = "grafana cli" if expected_pmm_minor_version >= 39 else "grafana-cli"
+    if not is_ami:
+        pmm_server_docker_container = verify_command(
+            """docker ps --format "table {{.ID}}\t{{.Image}}\t{{.Names}}" | grep 'pmm-server' | awk '{print $3}'""")
+        assert pmm_server_docker_container, "No docker container found!"
 
 
-if __name__ == "__main__":
-    main()
+    def grep_rpm(query):
+        """Polymorphic shortcut to use in test"""
+        if is_ami:
+            return verify_command(f"rpm -qa | grep {query}")
+        else:
+            return verify_command(f"docker exec {pmm_server_docker_container} rpm -qa | grep {query}")
+
+
+    def grep_supervisor_status(name):
+        """Polymorphic shortcut to use in test"""
+        if is_ami:
+            return verify_command(f"sudo supervisorctl status | grep {name}")
+        else:
+            return verify_command(f"docker exec {pmm_server_docker_container} supervisorctl status | grep {name}")
+
+
+    # leaving sys.argv[0] alone
+    del sys.argv[1:]
+    unittest.main(verbosity=2)
