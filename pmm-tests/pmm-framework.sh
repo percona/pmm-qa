a178eb48f00f4d731cc1df753207510993816cb1
Vasyl Yurkovych <59879559+yurkovychv@users.noreply.github.com>
2023-10-18 14:46:54 +0300

    PMM-12549 mongo 7 by default (#647)

--- pmm-tests/pmm-framework.sh
+++ pmm-tests/pmm-framework.sh
@@ -3042,7 +3042,7 @@ setup_mongo_replica_for_backup() {
   fi
   mkdir /tmp/backup_data && chmod 777 /tmp/backup_data
   pushd qa-integration/pmm_psmdb-pbm_setup
-  PSMDB_VERSION=4.4.18-18 COMPOSE_PROFILES=extra ./start-rs-only.sh
+  PSMDB_VERSION=7.0.2-1 COMPOSE_PROFILES=extra ./start-rs-only.sh
   popd
   popd
 }
