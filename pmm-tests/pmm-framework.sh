0c3a1e17211976a2ebf716f1c2e510cec370ab92
Vasyl Yurkovych <59879559+yurkovychv@users.noreply.github.com>
2024-03-11 17:24:38 +0200

    PMM-7 test changes

--- pmm-tests/pmm-framework.sh
+++ pmm-tests/pmm-framework.sh
@@ -3043,7 +3043,7 @@ setup_mongo_replica_for_backup() {
   mkdir -p /tmp/mongodb_backup_replica || :
   pushd /tmp/mongodb_backup_replica
   if [ ! -d "qa-integration" ]; then
-    git clone -b main https://github.com/Percona-Lab/qa-integration
+    git clone -b PMM-7-improve-pmm-rc-setup https://github.com/Percona-Lab/qa-integration
   fi
   mkdir /tmp/backup_data && chmod 777 /tmp/backup_data
   pushd qa-integration/pmm_psmdb-pbm_setup
