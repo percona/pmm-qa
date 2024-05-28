4571931cbf83e5f53b3252f428971ba3b91405b6
Saikumar <saikumar.vs@percona.com>
2024-05-28 12:43:57 +0530

    Updates for user_stat

--- pmm-tests/client_container_ms_setup.sh
+++ pmm-tests/client_container_ms_setup.sh
@@ -59,6 +59,7 @@ if [[ $number_of_nodes == 1 ]];then
       export db_sandbox=$(dbdeployer sandboxes | awk -F' ' '{print $1}')
       node_port=`dbdeployer sandboxes --header | grep  ${db_version_sandbox} | grep 'single' | awk -F'[' '{print $2}' | awk -F' ' '{print $1}'`
       mysql -h 127.0.0.1 -u msandbox -pmsandbox --port $node_port -e "ALTER USER 'msandbox'@'localhost' IDENTIFIED WITH mysql_native_password BY 'msandbox';"
+      mysql -h 127.0.0.1 -u msandbox -pmsandbox --port $node_port -e "SET GLOBAL userstat=1;"
       mysql -h 127.0.0.1 -u msandbox -pmsandbox --port $node_port -e "SET GLOBAL innodb_monitor_enable=all;"
       mysql -h 127.0.0.1 -u msandbox -pmsandbox --port $node_port -e "UPDATE performance_schema.setup_consumers SET ENABLED = 'YES' WHERE NAME LIKE '%statements%';"
       if [[ "${query_source}" == "slowlog" ]]; then
