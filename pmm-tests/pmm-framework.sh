5a7d53b35237a66d304e9e859ccfb9fed53f7d35
Saikumar <saikumar.vs@percona.com>
2024-06-03 12:50:44 +0530

    Updates for docker only PS installation

--- pmm-tests/pmm-framework.sh
+++ pmm-tests/pmm-framework.sh
@@ -1606,6 +1606,8 @@ add_clients(){
           mysql -h 127.0.0.1 -u root -p${PS_PASSWORD} --port $PS_PORT -e "SET GLOBAL userstat=1;"
           mysql -h 127.0.0.1 -u root -p${PS_PASSWORD} --port $PS_PORT -e "SET GLOBAL innodb_monitor_enable=all;"
           mysql -h 127.0.0.1 -u root -p${PS_PASSWORD} --port $PS_PORT -e "ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'GRgrO9301RuF';"
+          mysql -h 127.0.0.1 -u root -p${PS_PASSWORD} --port $PS_PORT -e "UPDATE performance_schema.setup_consumers SET ENABLED = 'YES' WHERE NAME LIKE '%statements%';"
+	  
           if [[ "$query_source" != "perfschema" ]]; then
             mysql -h 127.0.0.1 -u root -p${PS_PASSWORD} --port $PS_PORT -e "SET GLOBAL slow_query_log='ON';"
             mysql -h 127.0.0.1 -u root -p${PS_PASSWORD} --port $PS_PORT -e "SET GLOBAL long_query_time=0;"
