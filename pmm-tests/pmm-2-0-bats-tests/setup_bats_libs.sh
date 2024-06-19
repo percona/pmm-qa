65ba30d8caf9409a271f8bd34c65859c65cec615
Vasyl Yurkovych <59879559+yurkovychv@users.noreply.github.com>
2024-06-19 16:03:04 +0300

    PMM-13170 fix helm tests (#694)
    * PMM-13170 fix helm tests
    
    * PMM-13170 fix helm tests
    
    * PMM-13170 fix helm tests
    
    * PMM-13170 fix helm tests
    
    * PMM-13170 fix helm tests
    
    * PMM-13170 remove pull request event
    
    * PMM-13170 fix helm tests

---
+++ pmm-tests/pmm-2-0-bats-tests/setup_bats_libs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Define the directory where the libraries will be cloned
+LIBS_DIR="lib"
+
+# Create the directory if it doesn't exist
+mkdir -p $LIBS_DIR
+
+# Clone bats-support
+if [ ! -d "$LIBS_DIR/bats-support" ]; then
+    git clone https://github.com/bats-core/bats-support.git $LIBS_DIR/bats-support
+else
+    echo "bats-support is already present."
+fi
+
+# Clone bats-assert
+if [ ! -d "$LIBS_DIR/bats-assert" ]; then
+    git clone https://github.com/bats-core/bats-assert.git $LIBS_DIR/bats-assert
+else
+    echo "bats-assert is already present."
+fi
+
+echo "BATS libraries setup completed."
