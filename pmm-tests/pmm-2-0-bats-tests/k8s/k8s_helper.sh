cc1a445e7528f59b7c309c75ea86d8982a0b5130
Vasyl Yurkovych <59879559+yurkovychv@users.noreply.github.com>
2024-06-19 16:03:04 +0300

    PMM-13170 fix helm tests (#694)
    * PMM-13170 fix helm tests
    
    * PMM-13170 fix helm tests
    
    * PMM-13170 fix helm tests
    
    * PMM-13170 fix helm tests
    
    * PMM-13170 fix helm tests
    
    * PMM-13170 remove pull request event
    
    * PMM-13170 fix helm tests

---
+++ pmm-tests/pmm-2-0-bats-tests/k8s/k8s_helper.sh
@@ -0,0 +1,39 @@
+wait_for_pmm(){
+    sleep 5
+    kubectl wait --for=condition=Ready --selector=app.kubernetes.io/name=pmm pod --timeout=5m
+}
+
+delete_pvc(){
+    kubectl delete pvc --selector=app.kubernetes.io/name=pmm
+    kubectl wait --for=delete --selector=app.kubernetes.io/name=pmm pvc --timeout=5m
+}
+
+get_pmm_pswd(){
+    kubectl get secret pmm-secret -o jsonpath='{.data.PMM_ADMIN_PASSWORD}' | base64 --decode
+}
+
+# Function to start port forwarding
+start_port_forward(){
+    POD_NAME=$(kubectl get pods -n default -l app.kubernetes.io/name=pmm -o jsonpath='{.items[0].metadata.name}')
+    kubectl port-forward $POD_NAME 8443:443 -n default &
+    PORT_FORWARD_PID=$!
+    echo $PORT_FORWARD_PID > port_forward.pid
+    sleep 5 # Give port forwarding some time to set up
+}
+
+# Function to stop port forwarding
+stop_port_forward(){
+    if [ -f port_forward.pid ]; then
+        PORT_FORWARD_PID=$(cat port_forward.pid)
+        kill $PORT_FORWARD_PID || true
+        rm port_forward.pid
+    fi
+}
+
+# Retrieves the value of a specified environment variable from the first pod of a given Kubernetes instance.
+get_env_variable(){
+    local instance_name=$1
+    local env_var=$2
+    local pod_name=$(kubectl get pods -l app.kubernetes.io/instance=$instance_name -o jsonpath='{.items[0].metadata.name}')
+    kubectl exec -it $pod_name -- printenv $env_var
+}
