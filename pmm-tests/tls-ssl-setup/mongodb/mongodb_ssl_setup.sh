d4b140d6e46e15a4ea77741ab6a0efa88838f7f4
saikumar-vs <112166980+saikumar-vs@users.noreply.github.com>
2024-08-19 19:15:02 +0530

    Update mongodb_ssl_setup.sh
    Add additional check till 7.0 is also fixed

--- pmm-tests/tls-ssl-setup/mongodb/mongodb_ssl_setup.sh
+++ pmm-tests/tls-ssl-setup/mongodb/mongodb_ssl_setup.sh
@@ -22,7 +22,11 @@ wget https://raw.githubusercontent.com/percona/pmm-qa/main/pmm-tests/mongodb_use
 
 ### Detect latest tarball link for specified mongodb_version: 7.0 | 6.0 | 5.0 | 4.4 | 4.2 at the moment
 psmdb_latest=$(wget -q --post-data "version=percona-server-mongodb-${mongodb_version}" https://www.percona.com/products-api.php -O - | grep  -oP "(?<=value\=\")[^\"]*" | sort -V | tail -1)
-psmdb_tarball=$(wget -q --post-data "version_files=${psmdb_latest}&software_files=binary" https://www.percona.com/products-api.php -O - | jq -r '.[] | select(.link | contains("sha") | not) | .link' | grep focal-minimal)
+if [[ "$mongodb_version" == "5.0" || "$mongodb_version" == "6.0" ]]; then
+   psmdb_tarball=$(wget -q --post-data "version_files=${psmdb_latest}&software_files=binary" https://www.percona.com/products-api.php -O - | jq -r '.[] | select(.link | contains("sha") | not) | .link' | grep focal-minimal)
+else
+   psmdb_tarball=$(wget -q --post-data "version_files=${psmdb_latest}&software_files=binary" https://www.percona.com/products-api.php -O - | jq -r '.[] | select(.link | contains("sha") | not) | .link' | grep glibc2\.17-minimal)
+fi
 
 echo "Downloading ${psmdb_latest} ..."
 wget -O percona_server_mongodb.tar.gz ${psmdb_tarball}
