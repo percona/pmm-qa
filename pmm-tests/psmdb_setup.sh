487a005be5083d11ff46208f9001cbbd75024acc
yurkovychv <vasyl.yurkovych@percona.com>
2024-10-21 17:26:59 +0300

    PMM-7 PSMDB setup 8.0

--- pmm-tests/psmdb_setup.sh
+++ pmm-tests/psmdb_setup.sh
@@ -40,7 +40,7 @@ psmdb_latest=$(wget -q --post-data "version=percona-server-mongodb-${mongodb_ver
 if [[ "$mongodb_version" == "4.4" ]]; then
    psmdb_tarball=$(wget -q --post-data "version_files=${psmdb_latest}&software_files=binary" https://www.percona.com/products-api.php -O - | jq -r '.[] | select(.link | contains("sha") | not) | .link' | grep glibc2\.17-minimal)
 elif [[ "$mongodb_version" == "8.0" ]]; then
-   psmdb_tarball=https://downloads.percona.com/downloads/TESTING/psmdb-8.0.1/percona-server-mongodb-8.0.1-1-x86_64.focal-minimal.tar.gz?_gl=1*1jj4ykm*_gcl_au*MTcyMjk2MjY2Mi4xNzI1NDQ2MzY1
+   psmdb_tarball="https://downloads.percona.com/downloads/TESTING/psmdb-8.0.1/percona-server-mongodb-8.0.1-1-x86_64.focal-minimal.tar.gz?_gl=1*1jj4ykm*_gcl_au*MTcyMjk2MjY2Mi4xNzI1NDQ2MzY1"
 else
    psmdb_tarball=$(wget -q --post-data "version_files=${psmdb_latest}&software_files=binary" https://www.percona.com/products-api.php -O - | jq -r '.[] | select(.link | contains("sha") | not) | .link' | grep focal-minimal)
 fi
