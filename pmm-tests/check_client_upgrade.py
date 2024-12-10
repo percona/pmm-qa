1da3455c2b3d33dcdb4bd9cc22b46fd72b2afbe6
Peter Sirotnak <sirotnak.p@gmail.com>
2024-12-10 11:04:20 +0100

    PMM-7: python file

--- pmm-tests/check_client_upgrade.py
+++ pmm-tests/check_client_upgrade.py
@@ -4,13 +4,26 @@ containers = subprocess.run(["docker", "ps", "-a"], capture_output=True, text=Tr
 
 psContainerName = ""
 pgsqlContainerName = ""
+firstMongoReplica = ""
+secondMongoReplica = ""
+thirdMongoReplica = ""
 
 print(containers)
 
 for i in range(len(containers)):
+  print(containers[i])
   if "ps_pmm_" in containers[i]:
       psContainerName = containers[i][containers[i].index("ps_pmm"):]
       print(psContainerName)
   elif "pgsql_pgss_pmm" in containers[i]:
       pgsqlContainerName = containers[i][containers[i].index("pgsql_pgss_pmm"):]
       print(pgsqlContainerName)
+  elif "rs101" in containers[i]:
+    firstMongoReplica = containers[i][containers[i].index("rs101"):]
+    print(firstMongoReplica)
+  elif "rs102" in containers[i]:
+    secondMongoReplica = containers[i][containers[i].index("rs102"):]
+    print(secondMongoReplica)
+  elif "rs103" in containers[i]:
+    thirdMongoReplica = containers[i][containers[i].index("rs103"):]
+    print(thirdMongoReplica)
