141a12d391a0f8a666126fa305784395a82ab089
Peter Sirotnak <sirotnak.p@gmail.com>
2024-12-10 08:08:17 +0100

    PMM-7: python file

--- pmm-tests/check_client_upgrade.py
+++ pmm-tests/check_client_upgrade.py
@@ -2,4 +2,7 @@ import subprocess
 
 containers = subprocess.run(["docker", "ps", "-a"], capture_output=True, text=True) .stdout.splitlines()
 
-print(containers)
+for i in range(len(containers)):
+  print(containers[i])
+  if "ps_pmm_" in containers[i]:
+      print(containers[i])
