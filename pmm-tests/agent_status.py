fb6d471546b58fd4b0f1e48d79b0167d9693c274
Peter Sirotnak <sirotnak.p@gmail.com>
2025-06-25 15:35:44 +0200

    PMM-7: Cleanup

--- pmm-tests/agent_status.py
+++ pmm-tests/agent_status.py
@@ -20,7 +20,6 @@ def verify_command(command):
 
 if __name__ == '__main__':
     containerNames = verify_command("docker ps -a --format '{{.Names}}'")
-    print(containerNames)
     errors = []
     services = ['ps_', 'mysql_', 'pxc_', 'pgsql_', 'rs10']
 
