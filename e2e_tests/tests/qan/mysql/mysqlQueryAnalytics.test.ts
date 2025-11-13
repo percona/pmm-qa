0ffebffa22bdee97608c8f1c53e0387d5806ed18
Peter Sirotnak <sirotnak.p@gmail.com>
2025-11-13 10:16:13 +0100

    PMM-7: Exec fixes

--- e2e_tests/tests/qan/mysql/mysqlQueryAnalytics.test.ts
+++ e2e_tests/tests/qan/mysql/mysqlQueryAnalytics.test.ts
@@ -4,7 +4,7 @@ import { Timeouts } from '@helpers/timeouts';
 
 pmmTest.beforeAll(async ({ cliHelper, credentials }) => {
   const containerName = cliHelper.execute('docker ps --filter \'name=(ps|mysql)\' --format "{{.Names }}"');
-  cliHelper.execute(`docker exec -i ${containerName} mysql -h 127.0.0.1 --port 3306 \
+  cliHelper.execute(`docker exec -i ${containerName.stdout} mysql -h 127.0.0.1 --port 3306 \
                                       -u ${credentials.perconaServer.ps_84.username} \
                                       -p${credentials.perconaServer.ps_84.password} \
                                       < \${PWD}/testdata/PMM-T1897.sql`);
