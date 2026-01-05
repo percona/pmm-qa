65a68ec75dcea7fda85813821918c1d1f8cfefea
kiranvuyurru <kiran.vuyurru@percona.com>
2026-01-05 11:09:28 -0700

    New left menu test
    1. verify the collapse and expand of the left menu
    2. Traverse all menu and check no 404 response
    3. non admin RBAC
    4. Time range persists on various dashboards
    5. old navigation menu not visible and iframe visibility test

--- e2e_tests/tests/navigation.test.ts
+++ e2e_tests/tests/navigation.test.ts
@@ -1,18 +1,13 @@
 import pmmTest from '@fixtures/pmmTest';
 import { expect } from '@playwright/test';
 
-pmmTest.beforeEach(async ({ page, grafanaHelper }, testInfo) => {
-    if (testInfo.title.includes('RBAC/permissions')) {
-        return;
-    }
+pmmTest.beforeEach(async ({ page, grafanaHelper }) => {
     await page.goto('');
     await grafanaHelper.authorize();
     await page.waitForLoadState('domcontentloaded');
 });
 
 pmmTest('verify left menu sidebar collapse and expand', async ({ page, leftNavigation }) => {
-
-    const pmmLogo = leftNavigation.elements.sidebar.locator('rect');
 
     await expect(leftNavigation.elements.sidebar).toBeVisible();
     await expect(leftNavigation.elements.closeLeftNavigationButton).toBeVisible();
@@ -26,20 +21,14 @@ pmmTest('verify left menu sidebar collapse and expand', async ({ page, leftNavig
     await expect(leftNavigation.elements.openLeftNavigationButton).toBeVisible();
     await expect(leftNavigation.elements.closeLeftNavigationButton).not.toBeVisible();
 
-    const rectBox = await pmmLogo.boundingBox();
-    if (rectBox) {
-        await page.mouse.move(
-            rectBox.x + rectBox.width / 2,
-            rectBox.y + rectBox.height / 2
-        );
-    }
+    await leftNavigation.mouseHoverOnPmmLogo();
 
     await leftNavigation.elements.openLeftNavigationButton.click();
     await expect(leftNavigation.elements.closeLeftNavigationButton).toBeVisible();
     await expect(leftNavigation.elements.openLeftNavigationButton).not.toBeVisible();
 });
 
-pmmTest('Traverse all the menu items in left menu sidebar', async ({ page, leftNavigation }) => {
+pmmTest('Traverse all the menu items in left menu sidebar', async ({ leftNavigation }) => {
 
     pmmTest.slow(); // Test is taking so long to run due to many menu items
 
@@ -50,32 +39,40 @@ pmmTest('Traverse all the menu items in left menu sidebar', async ({ page, leftN
     await leftNavigation.traverseAllMenuItems();
 });
 
-pmmTest('RBAC/permissions', async ({ page, leftNavigation }) => {
+pmmTest('RBAC/permissions', async ({ leftNavigation, grafanaHelper }) => {
 
-    await page.goto('');
-    await leftNavigation.loginUser('admin', process.env.ADMIN_PASSWORD! || 'admin');
-    if (await leftNavigation.elements.skipButton.isVisible()) {
-        await leftNavigation.elements.skipButton.click();
-    }
     await leftNavigation.elements.usersAndAccess.click();
+    await grafanaHelper.createUser('nonadmin', 'nonadmin');
 
-    await leftNavigation.createNonAdminUser();
     await leftNavigation.elements.accounts.click();
     await leftNavigation.elements.accountsMenu.signOut.click();
 
-    await leftNavigation.loginUser('nonadmin', 'nonadmin');
+    await grafanaHelper.authorize('nonadmin', 'nonadmin');
     await expect(leftNavigation.elements.configuration).not.toBeVisible();
     await leftNavigation.elements.help.click();
     await expect(leftNavigation.elements.dumpLogs).not.toBeVisible();
 
 });
 
-pmmTest('121 - verify custom time range persists on any dashboard', async ({ page, leftNavigation }) => {
-    const expectedTimeRange = 'Last 15 minutes';
+pmmTest('verify custom time range persists on any dashboard', async ({ page, leftNavigation }) => {
+    const selectedTimeRange = 'Last 15 minutes';
 
     await leftNavigation.elements.home.click();
     await leftNavigation.elements.timePickerOpenButton.click();
-    await leftNavigation.getTimeRangeOption(expectedTimeRange).click();
+    await leftNavigation.getTimeRangeOption(selectedTimeRange).click();
+    await page.waitForLoadState('domcontentloaded');
+    await leftNavigation.verifyTimeRangePersistence(selectedTimeRange);
+});
 
-    await leftNavigation.verifyTimeRangePersistence(expectedTimeRange);
+pmmTest('Grafana embedding', async ({ leftNavigation }) => {
+
+    await expect(leftNavigation.elements.oldLeftMenu).not.toBeVisible();
+
+    await leftNavigation.elements.help.click();
+    await expect(leftNavigation.elements.iframe).not.toBeVisible();
+
+    await leftNavigation.elements.home.click();
+    await expect(leftNavigation.elements.iframe).toBeVisible();
+    await leftNavigation.elements.help.click();
+    await expect(leftNavigation.elements.iframe).toBeVisible();
 });
