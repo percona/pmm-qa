b49bf3cca40f37a93db5e10c73bfcc45a969c7e3
kiranvuyurru <kiran.vuyurru@percona.com>
2026-01-05 11:09:28 -0700

    New left menu test
    1. verify the collapse and expand of the left menu
    2. Traverse all menu and check no 404 response
    3. non admin RBAC
    4. Time range persists on various dashboards
    5. old navigation menu not visible and iframe visibility test

--- e2e_tests/helpers/grafana.helper.ts
+++ e2e_tests/helpers/grafana.helper.ts
@@ -1,7 +1,7 @@
 import { Page } from '@playwright/test';
 
 export default class GrafanaHelper {
-  constructor(private page: Page) {}
+  constructor(private page: Page) { }
 
   async authorize(username = 'admin', password = process.env.ADMIN_PASSWORD || 'admin', baseUrl = '') {
     const authToken = GrafanaHelper.getToken(username, password);
@@ -20,5 +20,21 @@ export default class GrafanaHelper {
 
   static getToken(username = 'admin', password = process.env.ADMIN_PASSWORD || 'admin') {
     return Buffer.from(`${username}:${password}`).toString('base64');
+  }
+
+
+  // creates non admin user
+  async createUser(username: string, password: string) {
+    const authToken = GrafanaHelper.getToken();
+    const response = await this.page.request.post('graph/api/admin/users', {
+      headers: { Authorization: `Basic ${authToken}` },
+      data: {
+        name: username,
+        login: username,
+        password: password,
+        OrgId: 1
+      }
+    });
+    return response;
   }
 }
