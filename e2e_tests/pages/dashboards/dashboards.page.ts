908c5437abed440863d0687d8e93dcf780db51a4
Peter Sirotnak <sirotnak.p@gmail.com>
2026-01-09 12:58:34 +0100

    PMM-14538: Refactors panels

--- e2e_tests/pages/dashboards/dashboards.page.ts
+++ e2e_tests/pages/dashboards/dashboards.page.ts
@@ -2,11 +2,6 @@ import { Page, expect, test } from '@playwright/test';
 import { GrafanaPanel } from '@interfaces/grafanaPanel';
 import { GetService } from '@interfaces/inventory';
 import { replaceWildcards } from '@helpers/metrics.helper';
-import TimeSeriesPanel from '@components/dashboards/panels/timeSeries.component';
-import StatPanel from '@components/dashboards/panels/stat.component';
-import BarGaugePanel from '@components/dashboards/panels/barGauge.component';
-import PolyStatPanel from '@components/dashboards/panels/polyStat.component';
-import TablePanel from '@components/dashboards/panels/table.component';
 import MysqlInstanceOverview from '@pages/dashboards/mysql/mysqlInstanceOverview';
 import {
   ValkeyClientsDashboard,
@@ -21,10 +16,8 @@ import {
   ValkeySlowlogDashboard,
 } from '@valkey';
 import { Timeouts } from '@helpers/timeouts';
-import TextPanel from '@components/dashboards/panels/text.component';
 import MysqlDashboards from '@pages/dashboards/mysql/mysql.dashboards';
-import StateTimePanel from '@components/dashboards/panels/stateTime.component';
-import BarTimePanel from '@components/dashboards/panels/barTime.component';
+import Panels from '@components/dashboards/panels';
 
 export const valkeyDashboards = {
   'Valkey Overview': new ValkeyOverviewDashboard(),
@@ -41,32 +34,18 @@ export const valkeyDashboards = {
 
 export default class Dashboards {
   private readonly page: Page;
-  private readonly timeSeriesPanel: TimeSeriesPanel;
-  private readonly statPanel: StatPanel;
-  private readonly barGaugePanel: BarGaugePanel;
-  private readonly barTimePanel: BarTimePanel;
-  private readonly polyStatPanel: PolyStatPanel;
-  private readonly tablePanel: TablePanel;
-  private readonly textPanel: TextPanel;
-  private readonly stateTime: StateTimePanel;
   // MySQL dashboards
   readonly mysql: MysqlDashboards;
   readonly mysqlInstanceOverview: MysqlInstanceOverview;
   // Valkey dashboards
   readonly valkeyDashboards: Record<string, any> = valkeyDashboards;
+  readonly panels;
 
   constructor(page: Page) {
     this.page = page;
-    this.timeSeriesPanel = new TimeSeriesPanel(this.page);
-    this.statPanel = new StatPanel(this.page);
-    this.barGaugePanel = new BarGaugePanel(this.page);
-    this.barTimePanel = new BarTimePanel(this.page);
-    this.polyStatPanel = new PolyStatPanel(this.page);
-    this.tablePanel = new TablePanel(this.page);
     this.mysqlInstanceOverview = new MysqlInstanceOverview();
-    this.textPanel = new TextPanel(this.page);
-    this.stateTime = new StateTimePanel(this.page);
     this.mysql = new MysqlDashboards();
+    this.panels = () => Panels(this.page);
   }
 
   private elements = {
@@ -148,28 +127,28 @@ export default class Dashboards {
     for (const panel of panelList) {
       switch (panel.type) {
         case 'timeSeries':
-          await this.timeSeriesPanel.verifyPanelData(panel.name);
+          await this.panels().timeSeries.verifyPanelData(panel.name);
           break;
         case 'stat':
-          await this.statPanel.verifyPanelData(panel.name);
+          await this.panels().stat.verifyPanelData(panel.name);
           break;
         case 'barGauge':
-          await this.barGaugePanel.verifyPanelData(panel.name);
+          await this.panels().barGauge.verifyPanelData(panel.name);
           break;
         case 'barTime':
-          await this.barTimePanel.verifyPanelData(panel.name);
+          await this.panels().barTime.verifyPanelData(panel.name);
           break;
         case 'polyStat':
-          await this.polyStatPanel.verifyPanelData(panel.name);
+          await this.panels().polyStat.verifyPanelData(panel.name);
           break;
         case 'table':
-          await this.tablePanel.verifyPanelData(panel.name);
+          await this.panels().table.verifyPanelData(panel.name);
           break;
         case 'text':
-          await this.textPanel.verifyPanelData(panel.name);
+          await this.panels().text.verifyPanelData(panel.name);
           break;
         case 'stateTime':
-          await this.stateTime.verifyPanelData(panel.name);
+          await this.panels().stateTime.verifyPanelData(panel.name);
           break;
         case 'summary':
           await this.elements.summaryPanelText().waitFor({ state: 'visible', timeout: Timeouts.TEN_SECONDS });
