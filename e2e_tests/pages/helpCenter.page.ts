a42ed25923a56230f6d66b6a079a8b6f8fa06e1b
kiranvuyurru <kiran.vuyurru@percona.com>
2026-01-09 10:24:49 -0700

    add tags and formatting to 2 space indentation

--- e2e_tests/pages/helpCenter.page.ts
+++ e2e_tests/pages/helpCenter.page.ts
@@ -1,34 +1,34 @@
 import { Page, Locator } from '@playwright/test';
 
 export default class HelpPage {
-    constructor(public page: Page) { }
+  constructor(public page: Page) { }
 
-    public elements() {
-        return {
-            viewDocsButton: () => this.page.getByRole('link', { name: 'View docs' }),
-            contactSupportButton: () => this.page.getByRole('link', { name: 'Contact Support' }),
-            viewForumButton: () => this.page.getByRole('link', { name: 'View forum' }),
-            manageDatasetsButton: () => this.page.getByRole('link', { name: 'Manage datasets' }),
-            exportLogsButton: () => this.page.getByRole('link', { name: 'Export logs' }),
-            startPmmTourButton: () => this.page.getByTestId('tips-card-start-product-tour-button'),
-            shareYourThoughtsButton: () => this.page.getByRole('link', { name: 'Share your thoughts' }),
-            nextTipButton: () => this.page.getByTestId('tour-next-step-button'),
-        };
-    }
+  public elements() {
+    return {
+      viewDocsButton: () => this.page.getByRole('link', { name: 'View docs' }),
+      contactSupportButton: () => this.page.getByRole('link', { name: 'Contact Support' }),
+      viewForumButton: () => this.page.getByRole('link', { name: 'View forum' }),
+      manageDatasetsButton: () => this.page.getByRole('link', { name: 'Manage datasets' }),
+      exportLogsButton: () => this.page.getByRole('link', { name: 'Export logs' }),
+      startPmmTourButton: () => this.page.getByTestId('tips-card-start-product-tour-button'),
+      shareYourThoughtsButton: () => this.page.getByRole('link', { name: 'Share your thoughts' }),
+      nextTipButton: () => this.page.getByTestId('tour-next-step-button'),
+    };
+  }
 
 
-    exportLogs = async (): Promise<any> => {
-        const download = this.page.waitForEvent('download');
-        await this.elements().exportLogsButton().click();
-        return download;
-    }
+  exportLogs = async (): Promise<any> => {
+    const download = this.page.waitForEvent('download');
+    await this.elements().exportLogsButton().click();
+    return download;
+  }
 
-    clickElement = async (element: Locator): Promise<void> => {
-        await element.click();
-    }
+  clickElement = async (element: Locator): Promise<void> => {
+    await element.click();
+  }
 
-    clickStartPmmTour = async (): Promise<Locator> => {
-        await this.elements().startPmmTourButton().click();
-        return this.elements().nextTipButton();
-    }
+  clickStartPmmTour = async (): Promise<Locator> => {
+    await this.elements().startPmmTourButton().click();
+    return this.elements().nextTipButton();
+  }
 }
