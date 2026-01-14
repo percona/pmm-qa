cf9965db3ae47826d0b9f26ff1b8e9810d039600
kiranvuyurru <kiran.vuyurru@percona.com>
2026-01-14 10:37:28 -0700

    fixed formatting and deleted a file

--- e2e_tests/fixtures/pmmTest.ts
+++ e2e_tests/fixtures/pmmTest.ts
@@ -54,12 +54,12 @@ const pmmTest = base.extend<{
   welcomePage: WelcomePage;
   mocks: Mocks;
 }>({
-  cliHelper: async ({ }, use) => {
+  cliHelper: async ({}, use) => {
     const cliHelper = new CliHelper();
     await use(cliHelper);
   },
 
-  credentials: async ({ }, use) => {
+  credentials: async ({}, use) => {
     const credentials = new Credentials();
     await use(credentials);
   },
@@ -84,7 +84,7 @@ const pmmTest = base.extend<{
     await use(queryAnalytics);
   },
 
-  urlHelper: async ({ }, use) => {
+  urlHelper: async ({}, use) => {
     const urlHelper = new UrlHelper();
     await use(urlHelper);
   },
