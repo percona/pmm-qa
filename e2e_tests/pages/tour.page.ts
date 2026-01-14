672f2715c25a12df1a26dd6ad9462c4a10f5482b
kiranvuyurru <kiran.vuyurru@percona.com>
2026-01-14 09:45:01 -0700

    PMM-14545 verify the PMM tour (#845)
    * PMM-14545 verify the PMM tour
    
    * refactored the code and created methods
    
    * refactored
    
    * added tags and fixed indentations
    
    * Update pmmTest.ts
    
    * Update pmmTour.test.ts
    
    * changes fixed
    
    * changes

---
+++ e2e_tests/pages/tour.page.ts
@@ -0,0 +1,35 @@
+import { Page, Locator } from '@playwright/test';
+
+export default class TourPage {
+  public readonly titles = [
+    'Percona Dashboards',
+    'Query Analytics (QAN) dashboard',
+    'Explore',
+    'Alerts & Percona Templates',
+    'Percona Advisors',
+    'Management: Inventory & Backups',
+    'Configurations',
+    'Help Center'
+  ];
+
+  constructor(public page: Page) { }
+
+  public elements = {
+    startTourButton: () => this.page.getByTestId('tips-card-start-product-tour-button'),
+    nextTip: () => this.page.getByTestId('tour-next-step-button'),
+    previousTip: () => this.page.getByTestId('tour-previous-step-button'),
+    endTourButton: () => this.page.getByTestId('tour-end-tour-button'),
+    closeButton: () => this.page.getByTestId('tour-close-button'),
+    stepTitle: () => this.page.getByTestId('tour-step-title'),
+  };
+
+  public navigateForward = async (stepsToMove: number): Promise<void> => {
+    for (let i = 0; i < stepsToMove; i++) {
+      await this.elements.nextTip().click();
+    }
+  }
+
+  public getStepTitle = async (): Promise<string> => {
+    return (await this.elements.stepTitle().innerText());
+  }
+}
