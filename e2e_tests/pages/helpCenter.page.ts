8b2ed7e7d8dc9b36848360e9d71b2302a8e5cfeb
kiranvuyurru <kiran.vuyurru@percona.com>
2026-01-15 07:58:02 -0700

    PMM-14545 help center options (#843)
    * PMM-14545 help center options
    
    Navigate to help center page
    Check each option visibility
    check for 200 response on click and open a new tab
    
    * added helper methods
    
    * PMM-14545 refactored
    
    * refactored the code
    
    * refactor
    
    * Refactor HelpPage element access and usage in tests
    
    * add tags and formatting to 2 space indentation
    
    * changes and fixes
    
    * Update helpCenter.page.ts

---
+++ e2e_tests/pages/helpCenter.page.ts
@@ -0,0 +1,29 @@
+import { Page, Locator, Download } from '@playwright/test';
+
+export default class HelpPage {
+  constructor(public page: Page) { }
+
+  public elements = {
+    viewDocsButton: () => this.page.getByRole('link', { name: 'View docs' }),
+    contactSupportButton: () => this.page.getByRole('link', { name: 'Contact Support' }),
+    viewForumButton: () => this.page.getByRole('link', { name: 'View forum' }),
+    manageDatasetsButton: () => this.page.getByRole('link', { name: 'Manage datasets' }),
+    exportLogsButton: () => this.page.getByRole('link', { name: 'Export logs' }),
+    startPmmTourButton: () => this.page.getByTestId('tips-card-start-product-tour-button'),
+    shareYourThoughtsButton: () => this.page.getByRole('link', { name: 'Share your thoughts' }),
+    nextTipButton: () => this.page.getByTestId('tour-next-step-button'),
+  };
+
+
+
+  exportLogs = async (): Promise<Download> => {
+    const download = this.page.waitForEvent('download');
+    await this.elements.exportLogsButton().click();
+    return download;
+  }
+
+  clickStartPmmTour = async (): Promise<Locator> => {
+    await this.elements.startPmmTourButton().click();
+    return this.elements.nextTipButton();
+  }
+}
