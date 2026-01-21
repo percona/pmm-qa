a9be3b96932dd97c52238091e7a0352e2b3ad4e4
kiranvuyurru <kiran.vuyurru@percona.com>
2026-01-21 09:18:37 -0700

    Refactor WelcomePage selectors and tests

--- e2e_tests/pages/welcome.page.ts
+++ e2e_tests/pages/welcome.page.ts
@@ -1,55 +1,62 @@
-import { Page, Locator, expect } from "@playwright/test";
+import { Page } from "@playwright/test";
+import { IPageObject } from "@interfaces/pageObject";
+import pmmTest from "@fixtures/pmmTest";
 
-export default class WelcomePage {
+export default class WelcomePage implements IPageObject {
+  public readonly buttons;
+  public readonly elements;
 
   public readonly cases = [
     { updateAvailable: true },
     { updateAvailable: false },
   ];
 
-  constructor(public page: Page) { }
+  constructor(public readonly page: Page) {
+    this.buttons = {
+      addServiceButton: this.page.getByTestId('welcome-card-add-service'),
+      dismissButton: this.page.getByTestId('welcome-card-dismiss'),
+      startTourButton: this.page.getByTestId('welcome-card-start-tour'),
+      updates: this.page.getByTestId('update-modal-go-to-updates-button'),
+      tourCloseButton: this.page.getByTestId('tour-close-button'),
+    };
 
-  public elements = {
-    welcomeCard: () => this.page.getByTestId('welcome-card'),
-    addServiceButton: () => this.page.getByTestId('welcome-card-add-service'),
-    dismissButton: () => this.page.getByTestId('welcome-card-dismiss'),
-    startTourButton: () => this.page.getByTestId('welcome-card-start-tour'),
-    tourPopover: () => this.page.locator('.reactour__popover'),
-    updates: () => this.page.getByTestId('update-modal-go-to-updates-button'),
-    tourCloseButton: () => this.page.getByTestId('tour-close-button'),
+    this.elements = {
+      welcomeCard: this.page.getByTestId('welcome-card'),
+      tourPopover: this.page.locator('.reactour__popover'),
+    };
   }
 
   // mock api for update available
-  public mockUpdateAvailable = async (updateAvailable: boolean): Promise<void> => {
-    await this.page.route('**/v1/server/updates?force=true', async (route) => {
+  public async mockUpdateAvailable(updateAvailable: boolean): Promise<void> {
+    await pmmTest.step('Mock update available API', async () => {
+      await this.page.route('**/v1/server/updates?force=true', async (route) => {
+        const installedTimestamp = '2025-12-18T00:00:00Z';
+        const now = new Date();
+        const millisecond = now.getMilliseconds().toString().padStart(3, '0');
+        const nanosecondTimestamp = now.toISOString().split('.')[0] + '.' + millisecond + '000000Z';
 
-      const installedTimestamp = '2025-12-18T00:00:00Z';
-      const now = new Date();
-      const millisecond = now.getMilliseconds().toString().padStart(3, '0');
-      const nanosecondTimestamp = now.toISOString().split('.')[0] + '.' + millisecond + '000000Z';
-
-
-      await route.fulfill({
-        status: 200,
-        contentType: 'application/json',
-        body: JSON.stringify({
-          installed: {
-            version: '',
-            full_version: '',
-            timestamp: installedTimestamp,
-          },
-          latest: {
-            version: '',
-            tag: '',
-            timestamp: null,
-            release_notes_url: 'https://example.com',
-            release_notes_text: 'New features'
-          },
-          update_available: updateAvailable,
-          latest_news_url: "https://example.com",
-          last_check: nanosecondTimestamp,
-        })
+        await route.fulfill({
+          status: 200,
+          contentType: 'application/json',
+          body: JSON.stringify({
+            installed: {
+              version: '',
+              full_version: '',
+              timestamp: installedTimestamp,
+            },
+            latest: {
+              version: '',
+              tag: '',
+              timestamp: null,
+              release_notes_url: 'https://example.com',
+              release_notes_text: 'New features'
+            },
+            update_available: updateAvailable,
+            latest_news_url: "https://example.com",
+            last_check: nanosecondTimestamp,
+          })
+        });
       });
     });
-  };
+  }
 }
--- e2e_tests/tests/welcomePage.test.ts
+++ e2e_tests/tests/welcomePage.test.ts
@@ -12,12 +12,11 @@ pmmTest('PMM-T2132 Verify welcome Card appears on fresh install @new-navigation'
     await mocks.mockFreshInstall();
     await mocks.mockNoServices();
   });
-
   await pmmTest.step('Verify welcome card and its buttons are visible', async () => {
-    await expect(welcomePage.elements.welcomeCard()).toBeVisible();
-    await expect(welcomePage.elements.addServiceButton()).toBeVisible();
-    await expect(welcomePage.elements.dismissButton()).toBeVisible();
-    await expect(welcomePage.elements.startTourButton()).toBeVisible();
+    await expect(welcomePage.elements.welcomeCard).toBeVisible();
+    await expect(welcomePage.buttons.addServiceButton).toBeVisible();
+    await expect(welcomePage.buttons.dismissButton).toBeVisible();
+    await expect(welcomePage.buttons.startTourButton).toBeVisible();
   });
 });
 
@@ -25,15 +24,13 @@ pmmTest('PMM-T2101 verify dismiss button on welcome card @new-navigation', async
   await pmmTest.step('Mock fresh install', async () => {
     await mocks.mockFreshInstall();
   });
-
   await pmmTest.step('Verify welcome card visibility and click dismiss', async () => {
-    await expect(welcomePage.elements.welcomeCard()).toBeVisible();
-    await welcomePage.elements.dismissButton().click();
+    await expect(welcomePage.elements.welcomeCard).toBeVisible();
+    await welcomePage.buttons.dismissButton.click();
   });
-
   await pmmTest.step('Reload page and verify welcome card is not visible', async () => {
     await page.reload();
-    await expect(welcomePage.elements.welcomeCard()).toBeHidden();
+    await expect(welcomePage.elements.welcomeCard).toBeHidden();
   });
 });
 
@@ -41,39 +38,33 @@ pmmTest('PMM-T2133 Verify Welcome Card start tour @new-navigation', async ({ pag
   await pmmTest.step('Mock fresh install', async () => {
     await mocks.mockFreshInstall();
   });
-
   await pmmTest.step('Verify welcome card and start tour', async () => {
-    await expect(welcomePage.elements.welcomeCard()).toBeVisible();
-    await welcomePage.elements.startTourButton().click();
+    await expect(welcomePage.elements.welcomeCard).toBeVisible();
+    await welcomePage.buttons.startTourButton.click();
   });
-
   await pmmTest.step('Verify tour popover and close tour', async () => {
-    await expect(welcomePage.elements.tourPopover()).toBeVisible();
-    await welcomePage.elements.tourCloseButton().click();
+    await expect(welcomePage.elements.tourPopover).toBeVisible();
+    await welcomePage.buttons.tourCloseButton.click();
   });
-
   await pmmTest.step('Reload page and verify welcome card is not visible', async () => {
     await page.reload();
-    await expect(welcomePage.elements.welcomeCard()).toBeHidden();
+    await expect(welcomePage.elements.welcomeCard).toBeHidden();
   });
 });
 
 pmmTest('PMM-T2134 Verify Update check @new-navigation', async ({ page, welcomePage, mocks }) => {
 
   const cases = welcomePage.cases;
-
   for (const c of cases) {
     await pmmTest.step('Verify update check', async () => {
       await welcomePage.mockUpdateAvailable(
         c.updateAvailable
       );
-
       await page.reload();
-
       if (c.updateAvailable) {
-        await expect(welcomePage.elements.updates()).toBeVisible({ timeout: 10000 });
+        await expect(welcomePage.buttons.updates).toBeVisible({ timeout: 10000 });
       } else {
-        await expect(welcomePage.elements.updates()).toBeHidden({ timeout: 10000 });
+        await expect(welcomePage.buttons.updates).toBeHidden({ timeout: 10000 });
       }
     });
   }
