e07da6dd6f08ea9dad1cd19afb6556c35c7a814b
Peter Sirotnak <sirotnak.p@gmail.com>
2026-01-27 09:23:58 +0100

    PMM-7: Fix tests (#867)

--- e2e_tests/tests/welcomePage.test.ts
+++ e2e_tests/tests/welcomePage.test.ts
@@ -6,33 +6,38 @@ pmmTest.beforeEach(async ({ page, grafanaHelper }) => {
   await grafanaHelper.authorize();
 });
 
+pmmTest(
+  'PMM-T2132 Verify welcome Card appears on fresh install @new-navigation',
+  async ({ welcomePage, mocks }) => {
+    await pmmTest.step('Mock fresh install and no services', async () => {
+      await mocks.mockFreshInstall();
+      await mocks.mockNoServices();
+    });
+    await pmmTest.step('Verify welcome card and its buttons are visible', async () => {
+      await expect(welcomePage.elements.welcomeCard).toBeVisible();
+      await expect(welcomePage.buttons.addServiceButton).toBeVisible();
+      await expect(welcomePage.buttons.dismissButton).toBeVisible();
+      await expect(welcomePage.buttons.startTourButton).toBeVisible();
+    });
+  },
+);
 
-pmmTest('PMM-T2132 Verify welcome Card appears on fresh install @new-navigation', async ({ welcomePage, mocks }) => {
-  await pmmTest.step('Mock fresh install and no services', async () => {
-    await mocks.mockFreshInstall();
-    await mocks.mockNoServices();
-  });
-  await pmmTest.step('Verify welcome card and its buttons are visible', async () => {
-    await expect(welcomePage.elements.welcomeCard).toBeVisible();
-    await expect(welcomePage.buttons.addServiceButton).toBeVisible();
-    await expect(welcomePage.buttons.dismissButton).toBeVisible();
-    await expect(welcomePage.buttons.startTourButton).toBeVisible();
-  });
-});
-
-pmmTest('PMM-T2101 verify dismiss button on welcome card @new-navigation', async ({ page, welcomePage, mocks }) => {
-  await pmmTest.step('Mock fresh install', async () => {
-    await mocks.mockFreshInstall();
-  });
-  await pmmTest.step('Verify welcome card visibility and click dismiss', async () => {
-    await expect(welcomePage.elements.welcomeCard).toBeVisible();
-    await welcomePage.buttons.dismissButton.click();
-  });
-  await pmmTest.step('Reload page and verify welcome card is not visible', async () => {
-    await page.reload();
-    await expect(welcomePage.elements.welcomeCard).toBeHidden();
-  });
-});
+pmmTest(
+  'PMM-T2101 verify dismiss button on welcome card @new-navigation',
+  async ({ page, welcomePage, mocks }) => {
+    await pmmTest.step('Mock fresh install', async () => {
+      await mocks.mockFreshInstall();
+    });
+    await pmmTest.step('Verify welcome card visibility and click dismiss', async () => {
+      await expect(welcomePage.elements.welcomeCard).toBeVisible();
+      await welcomePage.buttons.dismissButton.click();
+    });
+    await pmmTest.step('Reload page and verify welcome card is not visible', async () => {
+      await page.reload();
+      await expect(welcomePage.elements.welcomeCard).toBeHidden();
+    });
+  },
+);
 
 pmmTest('PMM-T2133 Verify Welcome Card start tour @new-navigation', async ({ page, welcomePage, mocks }) => {
   await pmmTest.step('Mock fresh install', async () => {
@@ -53,13 +58,10 @@ pmmTest('PMM-T2133 Verify Welcome Card start tour @new-navigation', async ({ pag
 });
 
 pmmTest('PMM-T2134 Verify Update check @new-navigation', async ({ page, welcomePage, mocks }) => {
-
   const cases = welcomePage.cases;
   for (const c of cases) {
     await pmmTest.step('Verify update check', async () => {
-      await welcomePage.mockUpdateAvailable(
-        c.updateAvailable
-      );
+      await mocks.mockUpdateAvailable(c.updateAvailable);
       await page.reload();
       if (c.updateAvailable) {
         await expect(welcomePage.buttons.updates).toBeVisible({ timeout: 10000 });
