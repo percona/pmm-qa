4d6ad15f497e9ae50697fa892dcce5d74c7ca65b
kiranvuyurru <kiran.vuyurru@percona.com>
2026-02-02 13:06:30 -0700

    Update navigation.test.ts

--- e2e_tests/tests/navigation.test.ts
+++ e2e_tests/tests/navigation.test.ts
@@ -33,14 +33,15 @@ pmmTest('verify left menu sidebar collapse and expand @new-navigation', async ({
 pmmTest(
   'Traverse all the menu items in left menu sidebar @new-navigation',
   async ({ page, leftNavigation }) => {
+    pmmTest.slow();
     await pmmTest.step('Traverse menu items', async () => {
       await leftNavigation.traverseAllMenuItems(async (locator, res) => {
         await pmmTest.step('click and verify menu items', async () => {
           await expect(locator).toBeVisible({ timeout: 10000 });
           if (res) {
-            expect(res.status).not.toBe(404);
+            expect(res.status()).not.toBe(404);
           }
-          await expect(page).not.toHaveURL(/404 |error|not-found/i);
+          await expect(page).not.toHaveURL(/404|error|not-found/i);
         });
       });
     });
