76ab6150ab71e57e4ba7a659e3636e9047293196
kiranvuyurru <kiran.vuyurru@percona.com>
2026-02-03 12:17:55 -0700

    create traverse test as a new file

---
+++ e2e_tests/tests/leftMenutravese.test.ts
@@ -0,0 +1,25 @@
+import pmmTest from '@fixtures/pmmTest';
+import { expect } from '@playwright/test';
+
+pmmTest.beforeEach(async ({ page, grafanaHelper }) => {
+  await page.goto('');
+  await grafanaHelper.authorize();
+});
+
+pmmTest(
+  'Traverse all the menu items in left menu sidebar @new-navigation',
+  async ({ page, leftNavigation }) => {
+    pmmTest.setTimeout(1200000);
+    await pmmTest.step('Traverse menu items', async () => {
+      await leftNavigation.traverseAllMenuItems(async (locator, res) => {
+        await pmmTest.step('click and verify menu items', async () => {
+          await expect(locator).toBeVisible({ timeout: 10000 });
+          if (res) {
+            expect(res.status()).not.toBe(404);
+          }
+          await expect(page).not.toHaveURL(/404|error|not-found/i);
+        });
+      });
+    });
+  },
+);
