5c5fe8a389f1c263a8d07db0a83d85f78719612d
kiranvuyurru <kiran.vuyurru@percona.com>
2026-02-03 08:55:20 -0700

    Update navigation.test.ts

--- e2e_tests/tests/navigation.test.ts
+++ e2e_tests/tests/navigation.test.ts
@@ -7,33 +7,35 @@ pmmTest.beforeEach(async ({ page, grafanaHelper }) => {
   await grafanaHelper.authorize();
 });
 
-pmmTest('verify left menu sidebar collapse and expand @new-navigation', async ({ page, leftNavigation }) => {
-  await pmmTest.step('verify left menu sidebar collapse and expand', async () => {
-    await expect(leftNavigation.elements.closeLeftNavigationButton).toBeVisible();
-    await expect(leftNavigation.elements.openLeftNavigationButton).toBeHidden();
-  });
+pmmTest.skip(
+  'verify left menu sidebar collapse and expand @new-navigation',
+  async ({ page, leftNavigation }) => {
+    await pmmTest.step('verify left menu sidebar collapse and expand', async () => {
+      await expect(leftNavigation.elements.closeLeftNavigationButton).toBeVisible();
+      await expect(leftNavigation.elements.openLeftNavigationButton).toBeHidden();
+    });
 
-  await pmmTest.step('collapse left menu sidebar and verify collapsed', async () => {
-    await leftNavigation.elements.closeLeftNavigationButton.click();
-    await expect(leftNavigation.elements.openLeftNavigationButton).toBeVisible();
-    await expect(leftNavigation.elements.closeLeftNavigationButton).toBeHidden();
-    await page.reload();
-    await expect(leftNavigation.elements.openLeftNavigationButton).toBeVisible();
-    await expect(leftNavigation.elements.closeLeftNavigationButton).toBeHidden();
-  });
+    await pmmTest.step('collapse left menu sidebar and verify collapsed', async () => {
+      await leftNavigation.elements.closeLeftNavigationButton.click();
+      await expect(leftNavigation.elements.openLeftNavigationButton).toBeVisible();
+      await expect(leftNavigation.elements.closeLeftNavigationButton).toBeHidden();
+      await page.reload();
+      await expect(leftNavigation.elements.openLeftNavigationButton).toBeVisible();
+      await expect(leftNavigation.elements.closeLeftNavigationButton).toBeHidden();
+    });
 
-  await pmmTest.step('expand left menu sidebar and verify expanded', async () => {
-    await leftNavigation.mouseHoverOnPmmLogo();
-    await leftNavigation.elements.openLeftNavigationButton.click();
-    await expect(leftNavigation.elements.closeLeftNavigationButton).toBeVisible();
-    await expect(leftNavigation.elements.openLeftNavigationButton).toBeHidden();
-  });
-});
+    await pmmTest.step('expand left menu sidebar and verify expanded', async () => {
+      await leftNavigation.mouseHoverOnPmmLogo();
+      await leftNavigation.elements.openLeftNavigationButton.click();
+      await expect(leftNavigation.elements.closeLeftNavigationButton).toBeVisible();
+      await expect(leftNavigation.elements.openLeftNavigationButton).toBeHidden();
+    });
+  },
+);
 
 pmmTest(
   'Traverse all the menu items in left menu sidebar @new-navigation',
   async ({ page, leftNavigation }) => {
-    pmmTest.slow();
     await pmmTest.step('Traverse menu items', async () => {
       await leftNavigation.traverseAllMenuItems(async (locator, res) => {
         await pmmTest.step('click and verify menu items', async () => {
@@ -48,7 +50,7 @@ pmmTest(
   },
 );
 
-pmmTest('RBAC/permissions @new-navigation', async ({ leftNavigation, grafanaHelper }) => {
+pmmTest.skip('RBAC/permissions @new-navigation', async ({ leftNavigation, grafanaHelper }) => {
   await pmmTest.step('Create non-admin user', async () => {
     await grafanaHelper.createUser('nonadmin', 'nonadmin');
   });
@@ -64,35 +66,38 @@ pmmTest('RBAC/permissions @new-navigation', async ({ leftNavigation, grafanaHelp
   });
 });
 
-pmmTest('verify custom time range persists on any dashboard @new-navigation', async ({ leftNavigation }) => {
-  const selectedTimeRange = 'Last 15 minutes';
-  const verifyTimeRangeOnDashboards = async (leftNavigation: LeftNavigation, selectedTimeRange: string) => {
-    const dashboards = leftNavigation.dashboardsToVerifyTimeRange();
-    for (const dashboard of dashboards) {
-      await pmmTest.step(`Verify time range`, async () => {
-        await leftNavigation.selectMenuItem(dashboard);
-        await expect(leftNavigation.elements.timePickerOpenButton).toContainText(selectedTimeRange, {
-          timeout: 10000,
-        });
-      });
-    }
-  };
-  await pmmTest.step('Select time range @new-navigation', async () => {
-    await leftNavigation.selectMenuItem('home');
-    await leftNavigation.elements.timePickerOpenButton.click();
-    await leftNavigation.selectTimeRange(selectedTimeRange);
-    await expect(leftNavigation.elements.timePickerOpenButton).toContainText(selectedTimeRange);
-  });
-  await pmmTest.step('Verify time range persistence', async () => {
-    await verifyTimeRangeOnDashboards(leftNavigation, selectedTimeRange);
-  });
-  await pmmTest.step('Verify new tab persistence', async () => {
-    const newPage = await leftNavigation.newTab();
-    await expect(leftNavigation.elements.timePickerOpenButton).toContainText(selectedTimeRange);
-  });
-});
+pmmTest.skip(
+  'verify custom time range persists on any dashboard @new-navigation',
+  async ({ leftNavigation }) => {
+    const selectedTimeRange = 'Last 15 minutes';
 
-pmmTest('Grafana embedding @new-navigation', async ({ leftNavigation }) => {
+    await pmmTest.step('Select time range @new-navigation', async () => {
+      await leftNavigation.selectMenuItem('home');
+      await leftNavigation.elements.timePickerOpenButton.click();
+      await leftNavigation.selectTimeRange(selectedTimeRange);
+      await expect(leftNavigation.elements.timePickerOpenButton).toContainText(selectedTimeRange);
+    });
+
+    await pmmTest.step('Verify time range persistence', async () => {
+      const dashboards = leftNavigation.dashboardsToVerifyTimeRange();
+      for (const dashboard of dashboards) {
+        await pmmTest.step(`Verify time range`, async () => {
+          await leftNavigation.selectMenuItem(dashboard);
+          await expect(leftNavigation.elements.timePickerOpenButton).toContainText(selectedTimeRange, {
+            timeout: 10000,
+          });
+        });
+      }
+    });
+
+    await pmmTest.step('Verify new tab persistence', async () => {
+      await leftNavigation.newTab();
+      await expect(leftNavigation.elements.timePickerOpenButton).toContainText(selectedTimeRange);
+    });
+  },
+);
+
+pmmTest.skip('Grafana embedding @new-navigation', async ({ leftNavigation }) => {
   await pmmTest.step('Verify old menu hidden', async () => {
     await expect(leftNavigation.elements.oldLeftMenu).toBeHidden();
   });
@@ -108,7 +113,7 @@ pmmTest('Grafana embedding @new-navigation', async ({ leftNavigation }) => {
   });
 });
 
-pmmTest('verify service persistence @new-navigation', async ({ page, leftNavigation }) => {
+pmmTest.skip('verify service persistence @new-navigation', async ({ page, leftNavigation }) => {
   await pmmTest.step('verify service persistence in overview and summary dashboards', async () => {
     await leftNavigation.selectMenuItem('mysql');
     const selectedService = await leftNavigation.selectService(5);
@@ -122,7 +127,7 @@ pmmTest('verify service persistence @new-navigation', async ({ page, leftNavigat
   });
 });
 
-pmmTest('verify node persistence @new-navigation', async ({ page, leftNavigation }) => {
+pmmTest.skip('verify node persistence @new-navigation', async ({ page, leftNavigation }) => {
   await pmmTest.step('verify node persistence in system and postgre dashboards @new-navigation', async () => {
     await leftNavigation.selectMenuItem('operatingsystem');
     const selectedNode = await leftNavigation.selectNode(3);
@@ -137,7 +142,7 @@ pmmTest('verify node persistence @new-navigation', async ({ page, leftNavigation
     await expect(leftNavigation.variableContext(selectedNode)).toBeVisible();
     await leftNavigation.selectMenuItem('operatingsystem');
     await expect(leftNavigation.variableContext(selectedNode)).toBeVisible();
-    const newPage = await leftNavigation.newTab();
+    await leftNavigation.newTab();
     await expect(leftNavigation.variableContext(selectedNode)).toBeVisible();
   });
 });
