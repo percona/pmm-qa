cf7fc87a8009127d37ec2f0f9c56c7df5ac426cf
Peter Sirotnak <sirotnak.p@gmail.com>
2026-02-18 16:44:56 +0100

    PMM-14636: Verify MongoDB RTA Agent displayed in Inventory UI (#877)
    * PMM-14636: Test to verify RTA Agent in Invetory UI
    
    * PMM-14636: Code cleanup
    
    * PMM-14636: PR comments
    
    * PMM-14636: PR comments

---
+++ e2e_tests/api/realtimeanalytics.api.ts
@@ -0,0 +1,14 @@
+import { APIRequestContext } from '@playwright/test';
+import GrafanaHelper from '../helpers/grafana.helper';
+
+export default class RealTimeAnalyticsApi {
+  constructor(private request: APIRequestContext) {}
+
+  startRealTimeAnalytics = async (serviceId: string) => {
+    return await this.request.post('/v1/realtimeanalytics/sessions:start', { data: { serviceId: serviceId }, headers: GrafanaHelper.getAuthHeader() }, );
+  }
+
+  stopRealTimeAnalytics = async (serviceId: string) => {
+    return await this.request.post('/v1/realtimeanalytics/sessions:stop', { data: { serviceId: serviceId }, headers: GrafanaHelper.getAuthHeader() }, );
+  }
+}
