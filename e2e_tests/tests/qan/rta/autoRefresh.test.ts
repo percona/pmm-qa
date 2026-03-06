1576009cb9cc3b995533766ad2de6eaaff0b020f
kiranvuyurru <kiran.vuyurru@percona.com>
2026-03-06 07:46:04 -0700

    removed test pause on hover and consolidated rtaoverview page in to realTimeAnalyticspage

--- e2e_tests/tests/qan/rta/autoRefresh.test.ts
+++ e2e_tests/tests/qan/rta/autoRefresh.test.ts
@@ -2,23 +2,26 @@ import pmmTest from '@fixtures/pmmTest';
 import { expect } from '@playwright/test';
 import { Timeouts } from '@helpers/timeouts';
 
-pmmTest.beforeEach(async ({ grafanaHelper, page, rtaOverview }) => {
-  await page.goto('');
+pmmTest.beforeEach(async ({ api, grafanaHelper, page, realTimeAnalyticsPage }) => {
   await grafanaHelper.authorize();
-  await rtaOverview.startMonitoringClusterService();
+
+  const service = await api.inventoryApi.getServiceDetailsByPartialName('rs101');
+
+  await api.realTimeAnalyticsApi.startRealTimeAnalytics(service.service_id);
+  await page.goto(realTimeAnalyticsPage.getUrlWithServices([service.service_id]));
 });
 
 pmmTest(
   'PMM-T2166 Verify auto refresh dropdown, default value &interval behavior @rta',
-  async ({ page, rtaOverview }) => {
+  async ({ page, realTimeAnalyticsPage }) => {
     await pmmTest.step('Verify auto-refresh dropdown is visible and default is 2s', async () => {
-      await expect(rtaOverview.buttons.autoRefreshDropdown).toContainText('2s');
+      await expect(realTimeAnalyticsPage.buttons.refreshIntervalDropdown).toContainText('2s');
     });
 
     await pmmTest.step('Verify auto-refresh has the required interval options', async () => {
-      await rtaOverview.buttons.autoRefreshDropdown.click();
+      await realTimeAnalyticsPage.buttons.refreshIntervalDropdown.click();
 
-      for (const interval of rtaOverview.refreshIntervals) {
+      for (const interval of realTimeAnalyticsPage.refreshIntervals) {
         await expect(page.getByRole('menuitem', { name: interval })).toBeVisible();
       }
 
@@ -26,7 +29,7 @@ pmmTest(
     });
 
     await pmmTest.step('Verify default 2s interval', async () => {
-      const isTwoSecondInterval = await rtaOverview.verifyRequestInterval(
+      const isTwoSecondInterval = await realTimeAnalyticsPage.verifyRequestInterval(
         2 * Timeouts.ONE_SECOND,
         200,
         10 * Timeouts.ONE_SECOND,
@@ -38,13 +41,12 @@ pmmTest(
   },
 );
 
-pmmTest('PMM-T2170 verify query refresh interval every 4s @rta', async ({ page, rtaOverview }) => {
-  await rtaOverview.buttons.autoRefreshDropdown.click();
+pmmTest('PMM-T2170 verify query refresh interval every 4s @rta', async ({ page, realTimeAnalyticsPage }) => {
+  await realTimeAnalyticsPage.buttons.refreshIntervalDropdown.click();
   await page.getByRole('menuitem', { name: '4s' }).click();
-  await rtaOverview.buttons.resume.click();
-  await expect(rtaOverview.buttons.autoRefreshDropdown).toContainText('4s');
+  await expect(realTimeAnalyticsPage.buttons.refreshIntervalDropdown).toContainText('4s');
 
-  const isFourSecondInterval = await rtaOverview.verifyRequestInterval(
+  const isFourSecondInterval = await realTimeAnalyticsPage.verifyRequestInterval(
     4 * Timeouts.ONE_SECOND,
     200,
     15 * Timeouts.ONE_SECOND,
@@ -52,19 +54,4 @@ pmmTest('PMM-T2170 verify query refresh interval every 4s @rta', async ({ page, 
   );
 
   expect(isFourSecondInterval).toBeTruthy();
-});
-
-pmmTest('PMM-T2167 Verify rta pause when hovered over overview table @rta', async ({ rtaOverview }) => {
-  await pmmTest.step('Hover first overview row and verify auto-refresh is paused', async () => {
-    const firstRow = rtaOverview.elements.overviewTableBody.locator('tr').first();
-
-    await expect(firstRow).toBeVisible();
-    await firstRow.hover();
-    await expect(rtaOverview.buttons.refresh).toBeVisible();
-    await expect(rtaOverview.buttons.resume).toBeVisible();
-
-    const count = await rtaOverview.getApiRequestCount(6 * Timeouts.ONE_SECOND);
-
-    expect(count).toBe(0);
-  });
 });
