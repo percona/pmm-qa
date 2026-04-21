94163d230d6327ece72ed097860494421e941f76
yurkovychv <vasyl.yurkovych@percona.com>
2026-04-21 12:37:31 +0300

    drop mongo meta metrics test

--- codeceptjs-e2e/tests/metrics/verifyMongoDB_test.js
+++ codeceptjs-e2e/tests/metrics/verifyMongoDB_test.js
@@ -20,16 +20,6 @@ const clientCredentialsFlags = gssapi.enabled
   ? gssapi.credentials_flags
   : `--username=${mongo_test_user.username} --password=${mongo_test_user.password}`;
 
-const telemetry = {
-  collstats: 'mongodb_collector_scrape_time_collstats',
-  dbstats: 'mongodb_collector_scrape_time_dbstats',
-  diagnosticData: 'mongodb_collector_scrape_time_diagnostic_data',
-  general: 'mongodb_collector_scrape_time_general',
-  indexstats: 'mongodb_collector_scrape_time_indexstats',
-  top: 'mongodb_collector_scrape_time_top',
-  replsetStatus: 'mongodb_collector_scrape_time_replset_status',
-};
-
 const containerName = 'rs101';
 
 BeforeSuite(async ({ I }) => {
@@ -48,7 +38,10 @@ BeforeSuite(async ({ I }) => {
   // check that rs101 docker container exists
   const dockerCheck = await I.verifyCommand(`docker ps | grep ${containerName}`);
 
-  assert.ok(dockerCheck.includes(containerName), 'rs101 docker container should exist. please run pmm-framework with --database psmdb');
+  assert.ok(
+    dockerCheck.includes(containerName),
+    'rs101 docker container should exist. please run pmm-framework with --database psmdb',
+  );
 });
 
 Before(async ({ I }) => {
@@ -63,36 +56,12 @@ AfterSuite(async ({ I }) => {
   await I.mongoDisconnect();
 });
 
-Scenario(
-  'PMM-T1241 - Verify add mongoDB service with "+" in user password @mongodb-exporter',
-  async ({ I, grafanaAPI }) => {
-    await I.say(
-      await I.verifyCommand(`docker exec ${containerName} pmm-admin add mongodb ${clientCredentialsFlags} --host=${containerName} --service-name=${mongodb_service_name}`),
-    );
+Scenario('PMM-T1241 - Verify add mongoDB service with "+" in user password @mongodb-exporter', async ({ I, grafanaAPI }) => {
+  await I.say(
+    await I.verifyCommand(
+      `docker exec ${containerName} pmm-admin add mongodb ${clientCredentialsFlags} --host=${containerName} --service-name=${mongodb_service_name}`,
+    ),
+  );
 
-    await grafanaAPI.waitForMetric('mongodb_up', { type: 'service_name', value: mongodb_service_name }, 65);
-  },
-);
-
-Scenario(
-  'PMM-T1458 - Verify MongoDB exporter meta-metrics supporting @mongodb-exporter',
-  async ({ I }) => {
-    await I.say(await I.verifyCommand(`docker exec ${containerName} pmm-admin add mongodb ${clientCredentialsFlags} --host=${containerName} --service-name=${mongodb_service_name} --enable-all-collectors`));
-    let logs = '';
-
-    await I.asyncWaitFor(async () => {
-      logs = await I.verifyCommand('docker exec pmm-server cat /srv/logs/pmm-managed.log | grep mongodb_collector_scrape_time || true');
-
-      return logs.includes(telemetry.collstats) && logs.includes(telemetry.dbstats) && logs.includes(telemetry.diagnosticData)
-        && logs.includes(telemetry.general) && logs.includes(telemetry.indexstats) && logs.includes(telemetry.top)
-        && logs.includes(telemetry.replsetStatus);
-    }, 60);
-    I.assertTrue(logs.includes(telemetry.collstats), `/srv/logs/pmm-managed.log expected to contain '${telemetry.collstats}'`);
-    I.assertTrue(logs.includes(telemetry.dbstats), `/srv/logs/pmm-managed.log expected to contain '${telemetry.dbstats}'`);
-    I.assertTrue(logs.includes(telemetry.diagnosticData), `/srv/logs/pmm-managed.log expected to contain '${telemetry.diagnosticData}'`);
-    I.assertTrue(logs.includes(telemetry.general), `/srv/logs/pmm-managed.log expected to contain '${telemetry.general}'`);
-    I.assertTrue(logs.includes(telemetry.indexstats), `/srv/logs/pmm-managed.log expected to contain '${telemetry.indexstats}'`);
-    I.assertTrue(logs.includes(telemetry.top), `/srv/logs/pmm-managed.log expected to contain '${telemetry.top}'`);
-    I.assertTrue(logs.includes(telemetry.replsetStatus), `/srv/logs/pmm-managed.log expected to contain '${telemetry.replsetStatus}'`);
-  },
-);
+  await grafanaAPI.waitForMetric('mongodb_up', { type: 'service_name', value: mongodb_service_name }, 65);
+});
