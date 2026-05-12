68c9a521200be0fccaa863e09d5de187c96d2f7e
Peter Sirotnak <sirotnak.p@gmail.com>
2026-05-12 16:58:44 +0200

    PMM-14934: PMM-T1255 + PMM-T1279 - Verify GF_SECURITY_ADMIN_PASSWORD environment variable also with changed admin credentials

--- e2e_tests/pages/dashboards/home.ts
+++ e2e_tests/pages/dashboards/home.ts
@@ -1,15 +1,21 @@
 import { GrafanaPanel } from '@interfaces/grafanaPanel';
 import DashboardInterface from '@interfaces/dashboard';
 import { Locator, Page } from '@playwright/test';
+import BasePage from '@pages/base.page';
 
-export default class HomeDashboard implements DashboardInterface {
+export default class HomeDashboard extends BasePage implements DashboardInterface {
   url = 'graph/d/pmm-home/home-dashboard';
   noDataMetrics: string[] = [];
+  builders = {};
+  buttons = {};
   elements: Record<string, Locator>;
+  inputs = {};
+  messages = {};
 
   constructor(protected page: Page) {
+    super(page);
     this.elements = {
-      headerLocator: this.page.locator('//header//span[contains(text(),"Home")]'),
+      headerLocator: this.grafanaIframe().locator('//header//span[contains(text(),"Home")]'),
     };
   }
 
