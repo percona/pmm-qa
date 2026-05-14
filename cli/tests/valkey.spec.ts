7807c9a9e4a27c46f8f5dc0dd11d904e71ee246e
Peter Sirotnak <sirotnak.p@gmail.com>
2026-05-14 11:26:14 +0200

    PMM-12832: Fix tests

--- cli/tests/valkey.spec.ts
+++ cli/tests/valkey.spec.ts
@@ -18,7 +18,7 @@ test.describe('Valeky CLI tests', { tag: '@valkey' }, async () => {
   });
 
 
-  test("PMM-T6666 User can use connection timeout while using pmm-admin add mysql @connectionTimeoutPSMDB", async ({ }) => {
+  test("PMM-T6666 User can use connection timeout while using pmm-admin add valkey", async ({ }) => {
     const output = await cli.exec(`docker exec ${containerName} pmm-admin add valkey --connection-timeout=5s --username=${username} --password="${password}" --service-name=${connectionTimeoutServiceName} --host=${containerName} --port=${port}`);
     await output.exitCodeEquals(0);
 
