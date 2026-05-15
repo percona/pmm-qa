5a2677526bf334b938dfa8324dedac640d013092
Peter Sirotnak <sirotnak.p@gmail.com>
2026-05-15 13:18:35 +0200

    PMM-14934: Fix tests

--- e2e_tests/tests/dockerConfiguration/srvFolder.test.ts
+++ e2e_tests/tests/dockerConfiguration/srvFolder.test.ts
@@ -45,6 +45,11 @@ pmmTest.describe('Test for SRV folder in pmm server.', () => {
         );
 
         console.log(`Running test ${configuration.testName}`);
+
+        if (configuration.testName === 'local folder') {
+          console.log(cliHelper.execSilent('docker logs pmm-server-srv'));
+        }
+
         console.log(cliHelper.execute('docker ps -a'));
         await grafanaHelper.authorize('admin', 'admin');
         await page.goto(urlHelper.buildUrlWithParameters(dashboard.home.url, {}));
