4622c39fb75013bb74f3eac8e7349dc1836d1027
travagliad <215686151+travagliad@users.noreply.github.com>
2026-05-18 17:32:37 +0100

    PMM-7: Use role locators for table panels

--- e2e_tests/components/dashboards/panels/table.component.ts
+++ e2e_tests/components/dashboards/panels/table.component.ts
@@ -3,9 +3,11 @@ import PanelComponent from './panel.component';
 export default class TablePanel extends PanelComponent {
   private elements = {
     tablePanelValue: (panelName: string) =>
-      this.grafanaIframe().locator(
-        `//section[@data-testid="data-testid Panel header ${panelName}"]//div[@data-testid="data-testid table body"]//div[@role="row"]//div[@role="cell" and position() >= 1 and position() <= last()] | //section[@data-testid="data-testid Panel header ${panelName}"]//div[@data-testid="data-testid panel content"]//*[@role="grid"]//*[@role="row" and @aria-rowindex > 1]//*[@role="gridcell"]`,
-      ),
+      this.grafanaIframe()
+        .getByTestId(`data-testid Panel header ${panelName}`)
+        .getByTestId('data-testid panel content')
+        .getByRole('grid')
+        .getByRole('gridcell'),
   };
 
   verifyPanelData = async (panelName: string) => {
