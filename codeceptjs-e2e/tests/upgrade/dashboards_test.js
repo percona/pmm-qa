2be25314bb5f98a2457284a54123bbb2b3950fed
Peter Sirotnak <sirotnak.p@gmail.com>
2026-05-19 14:44:03 +0200

    PMM14930: Fix dashboard test

--- codeceptjs-e2e/tests/upgrade/dashboards_test.js
+++ codeceptjs-e2e/tests/upgrade/dashboards_test.js
@@ -156,8 +156,7 @@ Scenario(
   async ({
     I, grafanaAPI, dashboardPage,
   }) => {
-    const workFolder = await I.verifyCommand(`pwd`);
-    const resp = JSON.parse(await I.readFileSync(`${workFolder}/dashboard.json`, false));
+    const resp = JSON.parse(await I.readFileSync('/home/ec2-user/workspace/pmm3-upgrade-test-runner/dashboard.json', false));
 
     const resp1 = await grafanaAPI.getDashboard(resp.DASHBOARD1_UID);
     const resp2 = await grafanaAPI.getDashboard(resp.DASHBOARD2_UID);
