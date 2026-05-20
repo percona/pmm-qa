9c08cf01565bffb3a614be1acaa2bc420824bb7a
yurkovychv <vasyl.yurkovych@percona.com>
2026-05-20 15:34:10 +0300

    update alerts test logic

--- codeceptjs-e2e/tests/ia/alerts_test.js
+++ codeceptjs-e2e/tests/ia/alerts_test.js
@@ -3,23 +3,31 @@ const { users } = require('../helper/constants');
 
 const ruleName = 'PSQL immortal rule';
 const ruleFolder = 'PostgreSQL';
-const rulesForAlerts = [{
-  severity: 'SEVERITY_CRITICAL',
-}, {
-  severity: 'SEVERITY_ERROR',
-}, {
-  severity: 'SEVERITY_NOTICE',
-}, {
-  severity: 'SEVERITY_WARNING',
-}, {
-  severity: 'SEVERITY_ALERT',
-}, {
-  severity: 'SEVERITY_INFO',
-}, {
-  severity: 'SEVERITY_DEBUG',
-}, {
-  severity: 'SEVERITY_EMERGENCY',
-},
+const rulesForAlerts = [
+  {
+    severity: 'SEVERITY_CRITICAL',
+  },
+  {
+    severity: 'SEVERITY_ERROR',
+  },
+  {
+    severity: 'SEVERITY_NOTICE',
+  },
+  {
+    severity: 'SEVERITY_WARNING',
+  },
+  {
+    severity: 'SEVERITY_ALERT',
+  },
+  {
+    severity: 'SEVERITY_INFO',
+  },
+  {
+    severity: 'SEVERITY_DEBUG',
+  },
+  {
+    severity: 'SEVERITY_EMERGENCY',
+  },
 ];
 
 Feature('Alerting: Alerts');
@@ -43,43 +51,35 @@ AfterSuite(async ({ rulesAPI, I }) => {
   await rulesAPI.removeAllAlertRules();
 });
 
-Scenario(
-  'PMM-T1482 - Verify fired alert @ia',
-  async ({ I, alertsPage, alertsAPI }) => {
-    await alertsAPI.waitForAlerts(24, 1);
-    await I.amOnPage(alertsPage.url);
-    alertsPage.columnHeaders.forEach((header) => {
-      const columnHeader = alertsPage.elements.columnHeaderLocator(header);
+Scenario('PMM-T1482 - Verify fired alert @ia', async ({ I, alertsPage, alertsAPI }) => {
+  await alertsAPI.waitForAlerts(24, 1);
+  await I.amOnPage(alertsPage.url);
+  alertsPage.columnHeaders.forEach((header) => {
+    const columnHeader = alertsPage.elements.columnHeaderLocator(header);
 
-      I.waitForVisible(columnHeader, 10);
-    });
+    I.waitForVisible(columnHeader, 10);
+  });
 
-    // Verify there are no duplicate alerts
-    await I.seeNumberOfElements(alertsPage.elements.alertRow(ruleName), 1);
-    // Wait for the firing alert to arrive in webhook and PD
-    await I.wait(60);
-  },
-);
+  // Verify there are no duplicate alerts
+  await I.seeNumberOfElements(alertsPage.elements.alertRow(ruleName), 1);
+  // Wait for the firing alert to arrive in webhook and PD
+  await I.wait(60);
+});
 
-Scenario(
-  'PMM-T1997 - verify viewer can not silence alert @ia',
-  async ({ I, alertsPage, alertsAPI }) => {
-    await I.Authorize(users.viewer.username, users.viewer.password);
+Scenario('PMM-T1997 - verify viewer can not silence alert @ia', async ({ I, alertsPage, alertsAPI }) => {
+  await I.Authorize(users.viewer.username, users.viewer.password);
 
-    await alertsAPI.waitForAlerts(24, 1);
+  await alertsAPI.waitForAlerts(24, 1);
 
-    I.amOnPage(alertsPage.url);
-    I.waitForElement(alertsPage.elements.alertRow(ruleName), 20);
-    I.seeNumberOfElements(alertsPage.elements.alertRow(ruleName), 1);
-    I.seeAttributesOnElements(alertsPage.buttons.silenceActivate(ruleName), { 'aria-disabled': 'true' });
-  },
-);
+  I.amOnPage(alertsPage.url);
+  I.waitForElement(alertsPage.elements.alertRow(ruleName), 20);
+  I.seeNumberOfElements(alertsPage.elements.alertRow(ruleName), 1);
+  I.seeAttributesOnElements(alertsPage.buttons.silenceActivate(ruleName), { 'aria-disabled': 'true' });
+});
 
 Scenario(
   'PMM-T1998 - verify editor is able to silence and unsilence alert @ia',
-  async ({
-    I, alertsPage, alertmanagerAPI, alertsAPI,
-  }) => {
+  async ({ I, alertsPage, alertmanagerAPI, alertsAPI }) => {
     await I.Authorize(users.editor.username, users.editor.password);
 
     await alertsAPI.waitForAlerts(24, 1);
@@ -96,29 +96,24 @@ Scenario(
   },
 );
 
-Scenario(
-  'PMM-T1494 + PMM-T1495 - Verify fired alert in Pager Duty and Webhook @ia',
-  async ({ I, rulesAPI, alertsAPI }) => {
-    const file = './testdata/ia/scripts/alert.txt';
-    const alertUID = await rulesAPI.getAlertUID(ruleName, ruleFolder);
+Scenario('PMM-T1494 + PMM-T1495 - Verify fired alert in Pager Duty and Webhook @ia', async ({ I, rulesAPI, alertsAPI }) => {
+  const file = './testdata/ia/scripts/alert.txt';
+  const alertUID = await rulesAPI.getAlertUID(ruleName, ruleFolder);
 
-    await alertsAPI.waitForAlerts(24, 1);
-    await contactPointsAPI.createContactPoints();
-    // Webhook notification check
-    I.waitForFile(file, 180);
-    I.seeFile(file);
-    I.seeInThisFile(ruleName);
+  await alertsAPI.waitForAlerts(24, 1);
+  await contactPointsAPI.createContactPoints();
+  // Webhook notification check
+  I.waitForFile(file, 180);
+  I.seeFile(file);
+  I.seeInThisFile(ruleName);
 
-    // Pager Duty notification check
-    // await alertsAPI.verifyAlertInPagerDuty(alertUID);
-  },
-);
+  // Pager Duty notification check
+  // await alertsAPI.verifyAlertInPagerDuty(alertUID);
+});
 
 Scenario(
   'PMM-T1496 + PMM-T1497 - Verify it is possible to silence and unsilence alert @ia',
-  async ({
-    I, alertsPage, alertmanagerAPI,
-  }) => {
+  async ({ I, alertsPage, alertmanagerAPI }) => {
     I.amOnPage(alertsPage.url);
     await alertsPage.verifyAlert(ruleName);
     await alertsPage.silenceAlert(ruleName);
@@ -134,9 +129,7 @@ Scenario(
 
 Scenario(
   'PMM-T1498 - Verify firing alerts dissappear when the condition is fixed @ia',
-  async ({
-    I, alertsPage, alertRulesPage,
-  }) => {
+  async ({ I, alertsPage, alertRulesPage }) => {
     I.amOnPage(alertsPage.url);
     I.waitForElement(alertsPage.elements.firedAlertLink(ruleName));
     I.click(alertsPage.elements.firedAlertLink(ruleName));
@@ -144,26 +137,23 @@ Scenario(
     I.fillField(alertRulesPage.fields.editRuleThreshold, '20m');
     I.click(alertRulesPage.buttons.saveEditRule);
     I.amOnPage(alertsPage.url);
-    I.asyncWaitFor(async () => {
+    await I.asyncWaitFor(async () => {
       const noAlerts = await I.grabNumberOfVisibleElements(alertsPage.elements.noAlerts);
 
-      return noAlerts === 0;
+      return noAlerts === 1;
     }, 100);
     I.dontSeeElement(alertsPage.elements.alertRow(ruleName));
   },
 );
 
 // FIXME: Skip until https://jira.percona.com/browse/PMM-11130 is fixed
-Scenario(
-  'PMM-T659 - Verify alerts are deleted after deleting rules @ia',
-  async ({ I, alertsPage, rulesAPI }) => {
-    // Deleting rules
-    await rulesAPI.removeAllAlertRules();
+Scenario('PMM-T659 - Verify alerts are deleted after deleting rules @ia', async ({ I, alertsPage, rulesAPI }) => {
+  // Deleting rules
+  await rulesAPI.removeAllAlertRules();
 
-    I.amOnPage(alertsPage.url);
-    I.waitForElement(alertsPage.elements.noAlerts, 20);
-  },
-);
+  I.amOnPage(alertsPage.url);
+  I.waitForElement(alertsPage.elements.noAlerts, 20);
+});
 
 Scenario.skip(
   'PMM-T1499 - Verify an alert with non-existing filter (label) does not show up in list @fb-alerting',
@@ -187,38 +177,30 @@ Scenario.skip(
   },
 );
 
-Scenario(
-  'PMM-T564 - Verify fired alert severity colors @ia',
-  async ({
-    I, alertsPage, rulesAPI, alertsAPI,
-  }) => {
-    await rulesAPI.removeAllAlertRules();
-    for (const rule of rulesForAlerts) {
-      await rulesAPI.createAlertRule({ ruleName: rule.severity, severity: rule.severity }, ruleFolder);
-    }
+Scenario('PMM-T564 - Verify fired alert severity colors @ia', async ({ I, alertsPage, rulesAPI, alertsAPI }) => {
+  await rulesAPI.removeAllAlertRules();
+  for (const rule of rulesForAlerts) {
+    await rulesAPI.createAlertRule({ ruleName: rule.severity, severity: rule.severity }, ruleFolder);
+  }
 
-    await alertsAPI.waitForAlerts(24, 8);
-    await I.wait(10);
-    await I.amOnPage(alertsPage.url);
-    rulesForAlerts.forEach((item) => I.waitForElement(alertsPage.elements.alertRow(item.severity), 10));
-    rulesForAlerts.forEach((item) => I.see('Active', alertsPage.elements.stateCell(item.severity)));
-    I.seeCssPropertiesOnElements(alertsPage.elements.criticalSeverity, { color: alertsPage.colors.critical });
-    I.seeCssPropertiesOnElements(alertsPage.elements.errorSeverity, { color: alertsPage.colors.error });
-    I.seeCssPropertiesOnElements(alertsPage.elements.noticeSeverity, { color: alertsPage.colors.notice });
-    I.seeCssPropertiesOnElements(alertsPage.elements.warningSeverity, { color: alertsPage.colors.warning });
-    I.seeCssPropertiesOnElements(alertsPage.elements.emergencySeverity, { color: alertsPage.colors.critical });
-    I.seeCssPropertiesOnElements(alertsPage.elements.debugSeverity, { color: alertsPage.colors.notice });
-    I.seeCssPropertiesOnElements(alertsPage.elements.infoSeverity, { color: alertsPage.colors.notice });
-    I.seeCssPropertiesOnElements(alertsPage.elements.alertSeverity, { color: alertsPage.colors.critical });
-  },
-);
+  await alertsAPI.waitForAlerts(24, 8);
+  await I.wait(10);
+  await I.amOnPage(alertsPage.url);
+  rulesForAlerts.forEach((item) => I.waitForElement(alertsPage.elements.alertRow(item.severity), 10));
+  rulesForAlerts.forEach((item) => I.see('Active', alertsPage.elements.stateCell(item.severity)));
+  I.seeCssPropertiesOnElements(alertsPage.elements.criticalSeverity, { color: alertsPage.colors.critical });
+  I.seeCssPropertiesOnElements(alertsPage.elements.errorSeverity, { color: alertsPage.colors.error });
+  I.seeCssPropertiesOnElements(alertsPage.elements.noticeSeverity, { color: alertsPage.colors.notice });
+  I.seeCssPropertiesOnElements(alertsPage.elements.warningSeverity, { color: alertsPage.colors.warning });
+  I.seeCssPropertiesOnElements(alertsPage.elements.emergencySeverity, { color: alertsPage.colors.critical });
+  I.seeCssPropertiesOnElements(alertsPage.elements.debugSeverity, { color: alertsPage.colors.notice });
+  I.seeCssPropertiesOnElements(alertsPage.elements.infoSeverity, { color: alertsPage.colors.notice });
+  I.seeCssPropertiesOnElements(alertsPage.elements.alertSeverity, { color: alertsPage.colors.critical });
+});
 
-Scenario(
-  'PMM-T1467 - Verify empty Fired alerts list @fb-alerting',
-  async ({ I, alertsPage, rulesAPI }) => {
-    await rulesAPI.removeAllAlertRules();
-    I.amOnPage(alertsPage.url);
-    I.waitForVisible(alertsPage.elements.noAlerts, 10);
-    I.seeElement(alertsPage.elements.noAlerts);
-  },
-);
+Scenario('PMM-T1467 - Verify empty Fired alerts list @fb-alerting', async ({ I, alertsPage, rulesAPI }) => {
+  await rulesAPI.removeAllAlertRules();
+  I.amOnPage(alertsPage.url);
+  I.waitForVisible(alertsPage.elements.noAlerts, 10);
+  I.seeElement(alertsPage.elements.noAlerts);
+});
