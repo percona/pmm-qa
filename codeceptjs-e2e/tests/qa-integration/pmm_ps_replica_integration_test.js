ff51b7b91dde5eb94faf5b20a25d7e256671bc6e
yurkovychv <vasyl.yurkovych@percona.com>
2026-05-21 20:17:18 +0300

    fix ps replica tests

--- codeceptjs-e2e/tests/qa-integration/pmm_ps_replica_integration_test.js
+++ codeceptjs-e2e/tests/qa-integration/pmm_ps_replica_integration_test.js
@@ -1,7 +1,11 @@
 const assert = require('assert');
 
 const { adminPage } = inject();
-const serviceList = [{ serviceName: '_1', replication_set: 'ps-async-replication' }, { serviceName: '_2', replication_set: 'ps-async-replication' }];
+const replicationSet = 'ps-async-replication';
+const serviceList = [
+  { serviceName: '_1' },
+  { serviceName: '_2' },
+];
 
 Feature('Integration tests for Percona Server (Replica) & PMM').retry(1);
 
@@ -9,18 +13,20 @@ Before(async ({ I }) => {
   await I.Authorize();
 });
 
-BeforeSuite(async ({ inventoryAPI }) => {
-  serviceList[0].serviceName = (await inventoryAPI.getServiceDetailsByPartialDetails({ service_name: '_1', replication_set: serviceList[0].replication_set })).service_name;
-  serviceList[1].serviceName = (await inventoryAPI.getServiceDetailsByPartialDetails({ service_name: '_2', replication_set: serviceList[1].replication_set })).service_name;
-});
-
 Data(serviceList).Scenario(
   'PMM-T2029 - Verify dashboard for PS Replica Instance @pmm-ps-replica-integration @not-ui-pipeline @nightly',
   async ({
-    I, dashboardPage, adminPage, current,
+    I, dashboardPage, adminPage, inventoryAPI, current,
   }) => {
+    const { service_name } = await inventoryAPI.getServiceDetailsByPartialDetails(
+      {
+        service_name: current.serviceName,
+        replication_set: replicationSet,
+      },
+    );
+
     const url = I.buildUrlWithParams(dashboardPage.mysqlReplcationDashboard.clearUrl, {
-      from: 'now-5m', to: 'now', service_name: current.serviceName, refresh: '5s',
+      from: 'now-5m', to: 'now', service_name, refresh: '5s',
     });
 
     I.amOnPage(url);
@@ -29,7 +35,7 @@ Data(serviceList).Scenario(
     await dashboardPage.expandEachDashboardRow();
     adminPage.performPageUp(5);
     await dashboardPage.verifyMetricsExistence(dashboardPage.mysqlReplcationDashboard.metrics);
-    if (current.serviceName === serviceList[0].serviceName) {
+    if (current.serviceName === '_1') {
       await dashboardPage.verifyThereAreNoGraphsWithoutData(7);
     } else {
       await dashboardPage.verifyThereAreNoGraphsWithoutData(4);
@@ -40,21 +46,28 @@ Data(serviceList).Scenario(
 Scenario(
   'PMM-T2030 - Verify QAN for PS Replica Instance @pmm-ps-replica-integration @not-ui-pipeline @nightly',
   async ({
-    I, queryAnalyticsPage,
+    I, queryAnalyticsPage, inventoryAPI,
   }) => {
+    const { service_name } = await inventoryAPI.getServiceDetailsByPartialDetails(
+      {
+        service_name: '_1',
+        replication_set: replicationSet,
+      },
+    );
+
     I.amOnPage(I.buildUrlWithParams(queryAnalyticsPage.url, { from: 'now-5m', refresh: '5s' }));
     queryAnalyticsPage.waitForLoaded();
     await adminPage.applyTimeRange('Last 12 hours');
     queryAnalyticsPage.waitForLoaded();
-    await queryAnalyticsPage.filters.selectFilterInGroup('ps-async-replication', 'Replication Set');
+    await queryAnalyticsPage.filters.selectFilterInGroup(replicationSet, 'Replication Set');
     queryAnalyticsPage.waitForLoaded();
-    await queryAnalyticsPage.filters.selectFilterInGroup(serviceList[1].serviceName, 'Service Name');
+    await queryAnalyticsPage.filters.selectFilterInGroup(service_name, 'Service Name');
     queryAnalyticsPage.waitForLoaded();
     await queryAnalyticsPage.filters.selectFilterInGroup('sbtest', 'Schema');
     queryAnalyticsPage.waitForLoaded();
 
     const count = await queryAnalyticsPage.data.getCountOfItems();
 
-    assert.ok(count > 0, `The queries for service ${serviceList[1].serviceName} instance do NOT exist, check QAN Data`);
+    assert.ok(count > 0, `The queries for service ${service_name} instance do NOT exist, check QAN Data`);
   },
 );
