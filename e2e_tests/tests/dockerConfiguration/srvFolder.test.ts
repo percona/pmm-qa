4f67896ade76cb686216358ae7497ddefd7aa32a
Peter Sirotnak <sirotnak.p@gmail.com>
2026-05-26 13:24:32 +0200

    PMM-7: Refactoring

--- e2e_tests/tests/dockerConfiguration/srvFolder.test.ts
+++ e2e_tests/tests/dockerConfiguration/srvFolder.test.ts
@@ -49,12 +49,9 @@ pmmTest.describe('Test for SRV folder in pmm server.', () => {
         );
 
         console.log(`Running test ${configuration.testName}`);
-
-        if (configuration.testName === 'docker volume') {
-          console.log(cliHelper.execSilent('docker logs pmm-server-srv'));
-        }
-
+        console.log(cliHelper.execSilent('docker logs pmm-server-srv'));
         console.log(cliHelper.execute('docker ps -a'));
+
         await grafanaHelper.authorize('admin', 'admin');
         await page.goto(urlHelper.buildUrlWithParameters(dashboard.home.url, {}));
         await page
