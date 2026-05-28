ad25676c303991050c3a077e45abe51ea3db6d9a
travagliad <215686151+travagliad@users.noreply.github.com>
2026-05-28 13:05:21 +0100

    PMM-7: Use client package release 1 for PMM 3.8.1+
    Align pinned pmm-client DEB/RPM URLs with percona/pmm#5433: release 7 before 3.8.0, 8 for 3.8.0, 1 for newer versions.

--- qa-integration/pmm_qa/pmm3-client-setup-centos.sh
+++ qa-integration/pmm_qa/pmm3-client-setup-centos.sh
@@ -83,10 +83,10 @@ fi
 
 if [[ "$client_version" =~ ^3\.[0-9]+\.[0-9]+$ ]]; then
   build_number=7
-  minor_version=${client_version#3.}
-  minor_version=${minor_version%%.*}
-  if [[ "$client_version" == "3.7.1" || "$minor_version" -gt 7 ]]; then
+  if [[ "$client_version" == "3.7.1" || "$client_version" == "3.8.0" ]]; then
     build_number=8
+  elif [[ "$(printf '%s\n' '3.8.0' "$client_version" | sort -V | tail -n1)" == "$client_version" && "$client_version" != "3.8.0" ]]; then
+    build_number=1
   fi
   wget -O pmm-client.rpm https://repo.percona.com/pmm3-client/yum/release/9/RPMS/x86_64/pmm-client-${client_version}-${build_number}.el9.x86_64.rpm
   rpm -i pmm-client.rpm
--- qa-integration/pmm_qa/pmm3-client-setup.sh
+++ qa-integration/pmm_qa/pmm3-client-setup.sh
@@ -98,10 +98,10 @@ fi
 ## Only supported for debian based systems for now
 if [[ "$client_version" =~ ^3\.[0-9]+\.[0-9]+$ ]]; then
   build_number=7
-  minor_version=${client_version#3.}
-  minor_version=${minor_version%%.*}
-  if [[ "$client_version" == "3.7.1" || "$minor_version" -gt 7 ]]; then
+  if [[ "$client_version" == "3.7.1" || "$client_version" == "3.8.0" ]]; then
     build_number=8
+  elif [[ "$(printf '%s\n' '3.8.0' "$client_version" | sort -V | tail -n1)" == "$client_version" && "$client_version" != "3.8.0" ]]; then
+    build_number=1
   fi
   wget -O pmm-client.deb https://repo.percona.com/pmm3-client/apt/pool/main/p/pmm-client/pmm-client_${client_version}-${build_number}.$(lsb_release -sc)_amd64.deb
   dpkg -i pmm-client.deb
