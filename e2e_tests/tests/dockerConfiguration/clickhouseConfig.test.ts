234ca2728cf2b3ba1981cf044687e52f27416676
Peter Sirotnak <sirotnak.p@gmail.com>
2026-06-17 11:40:38 +0200

    PMM-7: Clickhouse config file test

--- e2e_tests/tests/dockerConfiguration/clickhouseConfig.test.ts
+++ e2e_tests/tests/dockerConfiguration/clickhouseConfig.test.ts
@@ -8,14 +8,19 @@ pmmTest.describe('PMM Tests to verify clickhouse configuration file', () => {
   const dockerVersion = process.env.DOCKER_VERSION || 'perconalab/pmm-server:3-dev-latest';
   const configuration = [
     {
-      command: `docker run --detach --restart always --network="pmm-qa" -e PMM_ENABLE_TELEMETRY=0 --publish 83:8080 --publish 446:8443 --name ${dockerContainerName} ${dockerVersion}`,
+      command: `docker run --detach --restart always --network="pmm-qa" -e PMM_CLICKHOUSE_CONFIG=default -e PMM_ENABLE_TELEMETRY=0 --publish 83:8080 --publish 446:8443 --name ${dockerContainerName} ${dockerVersion}`,
       configName: 'default-config',
       port: 446,
     },
     {
-      command: `docker run --detach --restart always --network="pmm-qa" -e PMM_ENABLE_TELEMETRY=0 --publish 84:8080 --publish 447:8443 --name ${dockerContainerName} ${dockerVersion}`,
+      command: `docker run --detach --restart always --network="pmm-qa" -e PMM_CLICKHOUSE_CONFIG=low-memory -e PMM_ENABLE_TELEMETRY=0 --publish 84:8080 --publish 447:8443 --name ${dockerContainerName} ${dockerVersion}`,
       configName: 'docker volume',
       port: 447,
+    },
+    {
+      command: `docker run --detach --restart always --network="pmm-qa" -e PMM_ENABLE_TELEMETRY=0 --publish 83:8080 --publish 448:8443 --name ${dockerContainerName} ${dockerVersion}`,
+      configName: 'default-config',
+      port: 448,
     },
   ];
 
