9ca308dc0262e71ec6884bb04e6d3f9a66fdb9bb
Peter Sirotnak <sirotnak.p@gmail.com>
2026-06-25 09:54:05 +0200

    PMM-7: Fix MySql backups

--- e2e_tests/tests/dockerConfiguration/srvFolder.test.ts
+++ e2e_tests/tests/dockerConfiguration/srvFolder.test.ts
@@ -1,7 +1,6 @@
 import pmmTest from '@fixtures/pmmTest';
 import { expect } from '@playwright/test';
 import { Timeouts } from '@helpers/timeouts';
-import dataTest from '@fixtures/dataTest';
 
 pmmTest.describe('Test for SRV folder in pmm server.', () => {
   const baseUrl = `https://127.0.0.1:444/`;
@@ -14,7 +13,7 @@ pmmTest.describe('Test for SRV folder in pmm server.', () => {
   pmmTest.describe.configure({ mode: 'serial' });
   pmmTest.use({ baseURL: baseUrl });
 
-  const srvConfiguration = [
+  const srvConfigurations = [
     {
       command: `sudo mkdir -p $HOME/srv && sudo chown -R 1000:0 $HOME/srv && docker run --detach --restart always --network="pmm-qa" -e PMM_ENABLE_TELEMETRY=0 -e GF_SECURITY_ADMIN_USER=${newUser} -e GF_SECURITY_ADMIN_PASSWORD=${newPassword} -e PMM_ENABLE_INTERNAL_PG_QAN=1 --publish 444:8443 --volume "$HOME/srv":/srv --name ${dockerContainerName} ${dockerVersion}`,
       testName: 'local folder',
@@ -35,72 +34,74 @@ pmmTest.describe('Test for SRV folder in pmm server.', () => {
     cliHelper.execSilent(`docker rm -f ${dockerContainerName}`);
   });
 
-  dataTest(srvConfiguration).pmmTest(
-    'PMM-T1255 + PMM-T1279 - Verify GF_SECURITY_ADMIN_PASSWORD environment variable also with changed admin credentials @docker-configuration',
-    async (data, { api, cliHelper, dashboard, grafanaHelper, page, qanStoredMetrics, urlHelper }) => {
-      cliHelper.execSilent(data.command);
-      console.log(cliHelper.execSilent('docker logs pmm-server-srv').stdout);
-      await api.serverApi.waitForReady();
+  for (const configuration of srvConfigurations) {
+    pmmTest(
+      'PMM-T1255 + PMM-T1279 - Verify GF_SECURITY_ADMIN_PASSWORD environment variable also with changed admin credentials @docker-configuration',
+      async ({ api, cliHelper, dashboard, grafanaHelper, page, qanStoredMetrics, urlHelper }) => {
+        cliHelper.execSilent(configuration.command);
+        console.log(cliHelper.execSilent('docker logs pmm-server-srv').stdout);
+        await api.serverApi.waitForReady();
 
-      const logs = cliHelper.execSilent('docker logs pmm-server-srv').stdout;
+        const logs = cliHelper.execSilent('docker logs pmm-server-srv').stdout;
 
-      expect(logs).not.toContain(
-        'Configuration warning: unknown environment variable "GF_SECURITY_ADMIN_PASSWORD=newpass"',
-      );
+        expect(logs).not.toContain(
+          'Configuration warning: unknown environment variable "GF_SECURITY_ADMIN_PASSWORD=newpass"',
+        );
 
-      expect(logs).not.toContain(
-        'Error: The directory named as part of the path /srv/logs/supervisord.log does not exist',
-      );
+        expect(logs).not.toContain(
+          'Error: The directory named as part of the path /srv/logs/supervisord.log does not exist',
+        );
 
-      await grafanaHelper.authorize('admin', 'admin', baseUrl);
-      await page.goto(urlHelper.buildUrlWithParameters(baseUrl + dashboard.home.url, {}));
-      await page
-        .locator('//h1[text()="Percona Monitoring and Management"]')
-        .waitFor({ state: 'visible', timeout: Timeouts.TEN_SECONDS });
+        await grafanaHelper.authorize('admin', 'admin', baseUrl);
+        await page.goto(urlHelper.buildUrlWithParameters(baseUrl + dashboard.home.url, {}));
+        await page
+          .locator('//h1[text()="Percona Monitoring and Management"]')
+          .waitFor({ state: 'visible', timeout: Timeouts.TEN_SECONDS });
 
-      await grafanaHelper.unAuthorize();
-      // eslint-disable-next-line playwright/no-wait-for-timeout -- wait for un-authorization
-      await page.waitForTimeout(Timeouts.FIVE_SECONDS);
-      await grafanaHelper.authorize(newUser, newPassword, baseUrl);
+        await grafanaHelper.unAuthorize();
+        // eslint-disable-next-line playwright/no-wait-for-timeout -- wait for un-authorization
+        await page.waitForTimeout(Timeouts.FIVE_SECONDS);
+        await grafanaHelper.authorize(newUser, newPassword, baseUrl);
 
-      // eslint-disable-next-line playwright/no-wait-for-timeout -- wait for authorization
-      await page.waitForTimeout(Timeouts.FIVE_SECONDS);
-      await page.goto(urlHelper.buildUrlWithParameters(baseUrl + dashboard.home.url, {}));
-      await dashboard.home.elements.homeDashboardLocator.waitFor({
-        state: 'visible',
-        timeout: Timeouts.TWENTY_SECONDS,
-      });
+        // eslint-disable-next-line playwright/no-wait-for-timeout -- wait for authorization
+        await page.waitForTimeout(Timeouts.FIVE_SECONDS);
+        await page.goto(urlHelper.buildUrlWithParameters(baseUrl + dashboard.home.url, {}));
+        await dashboard.home.elements.homeDashboardLocator.waitFor({
+          state: 'visible',
+          timeout: Timeouts.TWENTY_SECONDS,
+        });
 
-      await grafanaHelper.unAuthorize();
-      cliHelper.execSilent('docker exec pmm-server-srv change-admin-password anotherpass');
-      // eslint-disable-next-line playwright/no-wait-for-timeout -- wait for password change
-      await page.waitForTimeout(Timeouts.FIVE_SECONDS);
-      await grafanaHelper.authorize(newUser, 'anotherpass', baseUrl);
-      // eslint-disable-next-line playwright/no-wait-for-timeout -- wait for auth
-      await page.waitForTimeout(Timeouts.FIVE_SECONDS);
-      await page.goto(urlHelper.buildUrlWithParameters(baseUrl + dashboard.home.url, {}));
-      await dashboard.home.elements.homeDashboardLocator.waitFor({
-        state: 'visible',
-        timeout: Timeouts.TWENTY_SECONDS,
-      });
+        await grafanaHelper.unAuthorize();
+        cliHelper.execSilent('docker exec pmm-server-srv change-admin-password anotherpass');
+        // eslint-disable-next-line playwright/no-wait-for-timeout -- wait for password change
+        await page.waitForTimeout(Timeouts.FIVE_SECONDS);
+        await grafanaHelper.authorize(newUser, 'anotherpass', baseUrl);
+        // eslint-disable-next-line playwright/no-wait-for-timeout -- wait for auth
+        await page.waitForTimeout(Timeouts.FIVE_SECONDS);
+        await page.goto(urlHelper.buildUrlWithParameters(baseUrl + dashboard.home.url, {}));
+        await dashboard.home.elements.homeDashboardLocator.waitFor({
+          state: 'visible',
+          timeout: Timeouts.TWENTY_SECONDS,
+        });
 
-      await page.goto(urlHelper.buildUrlWithParameters(baseUrl + qanStoredMetrics.url, { refresh: '10s' }));
-      await qanStoredMetrics.waitForQanStoredMetricsToHaveData(Timeouts.ONE_MINUTE);
+        await page.goto(urlHelper.buildUrlWithParameters(baseUrl + qanStoredMetrics.url, { refresh: '10s' }));
+        await qanStoredMetrics.waitForQanStoredMetricsToHaveData(Timeouts.ONE_MINUTE);
 
-      await page.goto(
-        urlHelper.buildUrlWithParameters(baseUrl + dashboard.os.nodeSummary.url, { from: 'now-1h' }),
-      );
+        await page.goto(
+          urlHelper.buildUrlWithParameters(baseUrl + dashboard.os.nodeSummary.url, { from: 'now-1h' }),
+        );
 
-      await dashboard.verifyMetricsPresent(dashboard.os.nodeSummary.metrics);
-      await dashboard.verifyAllPanelsHaveData([
-        ...dashboard.os.nodeSummary.noDataMetrics,
-        'System Uptime',
-        'Virtual CPUs',
-        'Disk Space',
-        'RAM',
-        'Virtual Memory',
-      ]);
-      await dashboard.verifyPanelValues(dashboard.os.nodeSummary.metricsWithData);
-    },
-  );
+        await dashboard.verifyMetricsPresent(dashboard.os.nodeSummary.metrics);
+        await dashboard.verifyAllPanelsHaveData([
+          ...dashboard.os.nodeSummary.noDataMetrics,
+          'System Uptime',
+          'Virtual CPUs',
+          'Disk Space',
+          'RAM',
+          'Virtual Memory',
+        ]);
+        await dashboard.verifyPanelValues(dashboard.os.nodeSummary.metricsWithData);
+      },
+    );
+  }
 });
