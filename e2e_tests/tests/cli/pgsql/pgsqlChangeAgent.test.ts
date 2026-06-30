6e6b7384eecb47e03ed6b9ac203ec5c29d36e341
Peter Sirotnak <sirotnak.p@gmail.com>
2026-06-30 14:23:17 +0200

    PMM-14919: Tests for pgsql

--- e2e_tests/tests/cli/pgsql/pgsqlChangeAgent.test.ts
+++ e2e_tests/tests/cli/pgsql/pgsqlChangeAgent.test.ts
@@ -1,18 +1,31 @@
 import pmmTest from '@fixtures/pmmTest';
 
 pmmTest.describe('Tests to verify pmm-admin inventory change agent functionality', () => {
-
   pmmTest('PMM-T9991 @pgsm-pmm-integration', async ({ cliHelper }) => {
+    const newPassword = 'new_password_change_agent';
     const containerName = cliHelper.execSilent(`docker ps --format '{{.Names}}' | grep pdpgsql`).stdout;
+    const pgVersion: string = containerName.match(/\d+/)[0]?;
     const serviceName = cliHelper.execSilent(
-      `docker exec ${containerName} pmm-admin list | grep pdpgsql_pmm_18 | head -1 | awk -F' ' '{print $2}'`,
+      `docker exec ${containerName} pmm-admin list | grep pdpgsql_pmm | head -1 | awk -F' ' '{print $2}'`,
     ).stdout;
     const serviceId = cliHelper.execSilent(
-      `docker exec ${containerName} pmm-admin list | grep pdpgsql_pmm_18 | head -1 | awk -F' ' '{print $4}'`,
+      `docker exec ${containerName} pmm-admin list | grep pdpgsql_pmm | head -1 | awk -F' ' '{print $4}'`,
     ).stdout;
 
+    console.log(cliHelper.execSilent(
+      `docker exec ${containerName} pmm-admin list'`,
+    ).stdout);
     console.log(`Container name is: ${containerName}`);
     console.log(`Service name is: ${serviceName}`);
     console.log(`Service ID is: ${serviceId}`);
+    cliHelper.execSilent(
+      `docker exec ${containerName} psql -U postgres -c "ALTER USER pmm WITH PASSWORD '${newPassword}';"`,
+    );
+
+    const restart = cliHelper.execSilent(
+      `docker exec ${containerName} pg_ctlcluster ${pgVersion} main restart`,
+    );
+
+    console.log(restart.stdout)
   });
 });
