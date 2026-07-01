850deb9ceae57d951fafdab798257e60e3d5e0ec
Peter Sirotnak <sirotnak.p@gmail.com>
2026-07-01 08:58:54 +0200

    PMM-14919: Tests for pgsql

--- e2e_tests/pages/inventory/agents.page.ts
+++ e2e_tests/pages/inventory/agents.page.ts
@@ -3,15 +3,28 @@ import { Timeouts } from '@helpers/timeouts';
 import { expect } from '@playwright/test';
 
 export default class AgentsPage extends BasePage {
-  builders = {};
+  builders = {
+    hideRowDetails: (agentId: string) =>
+      this.grafanaIframe().locator(
+        `//td[@title="${agentId}"]//ancestor::tr//button[@data-testid="hide-row-details"]`,
+      ),
+    showRowDetails: (agentId: string) =>
+      this.grafanaIframe().locator(
+        `//td[@title="${agentId}"]//ancestor::tr//button[@data-testid="show-row-details"]`,
+      ),
+    property: (propertyText: string) => this.grafanaIframe().locator(`//span[@data-testid="details-row-content"]//span[text()="${propertyText}"]`),
+  };
   buttons = {};
   elements = {
+    rowDetails: this.grafanaIframe().locator('//span[@data-testid="details-row-content"]'),
     rtaAgentStatus: this.grafanaIframe().locator(
       '//td[@title="rta-mongodb-agent"]//parent::tr//td[position()="2"]',
     ),
   };
   inputs = {};
   messages = {};
+  // eslint-disable-next-line perfectionist/sort-classes -- Issue with url being the method
+  readonly url = (serviceId: string) => `graph/inventory/services/${serviceId}/agents`;
 
   verifyRTAAgentStatus = async (expectedStatus: string, timeout: Timeouts = Timeouts.TEN_SECONDS) => {
     await expect(async () => {
@@ -23,5 +36,15 @@ export default class AgentsPage extends BasePage {
         `Real time analytics agent status is: ${await this.elements.rtaAgentStatus.textContent()} but should be ${expectedStatus}`,
       ).toHaveText(expectedStatus);
     }).toPass({ timeout: timeout });
+  };
+
+  showRowDetails = async (agentId: string) => {
+    await this.builders.showRowDetails(agentId).click();
+    await this.elements.rowDetails.waitFor({ state: 'visible' });
+  };
+
+  hideRowDetails = async (agentId: string) => {
+    await this.builders.hideRowDetails(agentId).click();
+    await this.elements.rowDetails.waitFor({ state: 'detached' });
   };
 }
--- e2e_tests/tests/cli/pgsql/pgsqlChangeAgent.test.ts
+++ e2e_tests/tests/cli/pgsql/pgsqlChangeAgent.test.ts
@@ -1,6 +1,7 @@
 import pmmTest from '@fixtures/pmmTest';
 import { Timeouts } from '@helpers/timeouts';
 import { ServiceStatus } from '@pages/inventory/services.page';
+import { expect } from '@playwright/test';
 
 pmmTest.describe('Tests to verify pmm-admin inventory change agent functionality', () => {
   pmmTest.describe.configure({ retries: 0 });
@@ -15,64 +16,75 @@ pmmTest.describe('Tests to verify pmm-admin inventory change agent functionality
     );
   });
 
-  pmmTest('PMM-T9991 @pgsm-pmm-integration', async ({ cliHelper, grafanaHelper, page, servicesPage }) => {
-    const newPassword = 'new_password_change_agent';
-    const containerName = cliHelper
-      .execSilent(`docker ps --format '{{.Names}}' | grep pdpgsql`)
-      .stdout.trim();
-    const pgVersion: string = containerName.match(/\d+/)?.[0] ?? '';
-    const serviceName = cliHelper
-      .execSilent(
-        `docker exec ${containerName} pmm-admin list | grep pdpgsql_pmm | head -1 | awk -F' ' '{print $2}'`,
-      )
-      .stdout.trim();
-    const serviceId = cliHelper
-      .execSilent(
-        `docker exec ${containerName} pmm-admin list | grep pdpgsql_pmm | head -1 | awk -F' ' '{print $4}'`,
-      )
-      .stdout.trim();
-    const pgExporterId = cliHelper
-      .execSilent(
-        `docker exec ${containerName} pmm-admin list | grep ${serviceId} | grep postgres_exporter | awk -F' ' '{print $4}'`,
-      )
-      .stdout.trim();
-    const pgStatMonitorId = cliHelper
-      .execSilent(
-        `docker exec ${containerName} pmm-admin list | grep ${serviceId} | grep postgresql_pgstatmonitor_agent | awk -F' ' '{print $3}'`,
-      )
-      .stdout.trim();
+  pmmTest(
+    'PMM-T9991 @pgsm-pmm-integration',
+    async ({ agentsPage, cliHelper, grafanaHelper, page, servicesPage }) => {
+      const newPassword = 'new_password_change_agent';
+      const containerName = cliHelper
+        .execSilent(`docker ps --format '{{.Names}}' | grep pdpgsql`)
+        .stdout.trim();
+      const pgVersion: string = containerName.match(/\d+/)?.[0] ?? '';
+      const serviceName = cliHelper
+        .execSilent(
+          `docker exec ${containerName} pmm-admin list | grep pdpgsql_pmm | head -1 | awk -F' ' '{print $2}'`,
+        )
+        .stdout.trim();
+      const serviceId = cliHelper
+        .execSilent(
+          `docker exec ${containerName} pmm-admin list | grep pdpgsql_pmm | head -1 | awk -F' ' '{print $4}'`,
+        )
+        .stdout.trim();
+      const pgExporterId = cliHelper
+        .execSilent(
+          `docker exec ${containerName} pmm-admin list | grep ${serviceId} | grep postgres_exporter | awk -F' ' '{print $4}'`,
+        )
+        .stdout.trim();
+      const pgStatMonitorId = cliHelper
+        .execSilent(
+          `docker exec ${containerName} pmm-admin list | grep ${serviceId} | grep postgresql_pgstatmonitor_agent | awk -F' ' '{print $3}'`,
+        )
+        .stdout.trim();
 
-    console.log(cliHelper.execSilent(`docker exec ${containerName} pmm-admin list'`).stdout);
-    console.log(`Container name is: ${containerName}`);
-    console.log(`Service name is: ${serviceName}`);
-    console.log(`Service ID is: ${serviceId}`);
-    console.log(
-      cliHelper.execSilent(`docker exec ${containerName} pmm-admin list | grep ${serviceId}`).stdout.trim(),
-    );
-    console.log(`PG Exporter ID is: ${pgExporterId}`);
-    console.log(`Pg stat monitor id is: ${pgStatMonitorId}`);
+      console.log(cliHelper.execSilent(`docker exec ${containerName} pmm-admin list'`).stdout);
+      console.log(`Container name is: ${containerName}`);
+      console.log(`Service name is: ${serviceName}`);
+      console.log(`Service ID is: ${serviceId}`);
+      console.log(
+        cliHelper.execSilent(`docker exec ${containerName} pmm-admin list | grep ${serviceId}`).stdout.trim(),
+      );
+      console.log(`PG Exporter ID is: ${pgExporterId}`);
+      console.log(`Pg stat monitor id is: ${pgStatMonitorId}`);
 
-    cliHelper.execSilent(
-      `docker exec ${containerName} psql -U postgres -c "ALTER USER pmm WITH PASSWORD '${newPassword}';"`,
-    );
-    cliHelper.execSilent(`docker exec ${containerName} pg_ctlcluster ${pgVersion} main restart`);
+      cliHelper.execSilent(
+        `docker exec ${containerName} psql -U postgres -c "ALTER USER pmm WITH PASSWORD '${newPassword}';"`,
+      );
+      cliHelper.execSilent(`docker exec ${containerName} pg_ctlcluster ${pgVersion} main restart`);
 
-    await grafanaHelper.authorize();
-    await page.goto(servicesPage.url);
-    await servicesPage.waitForServiceStatus(serviceName, ServiceStatus.DOWN, Timeouts.ONE_MINUTE);
+      await grafanaHelper.authorize();
+      await page.goto(servicesPage.url);
+      await servicesPage.waitForServiceStatus(serviceName, ServiceStatus.DOWN, Timeouts.ONE_MINUTE);
 
-    const changeAgentPassword = cliHelper.execSilent(
-      `docker exec ${containerName} pmm-admin inventory change agent postgres-exporter ${pgExporterId} --password=${newPassword} --custom-labels=env=qa_testing_pgexporter`,
-    );
-    const changePgstatmonitorAgentPassword = cliHelper.execSilent(
-      `docker exec ${containerName} pmm-admin inventory change agent qan-postgresql-pgstatmonitor-agent ${pgStatMonitorId} --password=${newPassword} --custom-labels=env=qa_testing_pgstatmonitor`,
-    );
+      const changeAgentPassword = cliHelper.execSilent(
+        `docker exec ${containerName} pmm-admin inventory change agent postgres-exporter ${pgExporterId} --password=${newPassword} --custom-labels=env=qa_testing_pgexporter`,
+      );
+      const changePgstatmonitorAgentPassword = cliHelper.execSilent(
+        `docker exec ${containerName} pmm-admin inventory change agent qan-postgresql-pgstatmonitor-agent ${pgStatMonitorId} --password=${newPassword} --custom-labels=env=qa_testing_pgstatmonitor`,
+      );
 
-    console.log(`change agent password is: ${changeAgentPassword.stdout}`);
-    console.log(`change agent password is: ${changePgstatmonitorAgentPassword.stdout}`);
+      console.log(`change agent password is: ${changeAgentPassword.stdout}`);
+      console.log(`change agent password is: ${changePgstatmonitorAgentPassword.stdout}`);
 
-    await servicesPage.waitForServiceStatus(serviceName, ServiceStatus.UP, Timeouts.ONE_MINUTE);
+      await servicesPage.waitForServiceStatus(serviceName, ServiceStatus.UP, Timeouts.ONE_MINUTE);
 
-    throw new Error('Expected!');
-  });
+      console.log(`Url is: ${agentsPage.url(serviceId)}`);
+      await page.goto(agentsPage.url(serviceId));
+      await agentsPage.showRowDetails(pgExporterId);
+      await expect(agentsPage.builders.property('env=qa_testing_pgexporter')).toBeVisible();
+      await agentsPage.hideRowDetails(pgExporterId);
+      await agentsPage.showRowDetails(pgStatMonitorId);
+      await expect(agentsPage.builders.property('env=qa_testing_pgstatmonitor')).toBeVisible();
+
+      throw new Error('Expected!');
+    },
+  );
 });
