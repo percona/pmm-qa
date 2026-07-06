4ff340de57cc19b5abf93dd06fb8633790ae2ebd
Peter Sirotnak <sirotnak.p@gmail.com>
2026-07-06 15:31:01 +0200

    PMM-14766: Valkey

--- e2e_tests/helpers/cli.helper.ts
+++ e2e_tests/helpers/cli.helper.ts
@@ -23,23 +23,23 @@ export default class CliHelper {
   createTlsCertificates = (containerName: string): void => {
     const prefix = `docker exec ${containerName}`;
 
-    this.execSilent(`${prefix} mkdir -p /certs`);
-    this.execSilent(`${prefix} apt install -y git nano`);
-    this.execSilent(`${prefix} git clone https://github.com/OpenVPN/easy-rsa.git`);
-    this.execSilent(`${prefix} /easy-rsa/easyrsa3/easyrsa --pki-dir=/easy-rsa/easyrsa3/pki init-pki`);
-    this.execSilent(
+    console.log(this.execSilent(`${prefix} mkdir -p /certs`));
+    console.log(this.execSilent(`${prefix} apt install -y git nano`));
+    console.log(this.execSilent(`${prefix} git clone https://github.com/OpenVPN/easy-rsa.git`));
+    console.log(this.execSilent(`${prefix} /easy-rsa/easyrsa3/easyrsa --pki-dir=/easy-rsa/easyrsa3/pki init-pki`));
+    console.log(this.execSilent(
       `${prefix} /easy-rsa/easyrsa3/easyrsa --pki-dir=/easy-rsa/easyrsa3/pki --req-cn=Percona --batch build-ca nopass`,
-    );
-    this.execSilent(
+    ));
+    console.log(this.execSilent(
       `${prefix} /easy-rsa/easyrsa3/easyrsa --pki-dir=/easy-rsa/easyrsa3/pki --req-ou=server --subject-alt-name=DNS:${containerName} --batch build-server-full pmm-server nopass`,
-    );
-    this.execSilent(
+    ));
+    console.log(this.execSilent(
       `${prefix} /easy-rsa/easyrsa3/easyrsa --pki-dir=/easy-rsa/easyrsa3/pki --req-ou=server --subject-alt-name=DNS:${containerName} --batch build-server-full ${containerName} nopass`,
-    );
-    this.execSilent(
+    ));
+    console.log(this.execSilent(
       `${prefix} /easy-rsa/easyrsa3/easyrsa --pki-dir=/easy-rsa/easyrsa3/pki --req-ou=client --batch build-client-full pmm-test nopass`,
-    );
-    this.execSilent(`${prefix} openssl dhparam -out /certs/dhparam.pem 2048`);
+    ));
+    console.log(this.execSilent(`${prefix} openssl dhparam -out /certs/dhparam.pem 2048`));
   };
 
   /**
