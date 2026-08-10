17358c062ae0157b537ed43f27143fa1e1d54430
Peter Sirotnak <sirotnak.p@gmail.com>
2026-08-10 12:21:32 +0200

    PMM-15258: Upgrade

--- codeceptjs-e2e/tests/upgrade/dashboards_test.js
+++ codeceptjs-e2e/tests/upgrade/dashboards_test.js
@@ -6,30 +6,6 @@ Before(async ({ I }) => {
   await I.Authorize();
 });
 
-Scenario(
-  'PMM-12587-2 Verify duplicate dashboards dont break after upgrade @post-dashboards-upgrade',
-  async ({
-    I, grafanaAPI, dashboardPage,
-  }) => {
-    const workFolder = await I.verifyCommand('pwd');
-    const resp = JSON.parse(await I.readFileSync(`${workFolder}/dashboard.json`, false));
-
-    const resp1 = await grafanaAPI.getDashboard(resp.DASHBOARD1_UID);
-    const resp2 = await grafanaAPI.getDashboard(resp.DASHBOARD2_UID);
-
-    // Trim leading '/' from response url
-    const url1 = resp1.meta.url.replace(/^\/+/g, '');
-    const url2 = resp2.meta.url.replace(/^\/+/g, '');
-
-    I.amOnPage(url1);
-    dashboardPage.waitForDashboardOpened();
-    I.seeInCurrentUrl(url1);
-    I.amOnPage(url2);
-    dashboardPage.waitForDashboardOpened();
-    I.seeInCurrentUrl(url2);
-  },
-);
-
 // mongodb dashboards test after upgrade replicaset cluster routers.
 // mysql overview and postgres overview.
 // invetory nodes, services and agents.
