33cd7293fd8ca98c0ae891b7e8d632a7e4e28f19
Peter Sirotnak <sirotnak.p@gmail.com>
2026-08-11 09:21:51 +0200

    PMM-15258: Upgrade

--- e2e_tests/tests/upgrade.test.ts
+++ e2e_tests/tests/upgrade.test.ts
@@ -5,8 +5,11 @@ import { Timeouts } from '@helpers/timeouts';
 import { expect, Page } from '@playwright/test';
 
 pmmTest.describe('PMM upgrade tests for custom password', () => {
-  const pdpgsql = { password: 'pmm', username: 'pmm' };
-  const mongo = { host: '127.0.0.1', password: 'pmmpass', port: '27027', username: 'pmm' };
+  // pmmTest.describe.configure({ retries: 0 });
+
+  const pdpgsql = { host: '127.0.0.1', password: 'pmm', port: '5432', username: 'pmm' };
+  const mongo = { host: '127.0.0.1', password: 'pmmpass', port: '27017', username: 'pmm' };
+  const mysql = { host: '127.0.0.1', password: 'GRgrO9301RuF', port: '3306', username: 'root' };
   const services = [
     {
       containerName: 'ps_pmm_8_4_1',
@@ -39,18 +42,16 @@ pmmTest.describe('PMM upgrade tests for custom password', () => {
         const pmmAgentId = details.agents.find(
           (agent: { agent_type: string }) => agent.agent_type === 'pmm-agent',
         )?.agent_id;
-        const isOvfAmiJob = !!process.env.JOB_NAME && /ami|ovf/.test(process.env.JOB_NAME);
-        const host = isOvfAmiJob ? '127.0.0.1' : details.address;
-        const remote = `--node-id=${details.node_id} --pmm-agent-id=${pmmAgentId} --port=${details.port} --host=${host}`;
         const labels = `--agent-password=uitests --custom-labels="testing=upgrade" upgrade-${service.upgradeService}`;
-
         const addCommand: Record<string, string> = {
           mongodb: `pmm-admin add mongodb --username=${mongo.username} --password="${mongo.password}" --port=${mongo.port} --host=${mongo.host} ${labels}`,
-          mysql: `pmm-admin add mysql ${remote} --password=${credentials.perconaServer.password} --query-source=perfschema ${labels}`,
-          postgresql: `pmm-admin add postgresql --username=${pdpgsql.username} --password=${pdpgsql.password} ${remote} ${labels}`,
+          mysql: `pmm-admin add mysql --password=${credentials.perconaServer.password} --port=${mysql.port} --host=${mysql.host} ${labels}`,
+          postgresql: `pmm-admin add postgresql --username=${pdpgsql.username} --password=${pdpgsql.password} --port=${mysql.port} --host=${mysql.host} ${labels}`,
         };
 
-        cliHelper.execute(`docker exec ${service.containerName} ${addCommand[service.serviceType]}`).assertSuccess();
+        cliHelper
+          .execSilent(`docker exec ${service.containerName} ${addCommand[service.serviceType]}`)
+          .assertSuccess();
       },
     );
   }
