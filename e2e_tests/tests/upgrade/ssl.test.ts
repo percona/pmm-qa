499b7fca4a1e3d99ff229d206616c5823876e42a
Peter Sirotnak <sirotnak.p@gmail.com>
2026-08-11 15:59:24 +0200

    PMM-15258: Upgrade

--- e2e_tests/tests/upgrade/ssl.test.ts
+++ e2e_tests/tests/upgrade/ssl.test.ts
@@ -17,7 +17,9 @@ pmmTest.describe('PMM upgrade tests for SSL', () => {
         .execSilent(`docker exec ${container} cat /mongodb_certs/client.pem`)
         .assertSuccess().stdout;
       const ca = cliHelper
-        .execSilent('cat /srv/qa-integration/pmm_psmdb_diffauth_setup/pki/ca.crt')
+        .execSilent(
+          'cat /home/runner/work/pmm-qa/pmm-qa/pmm-qa/qa-integration/pmm_psmdb_diffauth_setup/pki/ca.crt',
+        )
         .assertSuccess().stdout;
 
       await api.remoteInstanceApi.addRemoteInstance({
