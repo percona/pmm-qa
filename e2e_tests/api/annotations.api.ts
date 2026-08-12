d4d5ee65cfee1f68cdf9ba50beef5c3d1d28d230
Peter Sirotnak <sirotnak.p@gmail.com>
2026-08-12 09:07:51 +0200

    PMM-15258: Upgrade

--- e2e_tests/api/annotations.api.ts
+++ e2e_tests/api/annotations.api.ts
@@ -12,15 +12,6 @@ interface SetAnnotation {
 export default class AnnotationsApi {
   constructor(private request: APIRequestContext) {}
 
-  setAnnotation = async ({ nodeName, serviceNames, tags, text }: SetAnnotation) => {
-    const response = await this.request.post(apiEndpoints.management.annotations, {
-      data: { node_name: nodeName, service_names: serviceNames, tags, text },
-      headers: GrafanaHelper.getAuthHeader(),
-    });
-
-    expect(response.status(), `Failed to add annotation "${text}". Response: ${await response.text()}`).toEqual(200);
-  };
-
   getAnnotationsByTag = async (tag: string): Promise<{ tags: string[]; text: string }[]> => {
     const response = await this.request.get(`graph/api/annotations?tags=${tag}`, {
       headers: GrafanaHelper.getAuthHeader(),
@@ -29,5 +20,14 @@ export default class AnnotationsApi {
     expect(response.status()).toEqual(200);
 
     return await response.json();
+  };
+
+  setAnnotation = async ({ nodeName, serviceNames, tags, text }: SetAnnotation) => {
+    const response = await this.request.post(apiEndpoints.management.annotations, {
+      data: { node_name: nodeName, service_names: serviceNames, tags, text },
+      headers: GrafanaHelper.getAuthHeader(),
+    });
+
+    expect(response.status(), `Failed to add annotation "${text}". Response: ${await response.text()}`).toEqual(200);
   };
 }
