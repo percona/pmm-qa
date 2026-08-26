9ea46592002134a5f0aec7e0611a1fd5dd390a2d
Peter Sirotnak <sirotnak.p@gmail.com>
2026-08-26 12:50:39 +0200

    PMM-7: Fix dashboard test

--- e2e_tests/tests/cli/mysql/rdsExporterChangeAgent.test.ts
+++ e2e_tests/tests/cli/mysql/rdsExporterChangeAgent.test.ts
@@ -293,6 +293,11 @@ pmmTest.describe('Tests to verify pmm-admin inventory change agent functionality
         .stdout.trim()
         .replaceAll('"', '');
 
+      console.log(
+        cliHelper.execSilent(
+          `docker exec pmm-server curl -s -G 'http://127.0.0.1:9090/prometheus/api/v1/query' --data-urlencode 'query=count_over_time(rdsosmetrics_General_numVCPUs[1m])' | jq `,
+        ).stdout,
+      );
       console.log(`Push metrics counts is: "${pushMetricsCount}"`);
 
       expect(
