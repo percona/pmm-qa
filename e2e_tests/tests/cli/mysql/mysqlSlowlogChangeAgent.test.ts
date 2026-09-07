cfad32a3bb35b672a48cb47e5816d9fa085a1e3c
Peter Sirotnak <sirotnak.p@gmail.com>
2026-09-07 10:35:59 +0200

    PMM-7: node exporter

--- e2e_tests/tests/cli/mysql/mysqlSlowlogChangeAgent.test.ts
+++ e2e_tests/tests/cli/mysql/mysqlSlowlogChangeAgent.test.ts
@@ -257,28 +257,31 @@ pmmTest.describe('Tests to verify pmm-admin inventory change agent functionality
     },
   );
 
-  pmmTest('PMM-T1016 - Verify Change agent comments parsing @ps-integration', async ({ api, cliHelper }) => {
-    const commentsParsingCases = [
-      { disabled: false, response: '- enabled comments parsing', value: 'on' },
-      { disabled: true, response: '- disabled comments parsing', value: 'off' },
-    ];
+  pmmTest(
+    'PMM-T1016 - Verify Change agent comments parsing @ps-slowlog-integration',
+    async ({ api, cliHelper }) => {
+      const commentsParsingCases = [
+        { disabled: false, response: '- enabled comments parsing', value: 'on' },
+        { disabled: true, response: '- disabled comments parsing', value: 'off' },
+      ];
 
-    for (const commentsParsingCase of commentsParsingCases) {
-      await cliHelper
-        .execSilent(
-          `docker exec ${containerName} pmm-admin inventory change agent qan-mysql-slowlog-agent ${mysqldSlowlogAgentId} --comments-parsing=${commentsParsingCase.value}`,
-        )
-        .assertSuccess()
-        .outContains(commentsParsingCase.response);
+      for (const commentsParsingCase of commentsParsingCases) {
+        await cliHelper
+          .execSilent(
+            `docker exec ${containerName} pmm-admin inventory change agent qan-mysql-slowlog-agent ${mysqldSlowlogAgentId} --comments-parsing=${commentsParsingCase.value}`,
+          )
+          .assertSuccess()
+          .outContains(commentsParsingCase.response);
 
-      const agent = await api.inventoryApi.getAgentById(mysqldSlowlogAgentId);
+        const agent = await api.inventoryApi.getAgentById(mysqldSlowlogAgentId);
 
-      expect(
-        agent.comments_parsing_disabled ?? false,
-        `Comments parsing '${commentsParsingCase.value}' was not persisted on the qan_mysql_slowlog_agent`,
-      ).toEqual(commentsParsingCase.disabled);
-    }
-  });
+        expect(
+          agent.comments_parsing_disabled ?? false,
+          `Comments parsing '${commentsParsingCase.value}' was not persisted on the qan_mysql_slowlog_agent`,
+        ).toEqual(commentsParsingCase.disabled);
+      }
+    },
+  );
 
   pmmTest(
     'PMM-T9998 - Verify Change agent skip connection check @ps-slowlog-integration',
@@ -320,6 +323,27 @@ pmmTest.describe('Tests to verify pmm-admin inventory change agent functionality
         agent.table_count_tablestats_group_limit,
         'Tablestats group table limit was not persisted on the mysqld_exporter agent',
       ).toEqual(tablestatsGroupTableLimit);
+    },
+  );
+
+  pmmTest(
+    'PMM-T1015 - Verify Change agent max query length @ps-slowlog-integration',
+    async ({ api, cliHelper }) => {
+      const maxQueryLength = 2_048;
+
+      await cliHelper
+        .execSilent(
+          `docker exec ${containerName} pmm-admin inventory change agent qan-mysql-slowlog-agent ${mysqldSlowlogAgentId} --max-query-length=${maxQueryLength}`,
+        )
+        .assertSuccess()
+        .outContains(`- changed max query length to ${maxQueryLength}`);
+
+      const agent = await api.inventoryApi.getAgentById(mysqldSlowlogAgentId);
+
+      expect(
+        agent.max_query_length,
+        'Max query length was not persisted on the qan_mysql_perfschema_agent',
+      ).toEqual(maxQueryLength);
     },
   );
 
