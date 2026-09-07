8ce1096163371fc3c313e6362d175a453bef550c
Peter Sirotnak <sirotnak.p@gmail.com>
2026-09-07 10:10:04 +0200

    PMM-7: node exporter

--- e2e_tests/tests/cli/mysql/mysqlPerfschemaChangeAgent.test.ts
+++ e2e_tests/tests/cli/mysql/mysqlPerfschemaChangeAgent.test.ts
@@ -263,52 +263,46 @@ pmmTest.describe('Tests to verify pmm-admin inventory change agent functionality
     },
   );
 
-  pmmTest(
-    'PMM-T1015 - Verify Change agent max query length @ps-integration',
-    async ({ api, cliHelper }) => {
-      const maxQueryLength = 2_048;
+  pmmTest('PMM-T1015 - Verify Change agent max query length @ps-integration', async ({ api, cliHelper }) => {
+    const maxQueryLength = 2_048;
 
+    await cliHelper
+      .execSilent(
+        `docker exec ${containerName} pmm-admin inventory change agent qan-mysql-perfschema-agent ${mysqldPerfschemaAgentId} --max-query-length=${maxQueryLength}`,
+      )
+      .assertSuccess()
+      .outContains(`- changed max query length to ${maxQueryLength}`);
+
+    const agent = await api.inventoryApi.getAgentById(mysqldPerfschemaAgentId);
+
+    expect(
+      agent.max_query_length,
+      'Max query length was not persisted on the qan_mysql_perfschema_agent',
+    ).toEqual(maxQueryLength);
+  });
+
+  pmmTest('PMM-T1016 - Verify Change agent comments parsing @ps-integration', async ({ api, cliHelper }) => {
+    const commentsParsingCases = [
+      { disabled: false, response: '- enabled comments parsing', value: 'on' },
+      { disabled: true, response: '- disabled comments parsing', value: 'off' },
+    ];
+
+    for (const commentsParsingCase of commentsParsingCases) {
       await cliHelper
         .execSilent(
-          `docker exec ${containerName} pmm-admin inventory change agent qan-mysql-perfschema-agent ${mysqldPerfschemaAgentId} --max-query-length=${maxQueryLength}`,
+          `docker exec ${containerName} pmm-admin inventory change agent qan-mysql-perfschema-agent ${mysqldPerfschemaAgentId} --comments-parsing=${commentsParsingCase.value}`,
         )
         .assertSuccess()
-        .outContains(`- changed max query length to ${maxQueryLength}`);
+        .outContains(commentsParsingCase.response);
 
       const agent = await api.inventoryApi.getAgentById(mysqldPerfschemaAgentId);
 
       expect(
-        agent.max_query_length,
-        'Max query length was not persisted on the qan_mysql_perfschema_agent',
-      ).toEqual(maxQueryLength);
-    },
-  );
-
-  pmmTest(
-    'PMM-T1016 - Verify Change agent comments parsing @ps-integration',
-    async ({ api, cliHelper }) => {
-      const commentsParsingCases = [
-        { disabled: false, response: '- enabled comments parsing', value: 'on' },
-        { disabled: true, response: '- disabled comments parsing', value: 'off' },
-      ];
-
-      for (const commentsParsingCase of commentsParsingCases) {
-        await cliHelper
-          .execSilent(
-            `docker exec ${containerName} pmm-admin inventory change agent qan-mysql-perfschema-agent ${mysqldPerfschemaAgentId} --comments-parsing=${commentsParsingCase.value}`,
-          )
-          .assertSuccess()
-          .outContains(commentsParsingCase.response);
-
-        const agent = await api.inventoryApi.getAgentById(mysqldPerfschemaAgentId);
-
-        expect(
-          agent.comments_parsing_disabled ?? false,
-          `Comments parsing '${commentsParsingCase.value}' was not persisted on the qan_mysql_perfschema_agent`,
-        ).toEqual(commentsParsingCase.disabled);
-      }
-    },
-  );
+        agent.comments_parsing_disabled ?? false,
+        `Comments parsing '${commentsParsingCase.value}' was not persisted on the qan_mysql_perfschema_agent`,
+      ).toEqual(commentsParsingCase.disabled);
+    }
+  });
 
   pmmTest(
     'PMM-T1013 - Verify Change agent skip connection check @ps-integration',
@@ -436,7 +430,7 @@ pmmTest.describe('Tests to verify pmm-admin inventory change agent functionality
 
       commands = [
         `docker exec ${containerName} pmm-admin inventory change agent mysqld-exporter ${mysqldExporterId} --server-url=${serverUrl} --server-insecure-tls`,
-        `docker exec ${containerName} pmm-admin inventory change agent agent qan-mysql-perfschema-agent ${mysqldPerfschemaAgentId} --server-url=${serverUrl} --server-insecure-tls`,
+        `docker exec ${containerName} pmm-admin inventory change agent qan-mysql-perfschema-agent ${mysqldPerfschemaAgentId} --server-url=${serverUrl} --server-insecure-tls`,
       ];
 
       for (const command of commands) {
