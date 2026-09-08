b1e65f1853fa547d38de333ad6e8d88dfd72a3f4
Peter Sirotnak <sirotnak.p@gmail.com>
2026-09-08 09:29:02 +0200

    PMM-7: node exporter

--- e2e_tests/tests/cli/mysql/rdsExporterChangeAgent.test.ts
+++ e2e_tests/tests/cli/mysql/rdsExporterChangeAgent.test.ts
@@ -409,21 +409,24 @@ pmmTest.describe('Tests to verify pmm-admin inventory change agent functionality
     });
   });
 
-  pmmTest('PMM-T1009 - Verify Change agent pmm agent listen port @rds-integration', async ({ cliHelper }) => {
-    const commands = [
-      `docker exec ${containerName} sed -i 's/listen-port: 7777/listen-port: 7778/' /usr/local/percona/pmm/config/pmm-agent.yaml`,
-      `docker restart ${containerName}`,
-    ];
+  pmmTest(
+    'PMM-T1009 - Verify Change agent pmm agent listen port @rds-integration',
+    async ({ cliHelper, page }) => {
+      const commands = [
+        `docker exec ${containerName} sed -i 's/listen-port: 7777/listen-port: 7778/' /usr/local/percona/pmm/config/pmm-agent.yaml`,
+        `docker restart ${containerName}`,
+      ];
 
-    commands.forEach((command) => cliHelper.execSilent(command).assertSuccess());
+      commands.forEach((command) => cliHelper.execSilent(command).assertSuccess());
 
-    // eslint-disable-next-line playwright/no-wait-for-timeout -- Wait for parameter to be propagated to exporter
-    await page.waitForTimeout(Timeouts.TEN_SECONDS);
+      // eslint-disable-next-line playwright/no-wait-for-timeout -- Wait for parameter to be propagated to exporter
+      await page.waitForTimeout(Timeouts.TEN_SECONDS);
 
-    cliHelper
-      .execSilent(
-        `docker exec ${containerName} pmm-admin inventory change agent rds-exporter 97fb8372-a052-49df-b58e-76ca2fc8e3c0 --pmm-agent-listen-port=7778`,
-      )
-      .assertSuccess();
-  });
+      cliHelper
+        .execSilent(
+          `docker exec ${containerName} pmm-admin inventory change agent rds-exporter 97fb8372-a052-49df-b58e-76ca2fc8e3c0 --pmm-agent-listen-port=7778`,
+        )
+        .assertSuccess();
+    },
+  );
 });
