c8fd604212b4ae08ba7c8a20379bc8884e5d790a
Peter Sirotnak <sirotnak.p@gmail.com>
2026-09-09 13:15:58 +0200

    PMM-13798: Arm in runner

--- e2e_tests/tests/cli/azureExporterChangeAgent.test.ts
+++ e2e_tests/tests/cli/azureExporterChangeAgent.test.ts
@@ -199,4 +199,20 @@ pmmTest.describe('Tests to verify pmm-admin inventory change agent functionality
       await expect(agentsPage.builders.property(customLabel)).toBeVisible();
     },
   );
+
+  pmmTest('PMM-T1011 - Verify Change agent pmm agent listen port @ps-integration', async ({ cliHelper }) => {
+    let commands = [
+      `docker exec ${containerName} sed -i 's/listen-port: 7777/listen-port: 7778/' /usr/local/percona/pmm/config/pmm-agent.yaml`,
+      `docker restart ${containerName}`,
+      `docker exec -d ${containerName} pmm-agent --config-file=/usr/local/percona/pmm/config/pmm-agent.yaml`,
+    ];
+
+    commands.forEach((command) => cliHelper.execSilent(command).assertSuccess());
+
+    commands = [
+      `docker exec ${containerName} pmm-admin inventory change agent azure-database-exporter ${azureExporterId} --pmm-agent-listen-port=7778`,
+    ];
+
+    commands.forEach((command) => cliHelper.execSilent(command).assertSuccess());
+  });
 });
