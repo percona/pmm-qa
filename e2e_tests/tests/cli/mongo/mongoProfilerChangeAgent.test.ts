d8ff1ff8cd846d29368992fb93eafb9ceec6ce15
Peter Sirotnak <sirotnak.p@gmail.com>
2026-09-09 17:49:13 +0200

    PMM-13798: Arm in runner

--- e2e_tests/tests/cli/mongo/mongoProfilerChangeAgent.test.ts
+++ e2e_tests/tests/cli/mongo/mongoProfilerChangeAgent.test.ts
@@ -208,36 +208,39 @@ pmmTest.describe('Tests to verify pmm-admin inventory change agent functionality
     },
   );
 
-  pmmTest('PMM-T1008 - Verify Change agent push metrics @ps-integration', async ({ cliHelper, page }) => {
-    pgExporterPort = cliHelper
-      .execSilent(
-        `docker exec ${containerName} pmm-admin list | grep ${mysqldExporterId} | awk -F' ' '{print $6}'`,
-      )
-      .stdout.trim();
-    await cliHelper
-      .execSilent(
-        `docker exec ${containerName} pmm-admin inventory change agent mysqld-exporter ${mysqldExporterId} --push-metrics`,
-      )
-      .assertSuccess()
-      .outContains('- enabled push metrics');
+  pmmTest(
+    'PMM-T1008 - Verify Change agent push metrics @psmdb-profiler-integration',
+    async ({ cliHelper, page }) => {
+      pgExporterPort = cliHelper
+        .execSilent(
+          `docker exec ${containerName} pmm-admin list | grep ${mongoExporterId} | awk -F' ' '{print $6}'`,
+        )
+        .stdout.trim();
+      await cliHelper
+        .execSilent(
+          `docker exec ${containerName} pmm-admin inventory change agent mongodb-exporter ${mongoExporterId} --push-metrics`,
+        )
+        .assertSuccess()
+        .outContains('- enabled push metrics');
 
-    // eslint-disable-next-line playwright/no-wait-for-timeout -- Wait for parameter to be propagated to exporter
-    await page.waitForTimeout(Timeouts.FIVE_SECONDS);
-    await cliHelper
-      .execSilent(
-        `docker exec pmm-server curl -u pmm:${pgExporterPassword} http://${containerName}:${pgExporterPort}/metrics`,
-      )
-      .assertSuccess()
-      .outContains('mysql_up');
-    await cliHelper
-      .execSilent(
-        `docker exec ${containerName} cat /var/log/pmm-agent.log | grep vmagent | tail -20 | grep error`,
-      )
-      .outEquals('');
-    await cliHelper
-      .execSilent(`docker exec ${containerName} pmm-admin list | grep ${mysqldExporterId}`)
-      .outContains('Running');
-  });
+      // eslint-disable-next-line playwright/no-wait-for-timeout -- Wait for parameter to be propagated to exporter
+      await page.waitForTimeout(Timeouts.FIVE_SECONDS);
+      await cliHelper
+        .execSilent(
+          `docker exec pmm-server curl -u pmm:${pgExporterPassword} http://${containerName}:${pgExporterPort}/metrics`,
+        )
+        .assertSuccess()
+        .outContains('mongodb_up');
+      await cliHelper
+        .execSilent(
+          `docker exec ${containerName} cat /var/log/pmm-agent.log | grep vmagent | tail -20 | grep error`,
+        )
+        .outEquals('');
+      await cliHelper
+        .execSilent(`docker exec ${containerName} pmm-admin list | grep ${mongoExporterId}`)
+        .outContains('Running');
+    },
+  );
 
   pmmTest(
     'PMM-T1009 - Verify Change agent disable collectors @ps-integration',
