bfc735a0471972bb046131f96ad773f0e4f16048
Peter Sirotnak <sirotnak.p@gmail.com>
2026-09-09 13:16:18 +0200

    PMM-13798: Arm in runner

--- e2e_tests/tests/cli/azureExporterChangeAgent.test.ts
+++ e2e_tests/tests/cli/azureExporterChangeAgent.test.ts
@@ -200,19 +200,22 @@ pmmTest.describe('Tests to verify pmm-admin inventory change agent functionality
     },
   );
 
-  pmmTest('PMM-T1011 - Verify Change agent pmm agent listen port @ps-integration', async ({ cliHelper }) => {
-    let commands = [
-      `docker exec ${containerName} sed -i 's/listen-port: 7777/listen-port: 7778/' /usr/local/percona/pmm/config/pmm-agent.yaml`,
-      `docker restart ${containerName}`,
-      `docker exec -d ${containerName} pmm-agent --config-file=/usr/local/percona/pmm/config/pmm-agent.yaml`,
-    ];
+  pmmTest(
+    'PMM-T1011 - Verify Change agent pmm agent listen port @azure-integration',
+    async ({ cliHelper }) => {
+      let commands = [
+        `docker exec ${containerName} sed -i 's/listen-port: 7777/listen-port: 7778/' /usr/local/percona/pmm/config/pmm-agent.yaml`,
+        `docker restart ${containerName}`,
+        `docker exec -d ${containerName} pmm-agent --config-file=/usr/local/percona/pmm/config/pmm-agent.yaml`,
+      ];
 
-    commands.forEach((command) => cliHelper.execSilent(command).assertSuccess());
+      commands.forEach((command) => cliHelper.execSilent(command).assertSuccess());
 
-    commands = [
-      `docker exec ${containerName} pmm-admin inventory change agent azure-database-exporter ${azureExporterId} --pmm-agent-listen-port=7778`,
-    ];
+      commands = [
+        `docker exec ${containerName} pmm-admin inventory change agent azure-database-exporter ${azureExporterId} --pmm-agent-listen-port=7778`,
+      ];
 
-    commands.forEach((command) => cliHelper.execSilent(command).assertSuccess());
-  });
+      commands.forEach((command) => cliHelper.execSilent(command).assertSuccess());
+    },
+  );
 });
