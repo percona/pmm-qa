79f5cabedc9fa16f401a60e8571bcd77d12040d2
Peter Sirotnak <sirotnak.p@gmail.com>
2026-09-09 11:45:39 +0200

    PMM-13798: Arm in runner

--- e2e_tests/tests/cli/azureExporterChangeAgent.test.ts
+++ e2e_tests/tests/cli/azureExporterChangeAgent.test.ts
@@ -53,7 +53,9 @@ pmmTest.describe('Tests to verify pmm-admin inventory change agent functionality
       .execSilent(`docker exec ${containerName} pmm-admin list | grep rds_exporter | awk -F' ' '{print $5}'`)
       .stdout.trim();
     console.log(
-      await api.inventoryApi.getServiceDetailsByPartialName(process.env.PMM_QA_AZURE_MYSQL_HOST || ''),
+      (
+        await api.inventoryApi.getServiceDetailsByPartialName(process.env.PMM_QA_AZURE_MYSQL_HOST || '')
+      ).agents.find((agent) => agent.agent_type === 'azure_database_exporter'),
     );
     console.log(azureExporterId);
     console.log(azureExporterPort);
