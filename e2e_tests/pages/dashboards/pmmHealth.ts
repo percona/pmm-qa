15bf05fb875a5911b057fd0bf053f8d5570edb01
kiranvuyurru <kiran.vuyurru@percona.com>
2026-09-16 07:39:44 -0600

    updated

--- e2e_tests/pages/dashboards/pmmHealth.ts
+++ e2e_tests/pages/dashboards/pmmHealth.ts
@@ -3,15 +3,14 @@ import DashboardInterface from '@interfaces/dashboard';
 
 export default class PmmHealthDashboard implements DashboardInterface {
   url = 'graph/d/pmm-health/pmm-health';
-  statusPanels = [
-    { name: 'ManageD Status', panelId: '1086' },
-    { name: 'VictoriaMetrics Status', panelId: '1088' },
-    { name: 'PostgreSQL Status', panelId: '1089' },
-    { name: 'QAN API Status', panelId: '1085' },
-    { name: 'Grafana Status', panelId: '1087' },
-    { name: 'Node Status', panelId: '1090' },
-    { name: 'Clickhouse Status', panelId: '1091' },
+  metrics: GrafanaPanel[] = [
+    { name: 'ManageD Status', type: 'stat' },
+    { name: 'VictoriaMetrics Status', type: 'stat' },
+    { name: 'PostgreSQL Status', type: 'stat' },
+    { name: 'QAN API Status', type: 'stat' },
+    { name: 'Grafana Status', type: 'stat' },
+    { name: 'Node Status', type: 'stat' },
+    { name: 'Clickhouse Status', type: 'stat' },
   ];
-  metrics: GrafanaPanel[] = this.statusPanels.map(({ name }) => ({ name, type: 'unknown' as const }));
   noDataMetrics = [];
 }
--- e2e_tests/tests/dashboards/pmmHealthDashboard.test.ts
+++ e2e_tests/tests/dashboards/pmmHealthDashboard.test.ts
@@ -1,24 +1,23 @@
 import { expect } from '@playwright/test';
 import pmmTest from '@fixtures/pmmTest';
 import { Timeouts } from '@helpers/timeouts';
+import PmmHealthDashboard from '@pages/dashboards/pmmHealth';
 
 pmmTest.beforeEach(async ({ grafanaHelper }) => {
   await grafanaHelper.authorize();
 });
 
-pmmTest(
-  'PMM-T1988 - Verify that all statuses on PMM Health dashboards are UP @nightly  @gssapi-nightly',
-  async ({ dashboard, page }) => {
-    await page.goto(dashboard.pmmHealth.url);
-
-    for (const panel of dashboard.pmmHealth.statusPanels) {
+for (const panel of new PmmHealthDashboard().metrics) {
+  pmmTest(
+    `PMM-T1988 - Verify that all statuses on PMM Health dashboards are UP @nightly  @gssapi-nightly | ${panel.name}`,
+    async ({ dashboard, page }) => {
       const panelContent = dashboard.builders
-        .gridItemByPanelId(panel.panelId)
-        .getByTestId(`data-testid Panel header ${panel.name}`)
+        .panelByExactName(panel.name)
         .getByTestId('data-testid panel content');
 
+      await page.goto(dashboard.pmmHealth.url);
       await expect(panelContent).toBeVisible({ timeout: Timeouts.ONE_MINUTE });
       await expect(panelContent).toContainText('UP', { timeout: Timeouts.TEN_SECONDS });
-    }
-  },
-);
+    },
+  );
+}
