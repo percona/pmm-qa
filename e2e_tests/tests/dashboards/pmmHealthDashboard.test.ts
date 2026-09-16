1e6c14636ddd5b45091c2e588dda9fdc582a8b5d
kiranvuyurru <kiran.vuyurru@percona.com>
2026-09-16 08:35:55 -0600

    Update pmmHealthDashboard.test.ts

--- e2e_tests/tests/dashboards/pmmHealthDashboard.test.ts
+++ e2e_tests/tests/dashboards/pmmHealthDashboard.test.ts
@@ -1,23 +1,21 @@
 import { expect } from '@playwright/test';
 import pmmTest from '@fixtures/pmmTest';
 import { Timeouts } from '@helpers/timeouts';
-import PmmHealthDashboard from '@pages/dashboards/pmmHealth';
 
 pmmTest.beforeEach(async ({ grafanaHelper }) => {
   await grafanaHelper.authorize();
 });
 
-for (const panel of new PmmHealthDashboard().metrics) {
-  pmmTest(
-    `PMM-T1988 - Verify that all statuses on PMM Health dashboards are UP @nightly  @gssapi-nightly | ${panel.name}`,
-    async ({ dashboard, page, urlHelper }) => {
+pmmTest(
+  'PMM-T1988 - Verify that all statuses on PMM Health dashboards are UP @nightly  @gssapi-nightly',
+  async ({ dashboard, page, urlHelper }) => {
+    await page.goto(urlHelper.buildUrlWithParameters(dashboard.pmmHealth.url, { from: 'now-1h', to: 'now' }));
+
+    for (const panel of dashboard.pmmHealth.metrics) {
       const panelContent = dashboard.builders.panelContentByExactName(panel.name);
 
-      await page.goto(
-        urlHelper.buildUrlWithParameters(dashboard.pmmHealth.url, { from: 'now-1h', to: 'now' }),
-      );
       await expect(panelContent).toBeVisible({ timeout: Timeouts.ONE_MINUTE });
       await expect(panelContent).toContainText('UP', { timeout: Timeouts.TEN_SECONDS });
-    },
-  );
-}
+    }
+  },
+);
